8595ea805ae1cbf9ba4d1074cc12fa3c189e1473
Arie Trouw <arie.trouw@xy.company>
2021-04-28 12:52:02 -0700

    Smaller Beta Banner

--- src/components/BasePage/BasePage.tsx
+++ src/components/BasePage/BasePage.tsx
@@ -34,8 +34,16 @@ const BasePage: React.FC<BasePageProps> = (props) => {
       <Helmet title={title}>{noindex ? <meta content="noindex" name="robots" /> : null}</Helmet>
       {hideAppBar ? null : appBar}
       {beta ? (
-        <FlexRow margin={1} position="absolute" top={8} bgcolor="#cccccc88">
-          <Typography variant="body1">
+        <FlexRow
+          margin={1}
+          position="absolute"
+          top={0}
+          left={0}
+          bgcolor="#cccccc88"
+          paddingX={1}
+          style={{ opacity: 0.5 }}
+        >
+          <Typography variant="body2">
             Important: This page is a Beta page. It is possible that some information may not be correct.
           </Typography>
         </FlexRow>
