580ccedd843ce426c2784b7efffeb568578c79df
Arie Trouw <arie.trouw@xy.company>
2021-05-07 11:57:59 -0700

    target support for ButtonEx

--- src/components/ButtonEx/ButtonExProps.tsx
+++ src/components/ButtonEx/ButtonExProps.tsx
@@ -3,6 +3,7 @@ import { ButtonProps } from '@material-ui/core'
 import BoxlikeComponentProps from '../../BoxlikeComponentProps'
 
 interface ButtonExProps extends ButtonProps, BoxlikeComponentProps {
+  target?: string
   to?: string
 }
 
