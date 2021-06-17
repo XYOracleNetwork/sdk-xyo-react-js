04035bf0902e7d9102e8eb161083eb20d0378ca2
Arie Trouw <arie.trouw@xy.company>
2021-06-17 10:23:51 -0700

    Updated Cookie Consent

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -1,35 +1,30 @@
-import { Link, Typography, useTheme } from '@material-ui/core'
-import React from 'react'
-import CookieConsent, { CookieConsentProps } from 'react-cookie-consent'
+import { BoxProps, Link, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
 
-import { FlexGrowRow } from './FlexBox'
+import Background from './Background'
+import { ButtonEx } from './ButtonEx'
+import { FlexRow } from './FlexBox'
 
-const CustomCookieConsent: React.FC<CookieConsentProps> = (props) => {
-  const { buttonStyle, style, ...rootProps } = props
-  const theme = useTheme()
-  return (
-    <CookieConsent
-      buttonStyle={{
-        background: theme.palette.secondary.main,
-        color: theme.palette.secondary.contrastText,
-        ...buttonStyle,
-      }}
-      buttonText="Accept"
-      cookieName="CookiesAccepted"
-      expires={150}
-      location="bottom"
-      style={{
-        background: theme.palette.background.paper,
-        color: theme.palette.text.primary,
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'none',
-        justifyContent: 'stretch',
-        ...style,
-      }}
-      {...rootProps}
+const CookieConsent: React.FC<BoxProps> = (props) => {
+  const [accepted, setAccepted] = useState(localStorage.getItem('CookiesAccepted') === 'true')
+
+  const onAcceptClick = () => {
+    localStorage.setItem('CookiesAccepted', 'true')
+    setAccepted(true)
+  }
+
+  return accepted ? null : (
+    <Background
+      display="flex"
+      position="fixed"
+      justifyContent="space-between"
+      paddingY={2}
+      bottom={0}
+      width="100vw"
+      zIndex={1000}
+      {...props}
     >
-      <FlexGrowRow justifyContent="flex-start">
+      <FlexRow marginX={2}>
         <Typography variant="body2">
           This site uses{' '}
           <Link href="https://cookiesandyou.com/" rel="noopener noreferrer" target="_blank">
@@ -41,9 +36,12 @@ const CustomCookieConsent: React.FC<CookieConsentProps> = (props) => {
           </Link>{' '}
           to analyze traffic and for ads measurement purposes.
         </Typography>
-      </FlexGrowRow>
-    </CookieConsent>
+      </FlexRow>
+      <ButtonEx variant="contained" color="secondary" marginX={2} onClick={onAcceptClick}>
+        Accept
+      </ButtonEx>
+    </Background>
   )
 }
 
-export default CustomCookieConsent
+export default CookieConsent
