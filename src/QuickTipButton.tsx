213c74a2dc2f42121f4683519176b5ac25e7d4ff
Arie Trouw <arie.trouw@xy.compay>
2021-08-13 23:07:11 -0700

    QuickTip

--- src/QuickTipButton.tsx
+++ src/QuickTipButton.tsx
@@ -1,5 +1,5 @@
 import { IconButton, IconButtonProps } from '@material-ui/core'
-import React, { MouseEvent, useState } from 'react'
+import { useState } from 'react'
 import { AiOutlineQuestionCircle } from 'react-icons/ai'
 
 import { MessageDialog } from './dialogs'
@@ -7,26 +7,21 @@ import { MessageDialog } from './dialogs'
 const QuickTipButton: React.FC<IconButtonProps> = ({ title, children, ...props }) => {
   const [messageOpen, setMessageOpen] = useState(false)
 
-  const onClick = (event: MouseEvent) => {
-    event.stopPropagation()
-    setMessageOpen(true)
-  }
-
   return (
-    <IconButton onClick={onClick} size="small" {...props}>
-      <AiOutlineQuestionCircle size={16} />
-      {messageOpen ? (
-        <MessageDialog
-          onOk={() => setMessageOpen(false)}
-          onCancel={() => setMessageOpen(false)}
-          onClose={() => setMessageOpen(false)}
-          open={messageOpen}
-          title={title ?? ''}
-        >
-          {children}
-        </MessageDialog>
-      ) : null}
-    </IconButton>
+    <>
+      <IconButton onClick={() => setMessageOpen(true)} size="small" {...props}>
+        <AiOutlineQuestionCircle size={16} />
+      </IconButton>
+      <MessageDialog
+        onOk={() => setMessageOpen(false)}
+        onCancel={() => setMessageOpen(false)}
+        onClose={() => setMessageOpen(false)}
+        open={messageOpen}
+        title={title ?? ''}
+      >
+        {children}
+      </MessageDialog>
+    </>
   )
 }
 
