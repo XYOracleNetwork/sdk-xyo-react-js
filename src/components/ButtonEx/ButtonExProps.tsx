7340fffef9f9ab493f2f7c24495e95e0cb0ce6a4
Arie Trouw <arie.trouw@xy.compay>
2021-08-18 22:28:39 -0700

    to in ButtonEx for history info

--- src/components/ButtonEx/ButtonExProps.tsx
+++ src/components/ButtonEx/ButtonExProps.tsx
@@ -1,10 +1,11 @@
 import { ButtonProps } from '@material-ui/core'
+import type { LocationDescriptor, LocationState } from 'history'
 
 import BoxlikeComponentProps from '../../BoxlikeComponentProps'
 
 interface ButtonExProps extends ButtonProps, BoxlikeComponentProps {
   target?: string
-  to?: string
+  to?: LocationDescriptor<LocationState>
 }
 
 export default ButtonExProps
