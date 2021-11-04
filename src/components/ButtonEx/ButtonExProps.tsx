bf8b4d650d0b3ca56cb2aa65111747df5ea523cc
Arie Trouw <arie.trouw@xylabs.com>
2021-11-04 10:14:08 -0700

    React Router 6 update

--- src/components/ButtonEx/ButtonExProps.tsx
+++ src/components/ButtonEx/ButtonExProps.tsx
@@ -1,12 +1,12 @@
 import { ButtonProps } from '@mui/material'
-import type { LocationDescriptor, LocationState } from 'history'
+import { To } from 'react-router-dom'
 
 import BoxlikeComponentProps from '../../BoxlikeComponentProps'
 import BusyProps from '../../BusyProps'
 
 interface ButtonExProps extends ButtonProps, BoxlikeComponentProps, BusyProps {
   target?: string
-  to?: LocationDescriptor<LocationState>
+  to?: To
 }
 
 export default ButtonExProps
