f4f86378c11af8118be37535903f88afae5aaf71
Arie Trouw <arie.trouw@xylabs.com>
2021-11-15 20:58:23 -0800

    ButtonExBase

--- src/components/ButtonEx/ButtonExTo.tsx
+++ src/components/ButtonEx/ButtonExTo.tsx
@@ -1,7 +1,7 @@
-import { Button } from '@mui/material'
 import React, { MouseEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import ButtonExBase from './ButtonExBase'
 import ButtonExProps from './ButtonExProps'
 
 const ButtonExTo: React.FC<ButtonExProps> = ({ to, toOptions, onClick, ...props }) => {
@@ -13,7 +13,7 @@ const ButtonExTo: React.FC<ButtonExProps> = ({ to, toOptions, onClick, ...props 
     }
   }
 
-  return <Button onClick={localOnClick} {...props} />
+  return <ButtonExBase onClick={localOnClick} {...props} />
 }
 
 export default ButtonExTo
