9c59e4a38e22ecf2fe7864a0b05b01d2fbf6fec3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-01-27 17:48:51 -0500

    basic web3login storybook setup

---
+++ src/components/LoginForms/Web3Login/Web3Login.stories.tsx
@@ -0,0 +1,45 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { XyoMetaMaskConnector } from '@xyo-network/sdk-xyo-client-js'
+import { BrowserRouter } from 'react-router-dom'
+
+import { AuthApiLoader, AuthLoader, AuthServiceId } from '../../../contexts'
+import { Web3Login } from './Web3Login'
+
+const StorybookEntry = {
+  argTypes: {},
+  component: Web3Login,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+  title: 'Auth/Web3LoginProvider',
+} as ComponentMeta<typeof Web3Login>
+
+const Template: ComponentStory<typeof Web3Login> = (args) => {
+  const authServiceList = [
+    {
+      id: AuthServiceId.Web3Wallet,
+      title: 'Web3 wallet',
+    },
+  ]
+  const apiDomain = 'http://localhost:8080'
+  console.log(XyoMetaMaskConnector)
+  return (
+    <BrowserRouter>
+      <AuthLoader authServiceList={authServiceList}>
+        <AuthApiLoader apiDomain={apiDomain}>
+          <Web3Login {...args}></Web3Login>
+        </AuthApiLoader>
+      </AuthLoader>
+    </BrowserRouter>
+  )
+}
+
+const Default = Template.bind({})
+Default.args = {}
+
+export { Default }
+
+// eslint-disable-next-line import/no-default-export
+export default StorybookEntry
