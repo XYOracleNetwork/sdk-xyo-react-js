5e555dbd9ea676cea17869e96e6a32d08acd1773
Arie Trouw <arie.trouw@xylabs.com>
2022-01-31 17:20:58 -0800

    allowing address and archivists to get passed to XyoPanelLoader

--- src/contexts/XyoPanel/Loader.tsx
+++ src/contexts/XyoPanel/Loader.tsx
@@ -9,25 +9,37 @@ import { useEffect, useState } from 'react'
 
 import { XyoPanelContext } from './Context'
 
-export const XyoPanelLoader: React.FC = ({ children }) => {
+export interface XyoPanelLoaderProps {
+  address?: XyoAddress
+  archivists?: XyoArchivistApi[]
+}
+
+const getDefaultArchivists = () => {
+  const archivistConfigs: XyoArchivistApiConfig[] = [
+    {
+      apiDomain: process.env.API_DOMAIN || 'https://api.archivist.xyo.network',
+      archive: 'temp',
+    },
+  ]
+
+  return archivistConfigs.map((config) => {
+    return XyoArchivistApi.get(config)
+  })
+}
+
+export const XyoPanelLoader: React.FC<XyoPanelLoaderProps> = ({
+  address = XyoAddress.random(),
+  archivists = getDefaultArchivists(),
+  children,
+}) => {
   const [panel, setPanel] = useState<XyoPanel>()
 
   useEffect(() => {
-    const archivistConfigs: XyoArchivistApiConfig[] = [
-      {
-        apiDomain: process.env.API_DOMAIN || 'https://api.archivist.xyo.network',
-        archive: 'test',
-      },
-    ]
-
-    const archivists = archivistConfigs.map((config) => {
-      return XyoArchivistApi.get(config)
-    })
     const witnesses = [new XyoSystemInfoWitness()]
 
-    const panel = new XyoPanel({ address: XyoAddress.fromPhrase('test'), archivists, witnesses })
+    const panel = new XyoPanel({ address, archivists, witnesses })
     setPanel(panel)
-  }, [])
+  }, [address, archivists])
 
   return <XyoPanelContext.Provider value={{ panel }}>{panel ? children : null}</XyoPanelContext.Provider>
 }
