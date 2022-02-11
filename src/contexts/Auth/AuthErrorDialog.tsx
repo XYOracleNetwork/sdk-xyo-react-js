a4909e9006d72cfd62cd32ab1ce87a27c840fc51
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-02-11 17:40:08 -0500

    fix web3 auth issue and better error modal (#347)
    * update deploy task
    
    * update json links in new window by default
    
    * update deps to fix metamask challenge issue
    
    * fix web3 auth issue and better error modal

--- src/contexts/Auth/AuthErrorDialog.tsx
+++ src/contexts/Auth/AuthErrorDialog.tsx
@@ -1,17 +1,30 @@
 import { Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material'
 import { ButtonEx, FlexRow } from '@xylabs/sdk-react'
 import { AxiosError } from 'axios'
+import { useEffect, useState } from 'react'
 
 import { AuthActionTypes, AuthDispatch, AuthState } from './AuthStateTypes'
 
-const toAxiosError = (error: Error | AxiosError | undefined) => {
-  return (error as AxiosError)?.isAxiosError ? (error as AxiosError) : undefined
+type AuthError = Error | MetaMaskError | AxiosError | undefined
+interface MetaMaskError {
+  code: number
+  message: string
 }
 
-const isLoginError = (err: Error | AxiosError | undefined) => {
-  const loginUrls = ['/user/wallet/verify/', '/login']
-  if ((err as AxiosError)?.isAxiosError) {
+const isMetaMaskError = (error: AuthError): error is MetaMaskError => {
+  return (error as MetaMaskError)?.message?.includes('MetaMask')
+}
+
+const isAxiosError = (error: AuthError): error is AxiosError => {
+  return (error as AxiosError)?.isAxiosError
+}
+
+const isLoginError = (err: Error | AxiosError | MetaMaskError | undefined) => {
+  const loginUrls = ['/user/wallet/verify/', '/user/wallet/challenge/', '/login']
+  if (isAxiosError(err)) {
     return loginUrls.some((url) => (err as AxiosError)?.config.url?.endsWith(url))
+  } else if (isMetaMaskError(err)) {
+    return true
   } else {
     return false
   }
@@ -23,9 +36,21 @@ interface AuthErrorDialogProps {
 }
 
 const AuthErrorDialog: React.FC<AuthErrorDialogProps> = ({ authState, dispatch }) => {
+  // dialogError decouples the trigger of the modal (authError) from what error message is displayed
+  const [dialogError, setDiaLogError] = useState<AuthError>()
   const { authError } = authState
-  const axiosError = toAxiosError(authError)
-  const message = authError?.message ?? authError?.toString()
+
+  useEffect(() => {
+    if (authError === undefined) {
+      // Delay to allow time for the Dialog to close before updating the error message
+      // Prevents the error text from change to default right before close
+      setTimeout(() => {
+        setDiaLogError(undefined)
+      }, 500)
+    } else {
+      setDiaLogError(authError)
+    }
+  }, [authError, dispatch])
 
   const handleClose = () => {
     dispatch({ payload: { authError: undefined }, type: AuthActionTypes.UpdateAuthError })
@@ -42,7 +67,7 @@ const AuthErrorDialog: React.FC<AuthErrorDialogProps> = ({ authState, dispatch }
         </FlexRow>
         <FlexRow>
           <Typography color="error" variant="body1">
-            {axiosError ? `${message} [${axiosError?.code ?? 'Connection Failure'}]` : `${message}`}
+            {`${dialogError?.message} [${(dialogError as MetaMaskError)?.code ?? 'Request Failure'}]`}
           </Typography>
         </FlexRow>
       </DialogContent>
