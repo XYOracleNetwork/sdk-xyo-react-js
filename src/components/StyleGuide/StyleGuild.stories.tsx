b393742bed016b44f397a54acdf1fd694fad5ba4
Donald Parker <donald.parker@xylabs.com>
2022-02-11 14:36:00 -0800

    add StyleGuide story

---
+++ src/components/StyleGuide/StyleGuild.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { StyleGuide } from './StyleGuide'
+
+
+
+const StorybookEntry = {
+  argTypes: {},
+  component: StyleGuide,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+  title: 'UI/StyleGuild',
+} as ComponentMeta<typeof StyleGuide>
+
+const Template: ComponentStory<typeof StyleGuide> = () => <StyleGuide />
+
+const Default = Template.bind({})
+Default.args = {
+
+}
+Default.decorators = []
+
+export { Default }
+
+// eslint-disable-next-line import/no-default-export
+export default StorybookEntry
