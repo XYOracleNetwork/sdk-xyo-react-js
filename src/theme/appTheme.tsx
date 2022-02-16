34ef90b289b907a362810a7030bfc3aa650bcc92
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-02-16 14:28:23 -0500

    make clamp variant responsive

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -6,17 +6,23 @@ const appTheme: ThemeOptions = {
       styleOverrides: {
         root: ({ theme, ownerState }) => {
           const { clamped } = ownerState
-          const themeSpacing = theme.spacing(clamped as number)
-          const themeSpacing2x = theme.spacing((clamped as number) * 2)
-          const themeSpacing3x = theme.spacing((clamped as number) * 3)
 
           if (clamped) {
+            const factor = parseInt(clamped as string, 10)
+            const themeSpacing = theme.spacing(factor)
+            const themeSpacing2x = theme.spacing(factor * 2)
+            const themeSpacing3x = theme.spacing(factor * 3)
+            const isSm = theme.breakpoints.down('sm')
+
             return {
+              [isSm]: {
+                width: `clamp(1%, 100% - ${themeSpacing2x}, 100vw - ${themeSpacing3x})`,
+              },
               overflow: 'hidden',
               padding: themeSpacing,
               textOverflow: 'ellipsis',
               whiteSpace: 'nowrap',
-              width: `clamp(1%, 100% - ${themeSpacing2x}, 100vw - ${themeSpacing3x})`,
+              width: `clamp(1%, 100%, 100vw - ${themeSpacing3x})`,
             }
           }
         },
