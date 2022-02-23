34d6207ad60ef51b53d8cc526a1ef0d372c8c76d
Donald Parker <donald.parker@xylabs.com>
2022-02-23 08:59:45 -0800

    update webThemeOptions

--- src/theme/webThemeOptions.tsx
+++ src/theme/webThemeOptions.tsx
@@ -1,8 +1,34 @@
 import { ThemeOptions } from '@mui/material'
 import merge from 'lodash/merge'
 
+import { fontFamilyPrimary } from './fontFamily'
 import { themeOptions } from './themeOptions'
 
-const partialWebThemeOptions: ThemeOptions = {}
+const partialWebThemeOptions: ThemeOptions = {
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 4,
+        },
+      },
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  typography: {
+    fontFamily: fontFamilyPrimary,
+    h1: {
+      fontFamily: fontFamilyPrimary,
+    },
+    h2: {
+      fontFamily: fontFamilyPrimary,
+    },
+    h3: {
+      fontFamily: fontFamilyPrimary,
+    },
+  },
+}
 
 export const webThemeOptions = merge({}, themeOptions, partialWebThemeOptions)
