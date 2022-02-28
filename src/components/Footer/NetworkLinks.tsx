c726a50d24dc5008b2004073421b905d09c4d47f
Arie Trouw <arie.trouw@xylabs.com>
2022-02-28 11:46:24 -0800

    footer links updated

--- src/components/Footer/NetworkLinks.tsx
+++ src/components/Footer/NetworkLinks.tsx
@@ -5,8 +5,9 @@ import { FooterLinks } from './Links'
 
 export const NetworkLinks: React.FC<FlexBoxProps> = (props) => (
   <FooterLinks title="Network" {...props}>
-    <FooterLink href="https://xyo.network/network/components">Components</FooterLink>
-    <FooterLink href="https://xyo.network/network/protocol-features">Protocol Features</FooterLink>
-    <FooterLink href="https://xyo.network/network/vocabulary">Vocabulary</FooterLink>
+    <FooterLink href="https://xyo.network/network/network">Overview</FooterLink>
+    <FooterLink href="https://xyo.network/network/bound-witness">Bound Witness</FooterLink>
+    <FooterLink href="https://xyo.network/network/proof-of-origin">Proof Of Origin</FooterLink>
+    <FooterLink href="https://xyo.network/papers">White Paper</FooterLink>
   </FooterLinks>
 )
