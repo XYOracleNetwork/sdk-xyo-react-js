42b060fd95b37e876bfd5c86182e609229ee40a4
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-03-03 22:11:01 -0500

    clean up for readability

--- src/components/Auth/AuthStatusIndicator.tsx
+++ src/components/Auth/AuthStatusIndicator.tsx
@@ -2,17 +2,40 @@ import { Error } from '@mui/icons-material'
 import { ButtonBase, colors } from '@mui/material'
 import { ellipsize, EthAddress } from '@xylabs/sdk-js'
 import { FlexBoxProps, FlexRow, Identicon } from '@xylabs/sdk-react'
-import { useEffect, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { FaUserCircle } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 
-import { useAuthState } from '../../contexts'
+import { AuthState, useAuthState } from '../../contexts'
 
 const formatAccount = (account: string) => {
   if (account.startsWith('0x')) {
     return EthAddress.fromString(account)?.toShortString()
   } else {
     return ellipsize(account, 4)
+  }
+}
+
+const formatIconHint = (
+  authState: AuthState | undefined,
+  setIconHint: Dispatch<SetStateAction<string | undefined>>
+) => {
+  if (authState) {
+    // logged in and reAuth is false
+    if (authState.loggedInAccount && !authState.reAuthenticate) {
+      setIconHint(`Signed In as ${authState.loggedInAccount}`)
+    }
+    // not logged in and reAuth is true
+    if (!authState.loggedInAccount && authState.reAuthenticate) {
+      setIconHint('Please login again')
+    }
+    // not logged in and reAuth is false
+    if (!authState.loggedInAccount && !authState.reAuthenticate) {
+      setIconHint('Signed Out')
+    }
+  } else {
+    // if authState isn't present, assume signed out
+    setIconHint('Signed Out')
   }
 }
 
@@ -26,19 +49,7 @@ export const AuthStatusIndicator: React.FC<FlexBoxProps> = ({ onClick, ...props 
   const iconColor = authState?.loggedInAccount ? colors.lightBlue[50] : colors.grey[500]
 
   useEffect(() => {
-    if (authState) {
-      if (authState.loggedInAccount && !authState.reAuthenticate) {
-        setIconHint(`Signed In as ${authState.loggedInAccount}`)
-      }
-      if (!authState.loggedInAccount && authState.reAuthenticate) {
-        setIconHint('Please login again')
-      }
-      if (!authState.loggedInAccount && !authState.reAuthenticate) {
-        setIconHint('Signed Out')
-      }
-    } else {
-      setIconHint('Signed Out')
-    }
+    formatIconHint(authState, setIconHint)
   }, [authState, authState?.loggedInAccount, authState?.reAuthenticate])
 
   useEffect(() => {
