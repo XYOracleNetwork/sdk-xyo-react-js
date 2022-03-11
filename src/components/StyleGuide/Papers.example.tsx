473223809f0523a464baee10747ea4e076387f67
Arie Trouw <arie.trouw@xylabs.com>
2022-03-10 18:55:38 -0800

    updated example

--- src/components/StyleGuide/Papers.example.tsx
+++ src/components/StyleGuide/Papers.example.tsx
@@ -1,18 +1,5 @@
-import { Paper, styled, Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
 import { FlexCol, FlexRow } from '@xylabs/sdk-react'
-
-const Papered = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  color: theme.palette.text.secondary,
-  height: 60,
-  lineHeight: '60px',
-  textAlign: 'center',
-}))
-
-const PaperedSecondary = styled(Papered)(({ theme }) => ({
-  background: theme.palette.secondary.main,
-  color: 'white',
-}))
 
 export const PapersExample: React.FC = ({ ...props }) => {
   return (
@@ -21,12 +8,11 @@ export const PapersExample: React.FC = ({ ...props }) => {
       <FlexRow alignItems="stretch">
         {Array.from({ length: 5 }, () => undefined).map((_, index) => (
           <FlexCol key={`paper-${index}`} marginRight={2}>
-            <Papered key={`paper-${index}`} square elevation={index * 2}>
-              Elevation-{index * 3}
-            </Papered>
+            <Paper key={`paper-${index}`} elevation={index * 2} color="secondary">
+              <Typography margin={1}>Elevation-{index * 3}</Typography>
+            </Paper>
           </FlexCol>
         ))}
-        <PaperedSecondary>Secondary Background</PaperedSecondary>
       </FlexRow>
     </FlexCol>
   )
