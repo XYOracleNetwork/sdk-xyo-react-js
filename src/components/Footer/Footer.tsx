b263142ec3d8ddb670e4e3cb95d5fe5116404a83
Arie Trouw <arie.trouw@xylabs.com>
2022-03-18 16:16:52 -0700

    Footer dynamicheight ability

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { Container, Grid } from '@mui/material'
 import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/sdk-react'
+import { useState } from 'react'
 
 import { Copyright } from './Copyright'
 import { DeveloperLinks } from './DeveloperLinks'
@@ -9,33 +10,50 @@ import { SocialLinks } from './SocialLinks'
 import { SupportLinks } from './SupportLinks'
 import { XyoTokens } from './XyoTokens'
 
-export const Footer: React.FC<FlexBoxProps> = (props) => {
+export interface FooterProps extends FlexBoxProps {
+  dynamicHeight?: boolean
+}
+
+export const Footer: React.FC<FooterProps> = ({ dynamicHeight = false, ...props }) => {
+  const [hovering, setHovering] = useState(false)
   return (
-    <FlexCol alignItems="stretch" component="footer" {...props}>
-      <FlexRow alignItems="flex-start">
-        <Container>
-          <Grid container justifyContent="space-between" alignItems="flex-start">
-            <Grid item xs={12} md={2}>
-              <SocialLinks alignItems="flex-start" />
+    <FlexCol
+      onMouseEnter={() => {
+        setHovering(true)
+      }}
+      onMouseLeave={() => {
+        setHovering(false)
+      }}
+      alignItems="stretch"
+      component="footer"
+      {...props}
+    >
+      {hovering || !dynamicHeight ? (
+        <FlexRow alignItems="flex-start">
+          <Container>
+            <Grid container justifyContent="space-between" alignItems="flex-start">
+              <Grid item xs={12} md={2}>
+                <SocialLinks alignItems="flex-start" />
+              </Grid>
+              <Grid item xs={6} md={2}>
+                <NetworkLinks alignItems="flex-start" />
+              </Grid>
+              <Grid item xs={6} md={2}>
+                <XyoTokens alignItems="flex-start" />
+              </Grid>
+              <Grid item xs={6} md={2}>
+                <DeveloperLinks alignItems="flex-start" />
+              </Grid>
+              <Grid item xs={6} md={2}>
+                <MoreLinks alignItems="flex-start" />
+              </Grid>
+              <Grid item xs={6} md={2}>
+                <SupportLinks alignItems="flex-start" />
+              </Grid>
             </Grid>
-            <Grid item xs={6} md={2}>
-              <NetworkLinks alignItems="flex-start" />
-            </Grid>
-            <Grid item xs={6} md={2}>
-              <XyoTokens alignItems="flex-start" />
-            </Grid>
-            <Grid item xs={6} md={2}>
-              <DeveloperLinks alignItems="flex-start" />
-            </Grid>
-            <Grid item xs={6} md={2}>
-              <MoreLinks alignItems="flex-start" />
-            </Grid>
-            <Grid item xs={6} md={2}>
-              <SupportLinks alignItems="flex-start" />
-            </Grid>
-          </Grid>
-        </Container>
-      </FlexRow>
+          </Container>
+        </FlexRow>
+      ) : null}
       <FlexRow>
         <Container>
           <Copyright />
