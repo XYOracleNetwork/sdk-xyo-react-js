401af8dbfbde5b6c696b7d699ccd478ea0e4c021
Arie Trouw <arie.trouw@xylabs.com>
2022-03-21 11:45:11 -0700

    Not asserting on no panel

--- src/contexts/Panel/useXyoPanel.ts
+++ src/contexts/Panel/useXyoPanel.ts
@@ -1,9 +1,8 @@
-import { assertEx } from '@xylabs/sdk-js'
 import { useContext } from 'react'
 
 import { XyoPanelContext } from './Context'
 
 export const useXyoPanel = () => {
   const { panel, history, progress, reportingErrors, status } = useContext(XyoPanelContext)
-  return { history, panel: assertEx(panel, 'XyoPanel not initialized'), progress, reportingErrors, status }
+  return { history, panel, progress, reportingErrors, status }
 }
