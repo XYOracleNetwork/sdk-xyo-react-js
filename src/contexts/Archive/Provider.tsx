8f18c1fa7e8bb696d9eb6965d2e71ffddaf39b03
Arie Trouw <arie.trouw@xylabs.com>
2022-03-21 11:32:38 -0700

    ArchiveProvider children handler

--- src/contexts/Archive/Provider.tsx
+++ src/contexts/Archive/Provider.tsx
@@ -6,7 +6,7 @@ export interface ArchiveProviderProps {
   defaultArchive?: string
 }
 
-export const ArchiveProvider: React.FC<ArchiveProviderProps> = ({ defaultArchive = 'temp', ...props }) => {
+export const ArchiveProvider: React.FC<ArchiveProviderProps> = ({ children, defaultArchive = 'temp', ...props }) => {
   const [archive, setArchive] = useState(defaultArchive)
-  return <ArchiveContext.Provider value={{ archive, setArchive, ...props }} />
+  return <ArchiveContext.Provider value={{ archive, setArchive, ...props }}>{children}</ArchiveContext.Provider>
 }
