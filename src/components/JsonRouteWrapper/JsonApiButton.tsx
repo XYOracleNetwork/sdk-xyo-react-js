51398736782a942383ce6b74de014a76e4f2c1de
Arie Trouw <arie.trouw@xylabs.com>
2022-03-24 10:03:41 -0700

    fixed url settings

--- src/components/JsonRouteWrapper/JsonApiButton.tsx
+++ src/components/JsonRouteWrapper/JsonApiButton.tsx
@@ -2,9 +2,17 @@ import { ButtonEx, ButtonExProps } from '@xylabs/sdk-react'
 import { useSearchParams } from 'react-router-dom'
 
 export const JsonApiButton: React.FC<ButtonExProps> = (props) => {
-  const [, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
   return (
-    <ButtonEx marginX={2} variant="outlined" onClick={() => setSearchParams({ json: 'true' })} {...props}>
+    <ButtonEx
+      marginX={2}
+      variant="outlined"
+      onClick={() => {
+        searchParams.set('json', 'true')
+        setSearchParams(searchParams)
+      }}
+      {...props}
+    >
       JSON
     </ButtonEx>
   )
