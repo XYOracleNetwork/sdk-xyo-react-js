d045476f3cde7fdf30e0ed972b675f7e588fd21c
Jordan Trouw <jordan.trouw@gmail.com>
2022-03-24 15:59:12 -0700

    added discord link to XYO Footer.

--- src/components/Footer/SocialLinks.tsx
+++ src/components/Footer/SocialLinks.tsx
@@ -1,5 +1,6 @@
 import { Facebook, Instagram, LinkedIn, Reddit, Telegram, Twitter, YouTube } from '@mui/icons-material'
 import { FlexBoxProps, FlexRow } from '@xylabs/sdk-react'
+import { FaDiscord } from 'react-icons/fa'
 
 import { FooterLink } from './Link'
 import { FooterLinks } from './Links'
@@ -28,6 +29,9 @@ export const SocialLinks: React.FC<FlexBoxProps> = (props) => {
         </FooterLink>
         <FooterLink href="https://www.linkedin.com/company/officialxyo/">
           <LinkedIn />
+        </FooterLink>
+        <FooterLink href="https://discord.com/channels/935586624392298547/935586625101103227">
+          <FaDiscord />
         </FooterLink>
       </FlexRow>
     </FooterLinks>
