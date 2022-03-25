66ff230e7b504cbcecc5a323fe49daf2e912dd5d
Arie Trouw <arie.trouw@xylabs.com>
2022-03-25 13:55:58 -0700

    lint

--- src/contexts/Auth/index.ts
+++ src/contexts/Auth/index.ts
@@ -1,6 +1,6 @@
 export * from './ActionType'
 export * from './ApiLogger'
-export * from './Provider'
 export * from './Context'
 export * from './hooks'
+export * from './Provider'
 export * from './State'
