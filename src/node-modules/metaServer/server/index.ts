dd961e021aff497ba0ab6f625ba791b4f86781db
JoelBCarter <joelbcarter@gmail.com>
2022-04-11 17:10:11 -0500

    Add `asyncHandler` from Express SDK
    So server continues if error

--- src/node-modules/metaServer/server/index.ts
+++ src/node-modules/metaServer/server/index.ts
@@ -1,3 +1,4 @@
+import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
 import express from 'express'
 import { readFileSync } from 'fs'
 import { readFile } from 'fs/promises'
@@ -18,16 +19,19 @@ const server = (port = 80) => {
     console.warn('No config found!  Please create a config at meta.json file in your ./build folder')
   }
 
-  app.get('*', async (req, res) => {
-    const adjustedPath = extname(req.path).length > 0 ? join(req.path) : join(req.path, 'index.html')
-    if (config && extname(adjustedPath) === '.html') {
-      const html = await readFile(join(dirName, 'index.html'), { encoding: 'utf-8' })
-      const updatedHtml = await setHtmlMetaData(`${req.protocol}://${req.headers.host}${req.url}`, html, config)
-      res.send(updatedHtml)
-    } else {
-      res.send(await readFile(join(dirName, adjustedPath)))
-    }
-  })
+  app.get(
+    '*',
+    asyncHandler(async (req, res) => {
+      const adjustedPath = extname(req.path).length > 0 ? join(req.path) : join(req.path, 'index.html')
+      if (config && extname(adjustedPath) === '.html') {
+        const html = await readFile(join(dirName, 'index.html'), { encoding: 'utf-8' })
+        const updatedHtml = await setHtmlMetaData(`${req.protocol}://${req.headers.host}${req.url}`, html, config)
+        res.send(updatedHtml)
+      } else {
+        res.send(await readFile(join(dirName, adjustedPath)))
+      }
+    })
+  )
 
   const server = app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
