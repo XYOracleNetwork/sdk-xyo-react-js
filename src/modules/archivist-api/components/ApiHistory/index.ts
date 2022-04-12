906c10e1882b3f0477d29730cb423e0865e91ada
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-04-12 14:36:49 -0400

    deprecate old api logger and style new one (#402)
    * deprecate unused api logger context
    
    * add api logger components to use new archivist api context
    
    * add styled api history
    
    * better code hub fixes
    
    * extract history building to new component
    
    * fix typescript error

---
+++ src/modules/archivist-api/components/ApiHistory/index.ts
@@ -0,0 +1,2 @@
+export * from './ApiCallEntry'
+export * from './ApiHistory'
--- src/modules/auth/components/index.ts
+++ src/modules/auth/components/index.ts
@@ -1,6 +1,5 @@
 export * from './AuthErrors'
 export * from './AuthFooter'
-export * from './AuthLogs'
 export * from './AuthStatusIconButton'
 export * from './AuthThemeExtender'
 export * from './debug'
