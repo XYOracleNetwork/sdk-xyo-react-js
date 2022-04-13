db6556e8e58c8d970e0f64f9e4b8c49362a7d57e
JoelBCarter <joelbcarter@gmail.com>
2022-04-13 17:04:16 -0500

    Use parse function from lib

--- bin/start-meta.js
+++ bin/start-meta.js
@@ -1,4 +1,5 @@
+const { tryParseInt } = require("@xylabs/sdk-api-express-ecs")
 const { metaServer } = require("../dist/node")
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 8083 
+const port = tryParseInt(process.env.PORT) || 8083 
 metaServer(port)
