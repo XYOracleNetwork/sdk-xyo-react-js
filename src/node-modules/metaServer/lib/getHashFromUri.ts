e68c53f44dad8e9812a4d6d7076a45b6452701f0
JoelBCarter <joelbcarter@gmail.com>
2022-04-15 07:40:51 -0500

    Add tests for adjusted path

--- src/node-modules/metaServer/lib/getHashFromUri.ts
+++ src/node-modules/metaServer/lib/getHashFromUri.ts
@@ -1,8 +1,10 @@
+const hashLength = 64
+// TODO: Replace with regex or something more exact than length
 export const getHashFromUri = (uri: string): string | undefined => {
   const uriParts = uri.split('/').map((p) => p.split('?')[0])
   let hash: string | undefined = undefined
   uriParts.forEach((part) => {
-    if (part.length === 64) {
+    if (part?.length === hashLength) {
       hash = part
     }
   })
