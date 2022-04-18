cd10de357df43f08bbe124a2eb80fa88007125f4
JoelBCarter <joelbcarter@gmail.com>
2022-04-18 18:05:47 -0500

    Hide server output during test

--- src/node-modules/metaServer/middleware/archivistBlock/archivistBlock.spec.ts
+++ src/node-modules/metaServer/middleware/archivistBlock/archivistBlock.spec.ts
@@ -1,3 +1,4 @@
+import { tryParseInt } from '@xylabs/sdk-api-express-ecs'
 import { readFile } from 'fs/promises'
 import { Server } from 'http'
 import { join } from 'path'
@@ -14,6 +15,9 @@ describe('archivistBlock', () => {
   let server: Server
   let agent: SuperTest<Test>
   beforeAll(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {
+      // Disable console log from real server starting up for these tests
+    })
     const baseDir = __dirname
     const [serverA, agentA] = getAgent(baseDir, testServerPort)
     server = serverA
@@ -34,11 +38,13 @@ describe('archivistBlock', () => {
     expect(headers['content-type']).toBe('text/html; charset=utf-8')
 
     expect(headers['content-length']).toBeTruthy()
+    const contentLength = tryParseInt(headers['content-length'])
+    expect(contentLength).toBeDefined()
     // Read file in original HTML file
     const originalFile = await readFile(join(__dirname, 'index.html'))
     // HTTP Content-Length header should reflect that modified file is
     // bigger than original after modification
-    expect(headers['content-length']).toBeGreaterThan(originalFile.byteLength)
+    expect(contentLength).toBeGreaterThan(originalFile.byteLength)
 
     // Validate HTML document
     const actual = response.text
