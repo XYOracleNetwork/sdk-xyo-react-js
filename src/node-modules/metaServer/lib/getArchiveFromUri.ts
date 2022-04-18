6cc6ab906456487a0a55c9662a2aed9ffcf2d544
JoelBCarter <joelbcarter@gmail.com>
2022-04-18 07:30:33 -0500

    Remove comments

--- src/node-modules/metaServer/lib/getArchiveFromUri.ts
+++ src/node-modules/metaServer/lib/getArchiveFromUri.ts
@@ -1,6 +1,5 @@
 const archiveRegex = /\/(?:archive)\/([\w]+)/
 
 export const getArchiveFromUri = (uri: string): string | undefined => {
-  // TODO: Parse archive from real URI
   return uri.match(archiveRegex)?.[1]
 }
