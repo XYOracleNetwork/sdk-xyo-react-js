0dd054cc496d2dd0682dd3c46c7bb8d7ce0a532f
JoelBCarter <joelbcarter@gmail.com>
2022-04-18 14:44:12 -0500

    Use serve-static middleware

--- src/node-modules/metaServer/middleware/proxyOriginal/proxyOriginal.ts
+++ src/node-modules/metaServer/middleware/proxyOriginal/proxyOriginal.ts
@@ -1,15 +1,14 @@
-import { asyncHandler } from '@xylabs/sdk-api-express-ecs'
-import { readFile } from 'fs/promises'
-import { join } from 'path'
+import serveStatic from 'serve-static'
 
-import { getAdjustedPath } from '../../lib'
 import { ApplicationMiddlewareOptions, MountPathAndMiddleware } from '../../types'
 
 const getHandler = (baseDir: string) => {
-  return asyncHandler(async (req, res) => {
-    const adjustedPath = getAdjustedPath(req)
-    res.send(await readFile(join(baseDir, adjustedPath)))
-  })
+  return serveStatic(baseDir, {})
+
+  // return asyncHandler(async (req, res) => {
+  //   const adjustedPath = getAdjustedPath(req)
+  //   res.send(await readFile(join(baseDir, adjustedPath)))
+  // })
 }
 
 /**
