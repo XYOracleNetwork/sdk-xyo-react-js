5be1460597e225f171595a194e8faf0d7bcbf394
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-19 16:50:41 -0500

    Add script to generate .env

--- bin/docker-build.js
+++ bin/docker-build.js
@@ -1,15 +1,10 @@
 const { execSync } = require('child_process')
-
-const safeExit = (func) => {
-  try {
-    func()
-  } catch (ex) {
-    process.exit(ex.code)
-  }
-}
+const { safeExit } = require('./safeExit')
 
 safeExit(() => {
   console.log(`Docker build [${process.cwd()}]`)
+  // Append the current environment to the .env (mostly for build server)
+  execSync('printenv >> .env', { stdio: 'inherit' })
   // This is the path to the Dockerfile for any project which depends on this
   // library relative to the root of the dependent
   execSync('docker build -f ./node_modules/@xyo-network/sdk-xyo-react/Dockerfile .', { stdio: 'inherit' })
