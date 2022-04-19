8469031acee5a7f1798e09f8c06d7e265e68c3d7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-19 17:30:55 -0500

    Add dotenv generation

--- bin/docker-build.js
+++ bin/docker-build.js
@@ -3,8 +3,6 @@ const { safeExit } = require('./safeExit')
 
 safeExit(() => {
   console.log(`Docker build [${process.cwd()}]`)
-  // Append the current environment to the .env (mostly for build server)
-  execSync('printenv >> .env', { stdio: 'inherit' })
   // This is the path to the Dockerfile for any project which depends on this
   // library relative to the root of the dependent
   execSync('docker build -f ./node_modules/@xyo-network/sdk-xyo-react/Dockerfile .', { stdio: 'inherit' })
