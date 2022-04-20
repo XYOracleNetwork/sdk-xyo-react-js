68834dc85d6ca26ae1d1dd5df8ff334a5d32415e
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-19 19:55:41 -0500

    Documentation

--- bin/getFlags.js
+++ bin/getFlags.js
@@ -1,5 +1,10 @@
 const yargs = require('yargs')
 
+/**
+ * Determines the flags for the supplied command (without
+ * the command itself)
+ * @returns The flags supplied with the command invocation
+ */
 const getFlags = () => {
   const { _, $0, ...flags } = yargs.parserConfiguration({
     "camel-case-expansion": false,
@@ -7,6 +12,16 @@ const getFlags = () => {
   return flags
 }
 
+/**
+ * Return the string corresponding to the CLI flag
+ * name/value tuple. This method handles the case
+ * for flags that were supplied multiple times by
+ * generating the appropriate string for each
+ * supplied flag.
+ * @param {string} name Flag name
+ * @param {string|string[]} value Flag value(s) 
+ * @returns 
+ */
 const getFlagFromProperty = (name, value) => {
   const dash = name.length === 1 ? '-' : '--'
   return Array.isArray(value) ?
@@ -14,6 +29,11 @@ const getFlagFromProperty = (name, value) => {
     `${dash}${name} ${value}`
 }
 
+/**
+ * Parses the supplied CLI flags and returns them as a string
+ * to be fed to subsequent commands
+ * @returns The supplied CLI flags as a string
+ */
 const getFlagsString = () => {
   const flags = getFlags()
   const resp = Object
