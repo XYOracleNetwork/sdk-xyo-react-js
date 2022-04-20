827d2baeda2ffb9d11a49866d09ec84118b8e91c
Arie Trouw <arie.trouw@xylabs.com>
2022-04-20 13:02:24 -0700

    Logo Spacing

---
+++ src/modules/appbar/components/Toolbar/Context/XyoLogoLinkEx.tsx
@@ -0,0 +1,35 @@
+import { Typography, useTheme } from '@mui/material'
+import { FlexRow, LinkEx, LinkExProps } from '@xylabs/sdk-react'
+
+import { XyoLogo } from './XyoLogo'
+
+export interface XyoLogoLinkExProps extends LinkExProps {
+  version?: boolean | string
+}
+
+export const XyoLogoLinkEx: React.FC<XyoLogoLinkExProps> = ({ to = '/', version = false, ...props }) => {
+  const theme = useTheme()
+  return (
+    <LinkEx to={to} {...props}>
+      <FlexRow paddingX="4px">
+        <XyoLogo height="40" width="43" />
+        {version ? (
+          <Typography
+            position="absolute"
+            borderRadius={1}
+            right={6}
+            color={theme.palette.getContrastText(theme.palette.text.primary)}
+            bottom={0}
+            bgcolor={theme.palette.text.primary}
+            paddingX="2px"
+            lineHeight={1}
+            variant="caption"
+            border={`1px ${theme.palette.getContrastText(theme.palette.primary.main)} solid`}
+          >
+            {typeof version === 'string' ? version : '2.0'}
+          </Typography>
+        ) : null}
+      </FlexRow>
+    </LinkEx>
+  )
+}
