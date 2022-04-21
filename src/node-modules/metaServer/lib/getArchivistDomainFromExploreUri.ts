937655f42f1c9f185c30019ab7ca804a9faa3ac9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-21 15:32:47 -0500

    Always return an archivist

--- src/node-modules/metaServer/lib/getArchivistDomainFromExploreUri.ts
+++ src/node-modules/metaServer/lib/getArchivistDomainFromExploreUri.ts
@@ -8,11 +8,11 @@ export const networkToArchivistUri: Record<Network, string> = {
   main: 'https://api.archivist.xyo.network',
 }
 
-export const getArchivistDomainFromExploreUri = (uri: string): string | undefined => {
+export const getArchivistDomainFromExploreUri = (uri: string): string => {
   try {
     const parsed = new URL(uri)
     const queryParams = parse(parsed.search.substring(1))
-    const network: Network = queryParams?.network || 'main'
+    const network: Network = (queryParams?.network as Network) || 'main'
     return networkToArchivistUri[network] || networkToArchivistUri['main']
   } catch (error) {
     return networkToArchivistUri['main']
