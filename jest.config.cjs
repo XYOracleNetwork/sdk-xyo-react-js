0817aec540b44d3ac1940c8ed7addb65d4f68231
Arie Trouw <arie.trouw@xylabs.com>
2022-04-27 12:51:19 -0700

    using new sdk-xyo-js (monorepo)

--- jest.config.cjs
+++ jest.config.cjs
@@ -16,7 +16,7 @@ const generateJestConfig = ({ esModules }) => {
     testTimeout: 20000,
     transform: {
       [`(${esModuleslist}).+\\.js$`]: 'babel-jest',
-      '^.+\\.tsx?$': 'ts-jest',
+      '^.+\\.tsx?$': 'ts-jest/legacy',
     },
     transformIgnorePatterns: [`./node_modules/(?!${esModuleslist})`],
   }
