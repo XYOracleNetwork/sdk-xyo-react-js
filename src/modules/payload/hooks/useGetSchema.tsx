44bf2a02d0f64e05f7fa7cb255f793e7dff13119
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-05-01 08:02:34 -0400

    code cleanup

--- src/modules/payload/hooks/useGetSchema.tsx
+++ src/modules/payload/hooks/useGetSchema.tsx
@@ -1,55 +1,53 @@
 import { useAsyncEffect } from '@xylabs/sdk-react'
-import { Huri, XyoApiError, XyoPayload, XyoPayloadBuilder, XyoSchemaCache } from '@xyo-network/sdk-xyo-client-js'
-import { AnySchema } from 'ajv'
-import { useState } from 'react'
+import { Huri, XyoApiError, XyoPayload, XyoPayloadBuilder, XyoSchemaCache, XyoSchemaCacheEntry } from '@xyo-network/sdk-xyo-client-js'
+import { useEffect, useState } from 'react'
 
 /**
- * Gets a Huri from a schema
+ * Gets a Huri and schema payload from a schema string
  */
 const useGetSchemaPayload = (schema?: string) => {
   const [notFound, setNotFound] = useState(false)
   const [apiError, setApiError] = useState<XyoApiError>()
-  const [schemaDefinition, setSchemaDefinition] = useState<AnySchema>()
+  const [schemaCacheEntry, setSchemaCacheEntry] = useState<XyoSchemaCacheEntry>()
   const [schemaPayload, setSchemaPayload] = useState<XyoPayload | null | undefined>()
   const [schemaHuri, setSchemaHuri] = useState<Huri | undefined>()
-  const [searchComplete, setSearchComplete] = useState(false)
 
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
     async (mounted) => {
-      if (searchComplete === false && schema) {
+      if (schema && !notFound && !schemaCacheEntry) {
         try {
-          // check network context for uri
-          const fetchedSchema = await XyoSchemaCache.instance.get(schema)
+          const schemaCacheEntry = await XyoSchemaCache.instance.get(schema)
 
           if (mounted()) {
-            if (fetchedSchema === undefined || fetchedSchema === null) {
+            if (schemaCacheEntry === undefined || schemaCacheEntry === null) {
               setNotFound(true)
             } else {
-              setSchemaDefinition(schemaDefinition)
-
-              const { huri, payload } = fetchedSchema
-              const schemaPayload = new XyoPayloadBuilder(payload).fields(payload).build()
-
-              setSchemaPayload(schemaPayload)
-              setSchemaHuri(huri)
+              setSchemaCacheEntry(schemaCacheEntry)
             }
-
-            setSearchComplete(true)
           }
         } catch (e) {
           console.error(e)
           if (mounted()) {
             setApiError(e as XyoApiError)
-            setSearchComplete(true)
           }
         }
       }
     },
-    [apiError, schema, schemaDefinition, searchComplete]
+    [apiError, notFound, schema, schemaCacheEntry]
   )
 
-  return { apiError, notFound, schemaDefinition, schemaHuri, schemaPayload }
+  useEffect(() => {
+    if (schemaCacheEntry) {
+      const { huri, payload } = schemaCacheEntry
+      const schemaPayload = new XyoPayloadBuilder(payload).fields(payload).build()
+
+      setSchemaPayload(schemaPayload)
+      setSchemaHuri(huri)
+    }
+  }, [schemaCacheEntry])
+
+  return { apiError, notFound, schemaHuri, schemaPayload }
 }
 
 export { useGetSchemaPayload }
