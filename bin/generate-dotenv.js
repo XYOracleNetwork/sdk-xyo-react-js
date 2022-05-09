89ef4c478697b6465903eaaaa5931706d67f7e8e
Arie Trouw <arie.trouw@xylabs.com>
2022-05-09 12:58:22 -0700

    moved meta-server to own repo

--- bin/generate-dotenv.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process')
-const { safeExit } = require('./safeExit')
-
-safeExit(() => {
-  console.log(`Generate .env [${process.cwd()}]`)
-  // Append the current environment to the .env (mostly for build server)
-  execSync('printenv >> .env', { stdio: 'inherit' })
-})
--- bin/safeExit.js
+++
@@ -1,9 +0,0 @@
-const safeExit = (func) => {
-  try {
-    func()
-  } catch (ex) {
-    process.exit(ex.code)
-  }
-}
-
-module.exports = { safeExit }
