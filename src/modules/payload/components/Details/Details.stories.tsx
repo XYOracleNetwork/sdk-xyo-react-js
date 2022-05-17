61f61824ce727d85f0860e3212bd1e684ffbf26d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-05-17 10:03:04 -0400

    add archive link and change detail link colors

--- src/modules/payload/components/Details/Details.stories.tsx
+++ src/modules/payload/components/Details/Details.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { appThemeDecorator, samplePayload, samplePayloadFromBuilder } from '../../../.storybook'
 import { PayloadDetails } from './Details'
@@ -14,7 +15,14 @@ const StorybookEntry = {
   title: 'Payload/Details',
 } as ComponentMeta<typeof PayloadDetails>
 
-const Template: ComponentStory<typeof PayloadDetails> = (args) => <PayloadDetails {...args}></PayloadDetails>
+const Template: ComponentStory<typeof PayloadDetails> = (args) => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="temp" element={<h1>Successfully navigated to archivePath</h1>} />
+      <Route path="*" element={<PayloadDetails {...args}></PayloadDetails>}></Route>
+    </Routes>
+  </BrowserRouter>
+)
 
 const Default = Template.bind({})
 Default.args = {}
@@ -28,7 +36,11 @@ const WithBuilderSample = Template.bind({})
 WithBuilderSample.args = { payload: samplePayloadFromBuilder }
 WithBuilderSample.decorators = [appThemeDecorator]
 
-export { Default, WithBuilderSample, WithSample }
+const WithArchiveLink = Template.bind({})
+WithArchiveLink.args = { payload: samplePayloadFromBuilder, payloadMetaDetailsProps: { archivePath: 'temp' } }
+WithArchiveLink.decorators = [appThemeDecorator]
+
+export { Default, WithArchiveLink, WithBuilderSample, WithSample }
 
 // eslint-disable-next-line import/no-default-export
 export default StorybookEntry
