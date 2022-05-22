74fd076bebcc4a36eef2301b3f6c8bcd84616115
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-05-22 18:39:19 -0400

    add utility hook to push content above footer

---
+++ src/modules/utils/useFooterOffsetResizer.tsx
@@ -0,0 +1,33 @@
+import { MutableRefObject, RefObject, useLayoutEffect } from 'react'
+
+interface OffsetResizerArgs {
+  targetElementRef: MutableRefObject<HTMLElement | undefined>
+  observedElementRef: MutableRefObject<HTMLElement | undefined>
+  condition?: boolean
+}
+
+/**
+ * Useful for calculating the height of one element to adjust another
+ */
+
+const useFooterOffsetResizer = ({ targetElementRef, observedElementRef, condition = true }: OffsetResizerArgs) => {
+  useLayoutEffect(() => {
+    const observer = new ResizeObserver(() => {
+      if (targetElementRef?.current && observedElementRef?.current) {
+        const observedHeight = `${observedElementRef.current?.clientHeight}px`
+        targetElementRef.current.style.paddingBottom = observedHeight
+      }
+    })
+    if (observedElementRef?.current && condition) {
+      observer.observe(observedElementRef.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [condition, observedElementRef, targetElementRef])
+
+  return <></>
+}
+
+export { useFooterOffsetResizer }
