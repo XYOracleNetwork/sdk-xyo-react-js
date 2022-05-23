0fbb90813e02b22eb19b26fce29b7ae854a57361
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-05-23 10:14:51 -0400

    fix content not adjusting to footer height changes

--- src/modules/utils/useFooterOffsetResizer.stories.tsx
+++ src/modules/utils/useFooterOffsetResizer.stories.tsx
@@ -1,51 +1,29 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Paper } from '@mui/material'
 import { ComponentStory, Meta } from '@storybook/react'
-import { ButtonEx, FlexCol, FlexGrowCol } from '@xylabs/sdk-react'
-import { useRef, useState } from 'react'
+import { FlexCol, FlexGrowCol } from '@xylabs/sdk-react'
+import { useRef } from 'react'
 
+import { Footer } from '../general'
 import { useFooterOffsetResizer } from './useFooterOffsetResizer'
 
 const FooterOffsetResizer = () => {
-  const [condition, setCondition] = useState(false)
-  const footerHeight = '90px'
-
-  const observedElementRef = useRef<HTMLDivElement>()
   const targetElementRef = useRef<HTMLDivElement>()
 
-  useFooterOffsetResizer({ condition, observedElementRef, targetElementRef })
+  useFooterOffsetResizer({ targetElementRef })
 
   return (
     <>
       <FlexGrowCol minHeight="97vh">
         <Box display="flex" flexDirection="column" alignItems="stretch" flexGrow="1" pt={1} px={1} width="100%" ref={targetElementRef}>
-          <FlexCol>
-            <ButtonEx
-              marginBottom="12px"
-              variant="contained"
-              onClick={() =>
-                setCondition((condition) => {
-                  if (condition) {
-                    if (targetElementRef.current) {
-                      targetElementRef.current.style.paddingBottom = '0'
-                    }
-                  }
-                  return !condition
-                })
-              }
-            >
-              Toggle resizing
-            </ButtonEx>
-            <Typography variant="body1">{condition ? 'Hidden content is visible!' : 'Content hidden behind footer'}</Typography>
-          </FlexCol>
           <FlexGrowCol justifyContent="end">I am visible after resize and padding bottom is updated</FlexGrowCol>
         </Box>
       </FlexGrowCol>
 
-      <footer>
-        <Box position="fixed" bottom="0" left="0" right="0" ref={observedElementRef} width="100%" height={footerHeight} bgcolor="blue" color="white">
-          Footer Content of {footerHeight}
-        </Box>
-      </footer>
+      <FlexCol position="fixed" bottom={0} left={0} right={0} alignItems="stretch">
+        <Paper elevation={8}>
+          <Footer dynamicHeight width="100%" />
+        </Paper>
+      </FlexCol>
     </>
   )
 }
