078d57ed24ca545bae4389c0af2726e57e13ff1d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-06-07 15:36:17 -0400

    remove unnecessary workarounds

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const toPath = (filePath) => path.join(process.cwd(), filePath);
 
 module.exports = {
   core: {
@@ -14,24 +13,8 @@ module.exports = {
     "@storybook/addon-interactions",
     "storybook-dark-mode"
   ],
+  features: { modernInlineRendering: true },
   typescript: {
     check: true,
   },
-  // Actual recommended fix from MUI - https://mui.com/guides/migration-v4/#troubleshooting
-  webpackFinal: async (config) => ({
-    ...config,
-    resolve: {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        '@emotion/core': toPath('node_modules/@emotion/react'),
-        'emotion-theming': toPath('node_modules/@emotion/react'),
-     },
-     // can likely be removed in storybook > 6.4 see - https://github.com/storybookjs/storybook/issues/17458
-     fallback: {
-       ...config.resolve.fallback,
-      'assert': toPath('node_modules/assert')
-     }
-    },
- })
 }
