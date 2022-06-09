76e8dc40560b499cd53a2ad972477e2d7a7022b3
Arie Trouw <arie.trouw@xylabs.com>
2022-06-09 12:29:28 -0700

    SiteMenu updated

---
+++ packages/appbar/src/components/SiteMenu/MenuItems.tsx
@@ -0,0 +1,22 @@
+import { ListItem, ListItemIcon, ListItemProps, ListItemText, ListItemTextProps } from '@mui/material'
+import { ListItemButtonEx, ListItemButtonExProps } from '@xyo-network/react-shared'
+import { ReactNode } from 'react'
+import { To } from 'react-router-dom'
+
+export interface DrawerListItemProps extends ListItemProps {
+  primary: ListItemTextProps['primary']
+  to?: To
+  icon?: ReactNode
+  onButtonClick?: ListItemButtonExProps['onClick']
+}
+
+export const DrawerListItem: React.FC<DrawerListItemProps> = ({ primary, to, icon, onButtonClick, ...props }) => {
+  return (
+    <ListItem {...props}>
+      <ListItemButtonEx to={to} onClick={onButtonClick}>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={primary} />
+      </ListItemButtonEx>
+    </ListItem>
+  )
+}
