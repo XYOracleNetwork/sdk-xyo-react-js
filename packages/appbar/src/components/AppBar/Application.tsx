01747ab3d3093f2c5564513e11fba4b776e387de
Arie Trouw <arie.trouw@xylabs.com>
2022-06-09 17:36:47 -0700

    AppBar for Applications full width and now based on AppBarEx

--- packages/appbar/src/components/AppBar/Application.tsx
+++ packages/appbar/src/components/AppBar/Application.tsx
@@ -1,10 +1,11 @@
-import { AppBar, AppBarProps, Container, Toolbar, ToolbarProps, useMediaQuery, useTheme } from '@mui/material'
+import { Toolbar, ToolbarProps, useMediaQuery, useTheme } from '@mui/material'
+import { AppBarEx, AppBarExProps } from '@xylabs/react-common'
 import { FlexGrowRow, FlexRow } from '@xylabs/react-flexbox'
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 
 import { ContextToolbar, SystemToolbar } from '../Toolbar'
 
-export interface ApplicationAppBarProps extends AppBarProps {
+export interface ApplicationAppBarProps extends AppBarExProps {
   contextToolbar?: ReactElement<ToolbarProps>
   systemToolbar?: ReactElement<ToolbarProps>
   responsive?: boolean
@@ -14,15 +15,13 @@ export const ApplicationAppBar: React.FC<ApplicationAppBarProps> = ({ systemTool
   const { breakpoints } = useTheme()
   const belowSm = useMediaQuery(breakpoints.down('sm'))
   return (
-    <AppBar position="sticky" {...props}>
-      <Container maxWidth="xl" disableGutters>
-        <FlexRow flexWrap="nowrap" justifyContent="flex-start">
-          {contextToolbar ?? <ContextToolbar version />}
-          <FlexGrowRow>{belowSm && responsive ? null : children}</FlexGrowRow>
-          {systemToolbar ?? <SystemToolbar />}
-        </FlexRow>
-        {belowSm && children && responsive ? <Toolbar>{children}</Toolbar> : null}
-      </Container>
-    </AppBar>
+    <AppBarEx position="sticky" {...props}>
+      <FlexRow flexWrap="nowrap" justifyContent="flex-start">
+        {contextToolbar ?? <ContextToolbar version />}
+        <FlexGrowRow>{belowSm && responsive ? null : children}</FlexGrowRow>
+        {systemToolbar ?? <SystemToolbar />}
+      </FlexRow>
+      {belowSm && children && responsive ? <Toolbar>{children}</Toolbar> : null}
+    </AppBarEx>
   )
 }
