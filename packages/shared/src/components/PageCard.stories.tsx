fbfcea877a8b7f49a8a270dd275cf920c24cddc7
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-06-17 12:37:32 -0700

    add PageCard

---
+++ packages/shared/src/components/PageCard.stories.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-internal-modules */
+import { CardContent, Typography } from '@mui/material'
+import { ComponentStory, Meta } from '@storybook/react'
+import { useState } from 'react'
+
+import { PageCard } from './PageCard'
+
+const StorybookEntry: Meta = {
+  argTypes: {
+    onRefresh: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  component: PageCard,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+  title: 'shared/PageCard',
+}
+
+const Template: ComponentStory<typeof PageCard> = (props) => {
+  const [refreshedValue, setRefreshedValue] = useState<number>(Math.random)
+  return (
+    <PageCard title="Page Card" subheader="subheader" onRefresh={() => setRefreshedValue(Math.random())} {...props}>
+      <CardContent>
+        <Typography variant="body1">Page Card Content</Typography>
+        <Typography variant="body1">Refreshed Value: {refreshedValue?.toString()}</Typography>
+      </CardContent>
+    </PageCard>
+  )
+}
+
+const TemplateWithNoOnRefresh: ComponentStory<typeof PageCard> = (props) => {
+  return (
+    <PageCard title="Page Card" subheader="subheader" {...props}>
+      <CardContent>
+        <Typography variant="body1">Page Card Content</Typography>
+      </CardContent>
+    </PageCard>
+  )
+}
+
+const Default = Template.bind({})
+Default.args = {}
+Default.parameters = {
+  actions: { argTypesRegex: '' },
+}
+
+const WithNoOnRefresh = TemplateWithNoOnRefresh.bind({})
+WithNoOnRefresh.args = {}
+WithNoOnRefresh.parameters = {
+  actions: { argTypesRegex: '' },
+}
+
+export { Default, WithNoOnRefresh }
+
+// eslint-disable-next-line import/no-default-export
+export default StorybookEntry
