eeec4105751df6292a8a0ba3515148ebc8acc499
Arie Trouw <arie.trouw@xylabs.com>
2022-07-27 07:48:15 -0700

    errors fixed

--- packages/payload-plugin/src/components/XyoPayloadCardActionArea.tsx
+++ packages/payload-plugin/src/components/XyoPayloadCardActionArea.tsx
@@ -1,9 +1,8 @@
 import { CardActionArea, CardActionAreaProps } from '@mui/material'
-import { XyoPayload } from '@xyo-network/payload'
 
 import { XyoPayloadRenderProps } from '../XyoPayloadRenderPlugin'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const XyoPayloadCardActionArea: React.FC<XyoPayloadRenderProps<XyoPayload> & CardActionAreaProps> = ({ payload, ...props }) => {
+export const XyoPayloadCardActionArea: React.FC<XyoPayloadRenderProps & CardActionAreaProps> = ({ payload, ...props }) => {
   return <CardActionArea {...props} />
 }
--- packages/payload-plugin/src/components/XyoPayloadCardContent.tsx
+++ packages/payload-plugin/src/components/XyoPayloadCardContent.tsx
@@ -1,10 +1,9 @@
 import { CardContent, CardContentProps } from '@mui/material'
-import { XyoPayload } from '@xyo-network/payload'
 import { PayloadDetails } from '@xyo-network/react-payload'
 
 import { XyoPayloadRenderProps } from '../XyoPayloadRenderPlugin'
 
-export const XyoPayloadCardContent: React.FC<XyoPayloadRenderProps<XyoPayload> & CardContentProps> = ({ payload, ...props }) => {
+export const XyoPayloadCardContent: React.FC<XyoPayloadRenderProps & CardContentProps> = ({ payload, ...props }) => {
   return (
     <CardContent {...props}>
       <PayloadDetails payload={payload} />
