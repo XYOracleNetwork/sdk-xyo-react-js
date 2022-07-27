8a4ce1a2ac3b188aed0646b2a035858e56f77000
Arie Trouw <arie.trouw@xylabs.com>
2022-07-27 10:39:34 -0700

    createPayloadRenderPlugin generic

--- packages/payload-plugin/src/createPayloadRenderPlugin.ts
+++ packages/payload-plugin/src/createPayloadRenderPlugin.ts
@@ -3,6 +3,8 @@ import merge from 'lodash/merge'
 import { DefaultPayloadRenderPlugin } from './DefaultPayloadRenderPlugin'
 import { XyoPayloadRenderPlugin, XyoPayloadRenderPluginConfig } from './XyoPayloadRenderPlugin'
 
-export const createPayloadRenderPlugin = (plugin: XyoPayloadRenderPluginConfig): XyoPayloadRenderPlugin => {
+export const createPayloadRenderPlugin = <R extends XyoPayloadRenderPlugin = XyoPayloadRenderPlugin, C extends R & XyoPayloadRenderPluginConfig = R & XyoPayloadRenderPluginConfig>(
+  plugin: C
+): R => {
   return merge({}, DefaultPayloadRenderPlugin, plugin)
 }
