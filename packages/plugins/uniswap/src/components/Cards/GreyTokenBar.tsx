b1e2ad825e5e19414588014161010149868b0746
Jordan Trouw <jordan.trouw@gmail.com>
2022-08-02 15:40:53 -0700

    deps

--- packages/plugins/uniswap/src/components/Cards/GreyTokenBar.tsx
+++ packages/plugins/uniswap/src/components/Cards/GreyTokenBar.tsx
@@ -1,6 +1,5 @@
-import { alpha, Typography, useTheme } from '@mui/material'
+import { Typography, useTheme } from '@mui/material'
 import { FlexGrowRow } from '@xylabs/react-flexbox'
-import { useDarkMode } from 'storybook-dark-mode'
 interface GrayBarProps {
   text1?: string | number
   text2?: string | number
@@ -8,16 +7,8 @@ interface GrayBarProps {
 
 export const GrayTokenBar: React.FC<GrayBarProps> = ({ text1, text2 }) => {
   const theme = useTheme()
-  const darkMode = useDarkMode()
   return (
-    <FlexGrowRow
-      width="100%"
-      paddingY={1}
-      paddingX={2}
-      bgcolor={darkMode ? theme.palette.background.paper : alpha(theme.palette.grey[200], 0.3)}
-      borderRadius={1}
-      justifyContent="space-between"
-    >
+    <FlexGrowRow width="100%" paddingY={1} paddingX={2} bgcolor={theme.palette.background.paper} borderRadius={1} justifyContent="space-between">
       <Typography variant="body1" fontWeight={300}>
         {text1}
       </Typography>
