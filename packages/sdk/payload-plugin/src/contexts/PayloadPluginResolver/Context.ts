b33362d733c2e8925b58b3c203671b29913ad14e
Arie Trouw <arie.trouw@xylabs.com>
2022-08-02 13:16:15 -0700

    resolver contexts

---
+++ packages/sdk/payload-plugin/src/contexts/PayloadPluginResolver/Context.ts
@@ -0,0 +1,7 @@
+import { createContextEx } from '@xyo-network/react-shared'
+
+import { PayloadPluginResolverState } from './State'
+
+const PayloadPluginResolverContext = createContextEx<PayloadPluginResolverState>()
+
+export { PayloadPluginResolverContext }
---
+++ packages/sdk/payload-plugin/src/contexts/PayloadRenderPluginResolver/Context.ts
@@ -0,0 +1,7 @@
+import { createContextEx } from '@xyo-network/react-shared'
+
+import { PayloadRenderPluginResolverState } from './State'
+
+const PayloadRenderPluginResolverContext = createContextEx<PayloadRenderPluginResolverState>()
+
+export { PayloadRenderPluginResolverContext }
