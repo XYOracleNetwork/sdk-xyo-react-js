9380585064361ce35333c6222774f9346455e2ba
Arie Trouw <arie.trouw@xylabs.com>
2022-08-04 14:54:13 -0700

    payload diviner

---
+++ packages/sdk/payload/src/contexts/PayloadDiviner/Provider.tsx
@@ -0,0 +1,32 @@
+import { XyoHuriPayloadDiviner, XyoPayloadDiviner } from '@xyo-network/diviner'
+import { useArchivistApi } from '@xyo-network/react-archivist-api'
+import { ContextExProviderProps } from '@xyo-network/react-shared'
+import { useMemo, useState } from 'react'
+
+import { PayloadDivinerContext } from './Context'
+
+export type PayloadDivinerProviderProps = ContextExProviderProps<{
+  diviner?: XyoPayloadDiviner
+}>
+
+export const PayloadDivinerProvider: React.FC<PayloadDivinerProviderProps> = ({ diviner: divinerProp, required = false, children }) => {
+  const [diviner, setDiviner] = useState<XyoPayloadDiviner | undefined>(divinerProp)
+  const { api } = useArchivistApi()
+
+  const activeDiviner = useMemo(() => {
+    //if no diviner is set, then we use a XyoHuriPayloadDiviner
+    return diviner ?? new XyoHuriPayloadDiviner({ archivistUri: api?.config.apiDomain })
+  }, [diviner, api?.config.apiDomain])
+
+  return (
+    <PayloadDivinerContext.Provider
+      value={{
+        diviner: activeDiviner,
+        provided: true,
+        setDiviner,
+      }}
+    >
+      {diviner ? children : required ? null : children}
+    </PayloadDivinerContext.Provider>
+  )
+}
---
+++ packages/sdk/payload/src/contexts/PayloadDiviner/use.ts
@@ -0,0 +1,67 @@
+import { useAsyncEffect } from '@xylabs/react-shared'
+import { XyoPayloadDivinerPayloadSchema, XyoPayloadDivinerQueryPayloadSchema } from '@xyo-network/diviner'
+import { XyoPayload } from '@xyo-network/payload'
+import { useContextEx } from '@xyo-network/react-shared'
+import compact from 'lodash/compact'
+import { useState } from 'react'
+
+import { PayloadDivinerContext } from './Context'
+
+export const usePayloadDiviner = (required = false) => {
+  return useContextEx(PayloadDivinerContext, 'PayloadDiviner', required)
+}
+
+export const useDivinePayload = (huri?: string): [XyoPayload | undefined | null, Error | undefined] => {
+  const { diviner } = usePayloadDiviner()
+  const [payload, setPayload] = useState<XyoPayload | null>()
+  const [error, setError] = useState<Error>()
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async (mounted) => {
+      if (huri) {
+        try {
+          const payload = (await diviner?.divine({ huri, schema: XyoPayloadDivinerQueryPayloadSchema, targetSchema: XyoPayloadDivinerPayloadSchema }))?.payload
+          if (mounted()) {
+            setPayload(payload)
+          }
+        } catch (ex) {
+          if (mounted()) {
+            setError(ex as Error)
+          }
+        }
+      }
+    },
+    [diviner, huri]
+  )
+
+  return [payload, error]
+}
+
+export const useDivinePayloads = (huriList: string[]): [(XyoPayload | null)[] | undefined, Error[] | undefined] => {
+  const { diviner } = usePayloadDiviner()
+  const [payloads, setPayloads] = useState<(XyoPayload | null)[]>()
+  const [errors, setErrors] = useState<Error[]>()
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async (mounted) => {
+      console.log(`huriList: ${JSON.stringify(huriList, null, 2)}`)
+      const payloads = await Promise.allSettled(
+        huriList.map(
+          async (huri) =>
+            (await diviner?.divine({ huri, schema: XyoPayloadDivinerQueryPayloadSchema, targetSchema: XyoPayloadDivinerPayloadSchema }))?.payload ?? null
+        )
+      )
+      if (mounted()) {
+        setPayloads([...payloads.values()].map((value) => (value.status === 'rejected' ? null : value.value)))
+        setErrors(compact([...payloads.values()].map((value) => (value.status === 'rejected' ? Error('fivine failed', { cause: value.reason }) : undefined))))
+      }
+    },
+    [diviner, huriList]
+  )
+
+  console.log(`payloads: ${JSON.stringify(payloads, null, 2)}`)
+
+  return [payloads, errors]
+}
