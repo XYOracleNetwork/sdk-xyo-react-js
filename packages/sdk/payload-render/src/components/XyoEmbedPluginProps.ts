173b3f9b317397fe2be0883da2b1d14cf57d097c
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-08-04 17:43:23 -0400

    add embed plugin and package

---
+++ packages/sdk/payload-render/src/components/XyoEmbedPluginProps.ts
@@ -0,0 +1,7 @@
+import { FlexBoxProps } from '@xylabs/react-flexbox'
+import { XyoPayloadRenderPlugin } from '@xyo-network/react-payload-plugin'
+
+export interface XyoEmbedPluginProps extends FlexBoxProps {
+  plugins?: XyoPayloadRenderPlugin[]
+  huri?: string
+}
