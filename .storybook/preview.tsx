4c5e78d2333ddc5c250b19dd486eaa5b040d2758
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-08-08 16:29:50 -0400

    add app settings context to all stories and support darkmode toggle

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,8 @@ import { FlexCol } from '@xylabs/react-flexbox'
 import { CssBaseline } from '@mui/material';
 import { useDarkMode } from 'storybook-dark-mode';
 import { partialDarkThemeOptions, themeOptions } from '@xyo-network/react-theme'
+import { AppSettingsProvider } from '@xyo-network/react-app-settings'
+import React from 'react';
 
 
 export const parameters = {
@@ -23,17 +25,19 @@ export const parameters = {
 
 const withThemeProvider = (Story, context) => {
   // Clear the auth state with each story
-  localStorage.setItem('AuthState', null)
+  localStorage.setItem('AuthState', '')
 
   const darkMode = useDarkMode()
 
   return (
+    <AppSettingsProvider value={{darkMode}}>
     <InvertableThemeProvider dark={darkMode} darkTheme={partialDarkThemeOptions} options={themeOptions}>
       <CssBaseline enableColorScheme />
       <FlexCol alignItems="unset">
         <Story {...context}/>
       </FlexCol>
     </InvertableThemeProvider>
+    </AppSettingsProvider>
   );
 };
 
