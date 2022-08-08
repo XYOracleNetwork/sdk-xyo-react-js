a35b0e924c1403dc465a0a6ddea799191fd12bd3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-08-08 18:03:36 -0400

    support dark mode styling via props

--- packages/sdk/shared/src/components/TokenSummary/TokenSummary.tsx
+++ packages/sdk/shared/src/components/TokenSummary/TokenSummary.tsx
@@ -4,14 +4,15 @@ import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/react-flexbox'
 export interface TokenSummaryProps extends FlexBoxProps {
   icon?: string
   symbol?: string
+  imgBgColor?: string
 }
 
-export const TokenSummary: React.FC<TokenSummaryProps> = ({ icon, symbol, children, ...props }) => {
+export const TokenSummary: React.FC<TokenSummaryProps> = ({ imgBgColor, icon, symbol, children, ...props }) => {
   const theme = useTheme()
   return (
     <>
       <FlexRow paddingBottom={3} width="100%" justifyContent="flex-start" {...props}>
-        <FlexCol borderRadius="50%" padding={1} bgcolor={theme.palette.text.primary} height="40px" width="40px">
+        <FlexCol borderRadius="50%" padding={1} bgcolor={imgBgColor ? imgBgColor : theme.palette.text.primary} height="40px" width="40px">
           <img src={icon} height="25px" />
         </FlexCol>
         <Typography variant="h6" fontWeight={300} textTransform="uppercase" paddingLeft={1}>
