bf655ac872647213ed490e52b7ce26c31ab6a040
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-08-09 16:47:44 -0400

    testing custom color options

--- packages/sdk/theme/src/partialDarkThemeOptions.tsx
+++ packages/sdk/theme/src/partialDarkThemeOptions.tsx
@@ -1,5 +1,19 @@
-import { darken, ThemeOptions } from '@mui/material'
+import { Button, darken, ThemeOptions } from '@mui/material'
 import merge from 'lodash/merge'
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: Palette['primary']
+  }
+  interface PaletteOptions {
+    neutral?: PaletteOptions['primary']
+  }
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    neutral: true
+  }
+}
 
 const appComponents: ThemeOptions['components'] = {
   MuiPaper: {
@@ -16,6 +30,9 @@ export const partialDarkThemeOptions: ThemeOptions = {
       paper: '#171626',
     },
     mode: 'dark',
+    neutral: {
+      main: '#CCC',
+    },
     primary: {
       main: '#9993F5',
     },
@@ -26,3 +43,7 @@ export const partialDarkThemeOptions: ThemeOptions = {
 }
 
 export const partialAppDarkThemeOptions: ThemeOptions = merge({}, partialDarkThemeOptions, { components: appComponents })
+
+export const TestNeutralButton = () => {
+  return <Button color="neutral">neutral</Button>
+}
