dd9a113c63574df63ac920c7943320a0c6ff7892
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-08-08 23:52:30 -0400

    custom variant for paper elements in light mode

--- packages/plugins/aggregate-price/src/components/Asset.tsx
+++ packages/plugins/aggregate-price/src/components/Asset.tsx
@@ -1,7 +1,6 @@
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import { Card, CardContent, CardProps, Divider, IconButton, Paper, Tooltip } from '@mui/material'
 import { FlexCol, FlexGrowCol, FlexRow } from '@xylabs/react-flexbox'
-import { useAppSettings } from '@xyo-network/react-app-settings'
 import { TokenBar, TokenSummary, useGetTokenData } from '@xyo-network/react-shared'
 import { Fragment } from 'react'
 
@@ -14,14 +13,13 @@ interface CryptoAssetProps extends CardProps {
 
 export const CryptoAsset: React.FC<CryptoAssetProps> = ({ asset, priceInfo, ...props }) => {
   const [tokenInfo] = useGetTokenData([asset])
-  const { darkMode } = useAppSettings()
 
   return (
     <Card className="CryptoAsset-root" {...props}>
       <CardContent style={{ height: '100%' }}>
         <FlexCol alignItems="stretch" height="100%" justifyContent="flex-start">
           <FlexRow>
-            <TokenSummary icon={tokenInfo.icon} symbol={asset} imgBgColor={darkMode ? '' : '#F6F5FA'}>
+            <TokenSummary icon={tokenInfo.icon} symbol={asset} imgBgProps={{ variant: 'lightModeBg' }}>
               <IconButton sx={{ mb: 3 }}>
                 <Tooltip title="The price of cryptos based on multiple inputs.">
                   <HelpOutlineIcon />
@@ -30,10 +28,10 @@ export const CryptoAsset: React.FC<CryptoAssetProps> = ({ asset, priceInfo, ...p
             </TokenSummary>
           </FlexRow>
           <FlexCol alignItems="stretch" justifyContent="flex-start">
-            <Paper component={FlexGrowCol} elevation={0} alignItems="stretch" overflow="hidden">
+            <Paper component={FlexGrowCol} elevation={0} alignItems="stretch" overflow="hidden" sx={{ border: 'none' }}>
               {Object.entries(priceInfo.value).map(([currency, price], index, arr) => (
                 <Fragment key={currency}>
-                  <TokenBar square text1={currency.toUpperCase()} text2={price} sx={{ bgcolor: darkMode ? 'inherit' : '#F6F5FA' }} />
+                  <TokenBar square text1={currency.toUpperCase()} text2={price} variant="lightModeBg" />
                   {/* hide the last divider */}
                   {index !== arr.length - 1 ? <Divider flexItem /> : null}
                 </Fragment>
--- packages/sdk/shared/src/components/TokenSummary/TokenSummary.tsx
+++ packages/sdk/shared/src/components/TokenSummary/TokenSummary.tsx
@@ -1,27 +1,19 @@
-import { Typography, useTheme } from '@mui/material'
+import { Paper, PaperProps, Typography } from '@mui/material'
 import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/react-flexbox'
 
 export interface TokenSummaryProps extends FlexBoxProps {
   icon?: string
   symbol?: string
-  imgBgColor?: string
+  imgBgProps?: PaperProps
 }
 
-export const TokenSummary: React.FC<TokenSummaryProps> = ({ imgBgColor, icon, symbol, children, ...props }) => {
-  const theme = useTheme()
+export const TokenSummary: React.FC<TokenSummaryProps> = ({ imgBgProps, icon, symbol, children, ...props }) => {
   return (
     <>
       <FlexRow className="TokenSummary-root" paddingBottom={3} width="100%" justifyContent="flex-start" {...props}>
-        <FlexCol
-          borderRadius="50%"
-          padding={1}
-          className="token-logo-bg"
-          bgcolor={imgBgColor ? imgBgColor : theme.palette.text.primary}
-          height="40px"
-          width="40px"
-        >
+        <Paper component={FlexCol} padding={1} className="token-logo-bg" height="40px" width="40px" sx={{ borderRadius: '50%' }} {...imgBgProps}>
           <img src={icon} height="25px" />
-        </FlexCol>
+        </Paper>
         <Typography variant="h6" fontWeight={300} textTransform="uppercase" paddingLeft={1}>
           {symbol}
         </Typography>
