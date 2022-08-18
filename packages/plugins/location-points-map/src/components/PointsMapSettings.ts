49f6976088f18f7d9f5a3b69d6c356418b2e1021
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-08-18 13:43:47 -0400

    add location points renderer

---
+++ packages/plugins/location-points-map/src/components/PointsMapSettings.ts
@@ -0,0 +1,8 @@
+import { DefaultMapSettings } from '@xyo-network/react-map'
+
+const PointsMapSettings = DefaultMapSettings()
+const { fitToPoints, enableControls } = PointsMapSettings
+fitToPoints.value = true
+enableControls.value = true
+
+export { PointsMapSettings }
