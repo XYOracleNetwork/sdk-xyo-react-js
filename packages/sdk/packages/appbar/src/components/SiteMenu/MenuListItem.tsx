44d561fb9ecb3f54f3ca78c1de12c9c9797a9f12
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-09-16 23:34:57 -0400

    smooth out sidebar collapse animation

--- packages/sdk/packages/appbar/src/components/SiteMenu/MenuListItem.tsx
+++ packages/sdk/packages/appbar/src/components/SiteMenu/MenuListItem.tsx
@@ -6,11 +6,10 @@ export interface MenuListItemProps extends SiteMenuListItemBase, ListItemProps {
 
 export const MenuListItem: React.FC<MenuListItemProps> = ({ children, style, iconOnly, ...props }) => {
   const listItemStyles = {
-    display: iconOnly ? 'inline-flex' : 'block',
     width: iconOnly ? 'auto' : '100%',
   }
   return (
-    <ListItem style={{ ...listItemStyles, ...style }} {...props}>
+    <ListItem style={{ display: 'block', ...listItemStyles, ...style }} {...props}>
       {children}
     </ListItem>
   )
