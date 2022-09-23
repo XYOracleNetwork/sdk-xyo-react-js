4d623c8a4b9d8a3d9935ed8efc586a726ef4d271
Jordan Trouw <jordan.trouw@gmail.com>
2022-09-23 14:16:21 -0700

    updates to nav - may have fixed bug

--- packages/sdk/packages/appbar/src/components/CollapsibleDrawer/CollapseDrawer.stories.tsx
+++ packages/sdk/packages/appbar/src/components/CollapsibleDrawer/CollapseDrawer.stories.tsx
@@ -1,9 +1,9 @@
-import { Divider, List, ListSubheader, useTheme } from '@mui/material'
+import { useTheme } from '@mui/material'
 import { ComponentMeta, ComponentStory, DecoratorFn } from '@storybook/react'
 import { FlexGrowCol } from '@xylabs/react-flexbox'
 
 import { CollapsibleProvider, useCollapsible } from '../../contexts'
-import { SiteMenuListItem } from '../SiteMenu'
+import { MenuSection } from '../SiteMenu'
 import { CollapseToggleFlex } from './CollapseToggle'
 import { CollapsibleDrawer } from './CollapsibleDrawer'
 import { menuDataBottom, menuDataTop } from './ExampleMenuData'
@@ -29,30 +29,17 @@ const StorybookEntry = {
 } as ComponentMeta<typeof CollapsibleDrawer>
 
 const Template: ComponentStory<typeof CollapsibleDrawer> = (args) => {
-  const { collapse, setCollapseEnd, collapseEnd } = useCollapsible()
+  const { collapse, setCollapseEnd } = useCollapsible()
   const theme = useTheme()
   return (
     <>
       <FlexGrowCol alignItems="start" height="calc(100vh - 2rem)">
         <CollapsibleDrawer in={!collapse} orientation="horizontal" collapsedSize={theme.spacing(5)} onExited={() => setCollapseEnd?.(true)} {...args}>
-          <List>
-            {collapseEnd ? null : <ListSubheader>Explore & Create</ListSubheader>}
-            {menuDataTop.map((item, index) => (
-              <>
-                <SiteMenuListItem key={index} {...item}></SiteMenuListItem>
-              </>
-            ))}
-            {collapseEnd ? null : <ListSubheader>Settings & Analytics</ListSubheader>}
-            {menuDataBottom.map((item, index) => (
-              <>
-                <SiteMenuListItem key={index} {...item}></SiteMenuListItem>
-              </>
-            ))}
-          </List>
+          <MenuSection title="Explore & Create" listItems={menuDataTop} showTitle={!collapse} />
+          <MenuSection title="Settings & Analytics" listItems={menuDataBottom} showTitle={!collapse} />
           <FlexGrowCol />
           <CollapseToggleFlex justifyContent="start" />
         </CollapsibleDrawer>
-        <Divider orientation="vertical" flexItem />
       </FlexGrowCol>
     </>
   )
