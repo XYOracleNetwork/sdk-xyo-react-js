af110d5f29d820ecdf060b3157457c528ea3d93d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-09-29 18:13:55 -0400

    make fixed pages opt in via variant

---
+++ packages/sdk/packages/webapp/src/components/lib/cssValues.ts
@@ -0,0 +1,20 @@
+import { CSSProperties } from 'react'
+
+export const scrollableWrap: CSSProperties = {
+  inset: 0,
+  position: 'absolute',
+} as const
+
+export const fixedWrap: CSSProperties = {
+  inset: 'unset',
+  position: 'relative',
+} as const
+
+// Making a scrollable vs fixed wrapper and content is an inversion of the wrap and content styles
+export const scrollableContent: CSSProperties = {
+  ...fixedWrap,
+} as const
+
+export const fixedContent: CSSProperties = {
+  ...scrollableWrap,
+} as const
