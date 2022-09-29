cb32bc6cbe10653770c75a3259d6ee3f46eda981
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-09-29 14:05:44 -0400

    remove duplicate components

--- packages/sdk/packages/appbar/src/components/SiteMenu/MenuListItem.tsx
+++ packages/sdk/packages/appbar/src/components/SiteMenu/MenuListItem.tsx
@@ -2,14 +2,18 @@ import { ListItem, ListItemProps } from '@mui/material'
 
 import { SiteMenuListItemBase } from './lib'
 
-export interface MenuListItemProps extends SiteMenuListItemBase, ListItemProps {}
+export interface ListItemExProps extends SiteMenuListItemBase, ListItemProps {}
 
-export const MenuListItem: React.FC<MenuListItemProps> = ({ children, style, iconOnly, ...props }) => {
-  const listItemStyles = {
-    width: iconOnly ? 'auto' : '100%',
-  }
+export const MenuListItem: React.FC<ListItemExProps> = ({ iconOnly, collapseEnd, sx, children, ...props }) => {
+  const dense = props
+
+  const listItemSx = iconOnly ? { borderRadius: '50%', display: 'inline-flex', flexGrow: 0, width: 'auto' } : { width: '100%' }
+  // wait till collapseEnds to remove the spacing between items
+  const spacingSx = collapseEnd ? { columnGap: 0 } : { columnGap: 1.5 }
+  // adjusts to the paddingY value which does NOT scale along the theme.spacing
+  const paddingSx = dense ? { px: '8px' } : { px: '12px' }
   return (
-    <ListItem style={{ display: 'block', ...listItemStyles, ...style }} {...props}>
+    <ListItem sx={{ ...listItemSx, ...spacingSx, ...paddingSx, ...sx }} {...props}>
       {children}
     </ListItem>
   )
