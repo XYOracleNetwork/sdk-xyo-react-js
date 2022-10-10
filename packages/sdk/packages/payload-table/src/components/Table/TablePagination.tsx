4d5ac7298ccca260e3f3df9de0f408f1632f55f0
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-10-10 10:55:06 -0400

    emit new events

--- packages/sdk/packages/payload-table/src/components/Table/TablePagination.tsx
+++ packages/sdk/packages/payload-table/src/components/Table/TablePagination.tsx
@@ -3,6 +3,7 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
 import LastPageIcon from '@mui/icons-material/LastPage'
 import { Box, CircularProgress, IconButton, useTheme } from '@mui/material'
+import { useXyoEvent } from '@xyo-network/react-event'
 
 interface TablePaginationActionsProps {
   count: number
@@ -15,16 +16,20 @@ interface TablePaginationActionsProps {
 
 export function TablePaginationActions({ count, page, rowsPerPage, onPageChange, enableNextPage, loading }: TablePaginationActionsProps) {
   const theme = useTheme()
+  const [nextPageRef, nextPageDispatch] = useXyoEvent<HTMLButtonElement>()
+  const [previousPageRef, previousPageDispatch] = useXyoEvent<HTMLButtonElement>()
 
   const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     onPageChange(event, 0)
   }
 
   const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    previousPageDispatch('previousPage', 'click')
     onPageChange(event, page - 1)
   }
 
   const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    nextPageDispatch('nextPage', 'click')
     onPageChange(event, page + 1)
   }
 
@@ -39,10 +44,15 @@ export function TablePaginationActions({ count, page, rowsPerPage, onPageChange,
         <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page">
           {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
         </IconButton>
-        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        <IconButton ref={previousPageRef} onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
           {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
         </IconButton>
-        <IconButton onClick={handleNextButtonClick} disabled={!enableNextPage && page >= Math.ceil(count / rowsPerPage) - 1} aria-label="next page">
+        <IconButton
+          ref={nextPageRef}
+          onClick={handleNextButtonClick}
+          disabled={!enableNextPage && page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
           {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
         </IconButton>
         <IconButton onClick={handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="last page">
