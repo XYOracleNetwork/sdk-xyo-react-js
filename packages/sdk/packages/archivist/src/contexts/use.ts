06d2f4bb553f0bee2126a2ddbdcf2a4eb676e5ee
Arie Trouw <arie.trouw@xylabs.com>
2022-10-18 16:32:11 -0700

    updo and additional archivist hooks

--- packages/sdk/packages/archivist/src/contexts/use.ts
+++ packages/sdk/packages/archivist/src/contexts/use.ts
@@ -55,3 +55,49 @@ export const useArchivistFind = <TFilter extends XyoPayloadFindFilter>(filter: T
   )
   return [payloads, error]
 }
+
+export const useArchivistAll = (required = false): [(XyoPayload | null)[]?, Error?] => {
+  const { archivist } = useArchivist(required)
+  const [payloads, setPayloads] = useState<XyoPayloads>()
+  const [error, setError] = useState<Error>()
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async (mounted) => {
+      try {
+        const wrapper = archivist ? new XyoArchivistWrapper(archivist) : undefined
+        const result = await wrapper?.all()
+        if (mounted()) {
+          setError(undefined)
+          setPayloads(result)
+        }
+      } catch (ex) {
+        setError(ex as Error)
+      }
+    },
+    [archivist],
+  )
+  return [payloads, error]
+}
+
+export const useArchivistClear = (required = false): [(XyoPayload | null)[]?, Error?] => {
+  const { archivist } = useArchivist(required)
+  const [payloads, setPayloads] = useState<XyoPayloads>()
+  const [error, setError] = useState<Error>()
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async (mounted) => {
+      try {
+        const wrapper = archivist ? new XyoArchivistWrapper(archivist) : undefined
+        await wrapper?.clear()
+        if (mounted()) {
+          setError(undefined)
+          setPayloads([])
+        }
+      } catch (ex) {
+        setError(ex as Error)
+      }
+    },
+    [archivist],
+  )
+  return [payloads, error]
+}
