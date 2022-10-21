7608b6aa9f3ea95f752145a86bb5f6351866b4bb
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-10-20 21:17:19 -0400

    archivist hooks to their own files

---
+++ packages/sdk/packages/archivist/src/contexts/use/useArchivistStates.tsx
@@ -0,0 +1,23 @@
+import { XyoArchivistWrapper } from '@xyo-network/archivist'
+import { XyoPayloads } from '@xyo-network/payload'
+import { useEffect, useState } from 'react'
+
+import { useArchivist } from './use'
+
+export const useArchivistStates = (required: boolean) => {
+  const [payloads, setPayloads] = useState<XyoPayloads>()
+  const { archivist } = useArchivist(required)
+  const [error, setError] = useState<Error>()
+  const [refresh, setRefresh] = useState(1)
+  const refreshPayloads = () => setRefresh((previous) => previous + 1)
+
+  const [archivistWrapper, setArchivistWrapper] = useState<XyoArchivistWrapper>()
+
+  useEffect(() => {
+    if (archivist) {
+      setArchivistWrapper(new XyoArchivistWrapper(archivist))
+    }
+  }, [archivist])
+
+  return { archivistWrapper, error, payloads, refresh, refreshPayloads, setError, setPayloads }
+}
