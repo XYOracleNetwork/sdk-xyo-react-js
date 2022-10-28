2b126fcefc1e5d234328ba8b66351e96b471665c
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-10-28 14:12:49 -0400

    responsive styles and custom avatar max

--- packages/plugins/packages/boundwitness/src/components/Card/CardContent/CardContent.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/CardContent/CardContent.tsx
@@ -12,41 +12,35 @@ export interface BoundWitnessCardContentProps extends CardContentProps {
 
 export const BoundWitnessCardContent: React.FC<BoundWitnessCardContentProps> = ({ payload, ...props }) => {
   const boundwitness = payload as XyoPayload<XyoBoundWitness>
-  const theme = useTheme()
-  const additionalAddressCount = (count?: number) => {
-    if (!count || count <= 1) {
-      return null
-    }
-    return `+${count - 1}`
-  }
 
   return (
     <CardContentFlex {...props}>
-      <CardColumnsFlex borderRight={`1px solid ${theme.palette.divider}`}>
-        <CardColumnTitleH2 title="Payload Count">{boundwitness?.payload_hashes.length}</CardColumnTitleH2>
+      <CardColumnsFlex>
+        <CardColumnTitleH2>Payloads</CardColumnTitleH2>
         <SchemaAvatarGroup schemas={boundwitness?.payload_schemas} />
       </CardColumnsFlex>
       <CardColumnsFlex>
-        <CardColumnTitleH2 title="Additional Signers">{additionalAddressCount(boundwitness?.addresses.length)}</CardColumnTitleH2>
+        <CardColumnTitleH2>Signers</CardColumnTitleH2>
         <AddressAvatarGroup addresses={boundwitness?.addresses} />
       </CardColumnsFlex>
     </CardContentFlex>
   )
 }
 
-const CardColumnTitleH2: React.FC<TypographyProps> = (props) => <CardColumnTitle variant="h2" {...props} />
+const CardColumnTitleH2: React.FC<TypographyProps> = (props) => <CardColumnTitle variant="h4" {...props} />
 
 const CardContentFlex = styled(CardContent, { name: 'CardContentFlex' })(({ theme }) => ({
   display: 'flex',
   flexDirection: 'row',
-  [theme.breakpoints.down('xs')]: {
-    flexWrap: 'wrap',
-  },
+  flexWrap: 'wrap',
+  rowGap: theme.spacing(1),
 }))
 
 const CardColumnsFlex = styled(FlexCol, { name: 'CardColumnsFlex' })(({ theme }) => ({
-  flexBasis: '50%',
   rowGap: theme.spacing(1),
+  [theme.breakpoints.up('xs')]: {
+    flexBasis: '50%',
+  },
 }))
 
 const CardColumnTitle = styled(Typography, { name: 'CardColumnTitle' })(() => ({
--- packages/plugins/packages/boundwitness/src/components/Card/CardContent/SchemaAvatarGroup.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/CardContent/SchemaAvatarGroup.tsx
@@ -2,11 +2,12 @@ import { Avatar, AvatarGroup, AvatarGroupProps } from '@mui/material'
 
 interface SchemaAvatarGroupProps extends AvatarGroupProps {
   schemas?: string[]
+  maxAvatars?: number
 }
 
-export const SchemaAvatarGroup: React.FC<SchemaAvatarGroupProps> = ({ schemas, ...props }) => {
+export const SchemaAvatarGroup: React.FC<SchemaAvatarGroupProps> = ({ schemas, maxAvatars = 4, ...props }) => {
   return (
-    <AvatarGroup max={5} total={schemas?.length} {...props}>
+    <AvatarGroup max={maxAvatars} total={schemas?.length} {...props}>
       {schemas?.map((schema, index) => (
         <Avatar key={index + schema} title={schema}>
           {schema[0]}
