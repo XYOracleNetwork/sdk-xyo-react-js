38e10b06d385b082f2f8ec82a6cdda68e74f7770
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-04 16:10:28 -0400

    repurpose address to address history

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,4 +1,4 @@
-export * from '@xyo-network/react-address'
+export * from '@xyo-network/react-address-history'
 export * from '@xyo-network/react-api'
 export * from '@xyo-network/react-app-settings'
 export * from '@xyo-network/react-appbar'
