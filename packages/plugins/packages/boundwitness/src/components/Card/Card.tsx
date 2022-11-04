2a18afba47c3c029b3108ea0ac6a97e94d95456b
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-04 16:08:55 -0400

    keep bw plugin dep free

--- packages/plugins/packages/boundwitness/src/components/Card/Card.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/Card.tsx
@@ -1,21 +1,20 @@
 import { Card, CardProps } from '@mui/material'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { XyoPayload } from '@xyo-network/payload'
-import { useActiveBoundWitness } from '@xyo-network/react-address'
 
 import { BoundWitnessCardContent } from './CardContent'
 import { BoundWitnessCardHeader } from './CardHeader'
 
 export interface BoundWitnessCardProps extends CardProps {
   payload?: XyoPayload
+  active?: boolean
 }
-export const BoundWitnessRendererCard: React.FC<BoundWitnessCardProps> = ({ payload, ...props }) => {
+export const BoundWitnessRendererCard: React.FC<BoundWitnessCardProps> = ({ payload, active, ...props }) => {
   const boundwitness = payload as XyoPayload<XyoBoundWitness>
-  const { activeBoundWitness } = useActiveBoundWitness(false)
 
   return (
     <Card {...props}>
-      <BoundWitnessCardHeader payload={boundwitness} active={payload !== undefined ? activeBoundWitness === payload : undefined} />
+      <BoundWitnessCardHeader payload={boundwitness} active={active} />
       <BoundWitnessCardContent payload={boundwitness} />
     </Card>
   )
