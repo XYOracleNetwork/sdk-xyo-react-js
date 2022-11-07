75afb74157a07fd1be1d2ebf22eab293b730c6b8
Arie Trouw <arie.trouw@xylabs.com>
2022-11-07 15:30:54 -0800

    useModule updated

--- packages/sdk/packages/node/src/contexts/useDivinerModule.ts
+++ packages/sdk/packages/node/src/contexts/useDivinerModule.ts
@@ -3,6 +3,6 @@ import { XyoDivinerWrapper } from '@xyo-network/diviner'
 import { useModule } from './useModule'
 
 export const useDivinerModule = (address?: string) => {
-  const module = useModule(address)
+  const module = useModule({ address })
   return module ? new XyoDivinerWrapper(module) : undefined
 }
--- packages/sdk/packages/node/src/contexts/useWitnessModule.ts
+++ packages/sdk/packages/node/src/contexts/useWitnessModule.ts
@@ -3,6 +3,6 @@ import { XyoWitnessWrapper } from '@xyo-network/witness'
 import { useModule } from './useModule'
 
 export const useWitnessModule = (address?: string) => {
-  const module = useModule(address)
+  const module = useModule({ address })
   return module ? new XyoWitnessWrapper(module) : undefined
 }
