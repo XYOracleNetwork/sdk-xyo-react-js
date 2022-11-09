4e3c97d6916a9943536391e1f0dbedd1cdca755f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-09 08:20:19 -0500

    use Storage archivist

--- packages/sdk/packages/address-history/src/components/history/ActiveBWDecorator.stories.tsx
+++ packages/sdk/packages/address-history/src/components/history/ActiveBWDecorator.stories.tsx
@@ -1,12 +1,23 @@
 import { DecoratorFn } from '@storybook/react'
+import { WithChildren } from '@xylabs/react-shared'
+import { XyoStorageArchivistConfigSchema } from '@xyo-network/archivist'
+import { PayloadWrapper } from '@xyo-network/payload'
+import { StorageArchivistProvider, useArchivistInsert } from '@xyo-network/react-archivist'
 import { sampleAddressHistory } from '@xyo-network/react-storybook'
 
 import { ActiveBoundWitnessProvider } from '../../contexts'
 
 export const ActiveBWDecorator: DecoratorFn = (Story, args) => {
   return (
-    <ActiveBoundWitnessProvider activeBoundWitness={sampleAddressHistory[0]}>
-      <Story {...args} />
-    </ActiveBoundWitnessProvider>
+    <StorageArchivistProvider config={{ namespace: 'AddressHistory', schema: XyoStorageArchivistConfigSchema, type: 'local' }}>
+      <ActiveBWDecoratorInner>
+        <Story {...args} />
+      </ActiveBWDecoratorInner>
+    </StorageArchivistProvider>
   )
+}
+
+const ActiveBWDecoratorInner: React.FC<WithChildren> = ({ children }) => {
+  useArchivistInsert(sampleAddressHistory, true)
+  return <ActiveBoundWitnessProvider activeBoundWitnessHash={new PayloadWrapper(sampleAddressHistory[0]).hash}>{children}</ActiveBoundWitnessProvider>
 }
