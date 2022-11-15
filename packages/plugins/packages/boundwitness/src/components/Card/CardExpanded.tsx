e309f3dde58e389f7f2aa6c161035b0e420e2b9f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-15 14:36:16 -0500

    nav swaps card content children

--- packages/plugins/packages/boundwitness/src/components/Card/CardExpanded.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/CardExpanded.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardProps } from '@mui/material'
 import { FlexGrowCol } from '@xylabs/react-flexbox'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { XyoPayload } from '@xyo-network/payload'
-import { forwardRef } from 'react'
+import { forwardRef, useState } from 'react'
 
 import { CardColumnTitleH2 } from './CardContent'
 import { BoundWitnessCardHeader } from './CardHeader'
@@ -14,15 +14,17 @@ export interface BoundWitnessRendererCardExpandedProps extends CardProps {
 }
 const BoundWitnessRendererCardExpanded = forwardRef<HTMLDivElement, BoundWitnessRendererCardExpandedProps>(({ payload, active, ...props }, ref) => {
   const boundwitness = payload as XyoPayload<XyoBoundWitness>
-
+  const [activeTab, setActiveTab] = useState(0)
   return (
-    <Card {...props}>
+    <Card ref={ref} {...props}>
       <BoundWitnessCardHeader payload={boundwitness} active={active} />
       <FlexGrowCol alignItems="stretch">
         <CardContent>
-          <CardColumnTitleH2>Payloads</CardColumnTitleH2>
+          {activeTab === 0 ? <CardColumnTitleH2>Payloads</CardColumnTitleH2> : null}
+          {activeTab === 1 ? <CardColumnTitleH2>Bound Witnesses</CardColumnTitleH2> : null}
+          {activeTab === 2 ? <CardColumnTitleH2>Signatures</CardColumnTitleH2> : null}
         </CardContent>
-        <BoundWitnessCardBottomNavigation boundWitness={boundwitness} />
+        <BoundWitnessCardBottomNavigation boundWitness={boundwitness} activeTab={activeTab} setActiveTab={setActiveTab} />
       </FlexGrowCol>
     </Card>
   )
--- packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.tsx
@@ -1,36 +1,38 @@
 import { BottomNavigation, BottomNavigationAction, Paper, PaperProps, styled } from '@mui/material'
 import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
-import { forwardRef, useState } from 'react'
+import { Dispatch, forwardRef, SetStateAction } from 'react'
 import { FaSignature } from 'react-icons/fa'
 import { VscSymbolMethod, VscSymbolNamespace } from 'react-icons/vsc'
 
 export interface BoundWitnessCardBottomNavigationProps extends PaperProps {
   boundWitness?: XyoBoundWitness
+  activeTab?: number
+  setActiveTab?: Dispatch<SetStateAction<number>>
 }
 
-const BoundWitnessCardBottomNavigation = forwardRef<HTMLDivElement, BoundWitnessCardBottomNavigationProps>(({ boundWitness, ...props }, ref) => {
-  const [value, setValue] = useState(0)
+const BoundWitnessCardBottomNavigation = forwardRef<HTMLDivElement, BoundWitnessCardBottomNavigationProps>(
+  ({ activeTab = 0, setActiveTab, boundWitness, ...props }, ref) => {
+    const payloadCount = boundWitness?.payload_schemas.filter((schema) => schema !== XyoBoundWitnessSchema).length ?? 0
+    const boundWitnessCount = boundWitness?.payload_schemas.filter((schema) => schema === XyoBoundWitnessSchema).length ?? 0
+    const signers = boundWitness?.addresses.length ?? 0
 
-  const payloadCount = boundWitness?.payload_schemas.filter((schema) => schema !== XyoBoundWitnessSchema).length ?? 0
-  const boundWitnessCount = boundWitness?.payload_schemas.filter((schema) => schema === XyoBoundWitnessSchema).length ?? 0
-  const signers = boundWitness?.addresses.length ?? 0
-
-  return (
-    <PaperNavWrapper elevation={3} ref={ref} {...props}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(_event, newValue) => {
-          setValue(newValue)
-        }}
-      >
-        <BottomNavigationAction label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} />
-        <BottomNavigationAction label={`Bound Witnesses - ${boundWitnessCount}`} icon={<VscSymbolMethod />} />
-        <BottomNavigationAction label={`Signatures - ${signers}`} icon={<FaSignature />} />
-      </BottomNavigation>
-    </PaperNavWrapper>
-  )
-})
+    return (
+      <PaperNavWrapper elevation={3} ref={ref} {...props}>
+        <BottomNavigation
+          showLabels
+          value={activeTab}
+          onChange={(_event, newValue) => {
+            setActiveTab?.(newValue)
+          }}
+        >
+          <BottomNavigationAction label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} />
+          <BottomNavigationAction label={`Bound Witnesses - ${boundWitnessCount}`} icon={<VscSymbolMethod />} />
+          <BottomNavigationAction label={`Signatures - ${signers}`} icon={<FaSignature />} />
+        </BottomNavigation>
+      </PaperNavWrapper>
+    )
+  },
+)
 
 BoundWitnessCardBottomNavigation.displayName = 'BoundWitnessCardBottomNavigation'
 export { BoundWitnessCardBottomNavigation }
