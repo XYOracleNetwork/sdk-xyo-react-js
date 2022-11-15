450f4b758c369549873554ae2cbbb9e3e677f80c
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-15 14:59:48 -0500

    remove unnecessary paper

--- packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.tsx
@@ -1,10 +1,10 @@
-import { BottomNavigation, BottomNavigationAction, Paper, PaperProps, styled } from '@mui/material'
+import { BottomNavigation, BottomNavigationAction, BottomNavigationProps, styled } from '@mui/material'
 import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
 import { Dispatch, forwardRef, SetStateAction } from 'react'
 import { FaSignature } from 'react-icons/fa'
 import { VscSymbolMethod, VscSymbolNamespace } from 'react-icons/vsc'
 
-export interface BoundWitnessCardBottomNavigationProps extends PaperProps {
+export interface BoundWitnessCardBottomNavigationProps extends BottomNavigationProps {
   boundWitness?: XyoBoundWitness
   activeTab?: number
   setActiveTab?: Dispatch<SetStateAction<number>>
@@ -17,19 +17,19 @@ const BoundWitnessCardBottomNavigation = forwardRef<HTMLDivElement, BoundWitness
     const signers = boundWitness?.addresses.length ?? 0
 
     return (
-      <PaperNavWrapper elevation={3} ref={ref} {...props}>
-        <BottomNavigation
-          showLabels
-          value={activeTab}
-          onChange={(_event, newValue) => {
-            setActiveTab?.(newValue)
-          }}
-        >
-          <BottomNavigationAction label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} />
-          <BottomNavigationAction label={`Bound Witnesses - ${boundWitnessCount}`} icon={<VscSymbolMethod />} />
-          <BottomNavigationAction label={`Signatures - ${signers}`} icon={<FaSignature />} />
-        </BottomNavigation>
-      </PaperNavWrapper>
+      <BottomNavigationStyled
+        showLabels
+        value={activeTab}
+        onChange={(_event, newValue) => {
+          setActiveTab?.(newValue)
+        }}
+        ref={ref}
+        {...props}
+      >
+        <BottomNavigationAction label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} />
+        <BottomNavigationAction label={`Bound Witnesses - ${boundWitnessCount}`} icon={<VscSymbolMethod />} />
+        <BottomNavigationAction label={`Signatures - ${signers}`} icon={<FaSignature />} />
+      </BottomNavigationStyled>
     )
   },
 )
@@ -37,6 +37,6 @@ const BoundWitnessCardBottomNavigation = forwardRef<HTMLDivElement, BoundWitness
 BoundWitnessCardBottomNavigation.displayName = 'BoundWitnessCardBottomNavigation'
 export { BoundWitnessCardBottomNavigation }
 
-const PaperNavWrapper = styled(Paper, { name: 'PaperNavWrapper' })(({ theme }) => ({
+const BottomNavigationStyled = styled(BottomNavigation, { name: 'BottomNavigationStyled' })(({ theme }) => ({
   borderRadius: `0 0 ${theme.shape.borderRadius}px ${theme.shape.borderRadius}px`,
 }))
