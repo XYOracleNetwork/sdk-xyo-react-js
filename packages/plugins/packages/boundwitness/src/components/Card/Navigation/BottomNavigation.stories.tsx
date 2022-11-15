4aede5cdf8d517e5d7af86b7bad8c4adbf195bd5
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-15 13:59:56 -0500

    add counts and update story

--- packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.stories.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentStory, Meta } from '@storybook/react'
+import { FlexGrowRow } from '@xylabs/react-flexbox'
+import { sampleBlock } from '@xyo-network/react-storybook'
 
 import { BoundWitnessCardBottomNavigation } from './BottomNavigation'
 
@@ -9,10 +11,14 @@ export default {
 } as Meta
 
 const Template: ComponentStory<typeof BoundWitnessCardBottomNavigation> = (props) => {
-  return <BoundWitnessCardBottomNavigation {...props} />
+  return (
+    <FlexGrowRow height="calc(100vh - 20px)">
+      <BoundWitnessCardBottomNavigation {...props} />
+    </FlexGrowRow>
+  )
 }
 
 const Default = Template.bind({})
-Default.args = {}
+Default.args = { boundWitness: sampleBlock }
 
 export { Default }
--- packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/Navigation/BottomNavigation.tsx
@@ -1,10 +1,20 @@
 import { BottomNavigation, BottomNavigationAction, Paper, PaperProps, styled } from '@mui/material'
+import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
 import { useState } from 'react'
 import { FaSignature } from 'react-icons/fa'
 import { VscSymbolMethod, VscSymbolNamespace } from 'react-icons/vsc'
 
-export const BoundWitnessCardBottomNavigation: React.FC<PaperProps> = (props) => {
+export interface BoundWitnessCardBottomNavigationProps extends PaperProps {
+  boundWitness?: XyoBoundWitness
+}
+
+export const BoundWitnessCardBottomNavigation: React.FC<BoundWitnessCardBottomNavigationProps> = ({ boundWitness, ...props }) => {
   const [value, setValue] = useState(0)
+
+  const payloadCount = boundWitness?.payload_schemas.filter((schema) => schema !== XyoBoundWitnessSchema).length ?? 0
+  const boundWitnessCount = boundWitness?.payload_schemas.filter((schema) => schema === XyoBoundWitnessSchema).length ?? 0
+  const signers = boundWitness?.addresses.length ?? 0
+
   return (
     <PaperNavWrapper elevation={3} {...props}>
       <BottomNavigation
@@ -14,9 +24,9 @@ export const BoundWitnessCardBottomNavigation: React.FC<PaperProps> = (props) =>
           setValue(newValue)
         }}
       >
-        <BottomNavigationAction label="Payloads" icon={<VscSymbolNamespace />} />
-        <BottomNavigationAction label="Bound Witnesses" icon={<VscSymbolMethod />} />
-        <BottomNavigationAction label="Signatures" icon={<FaSignature />} />
+        <BottomNavigationAction label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} />
+        <BottomNavigationAction label={`Bound Witnesses - ${boundWitnessCount}`} icon={<VscSymbolMethod />} />
+        <BottomNavigationAction label={`Signatures - ${signers}`} icon={<FaSignature />} />
       </BottomNavigation>
     </PaperNavWrapper>
   )
