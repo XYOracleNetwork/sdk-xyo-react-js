26a99c842c6e747b62863d4b5aa09387bb8c9001
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-16 09:13:49 -0500

    story for different types of event nouns

--- packages/plugins/packages/boundwitness/src/components/Card/CardExpanded.stories.tsx
+++ packages/plugins/packages/boundwitness/src/components/Card/CardExpanded.stories.tsx
@@ -1,7 +1,17 @@
-import { ComponentStory, Meta } from '@storybook/react'
+import { ComponentStory, DecoratorFn, Meta } from '@storybook/react'
+import { useXyoEvent } from '@xyo-network/react-event'
 import { sampleBlockWithBoundWitnessPayload } from '@xyo-network/react-storybook'
+import { useRef } from 'react'
 
 import { BoundWitnessRendererCardExpanded } from './CardExpanded'
+
+const WithEventDecorator: DecoratorFn = (Story, args) => {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const [tableRef] = useXyoEvent<HTMLDivElement>((noun, verb, data) => console.log(`${noun}|${verb}|${data}`), ref)
+  args.args.ref = tableRef
+
+  return <Story {...args} />
+}
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -14,7 +24,11 @@ const Template: ComponentStory<typeof BoundWitnessRendererCardExpanded> = (props
 const Default = Template.bind({})
 Default.args = { payload: sampleBlockWithBoundWitnessPayload }
 
+const WithRef = Template.bind({})
+WithRef.decorators = [WithEventDecorator]
+WithRef.args = { payload: sampleBlockWithBoundWitnessPayload }
+
 const WithVisibleRows = Template.bind({})
 WithVisibleRows.args = { payload: sampleBlockWithBoundWitnessPayload, visibleRows: 4 }
 
-export { Default, WithVisibleRows }
+export { Default, WithRef, WithVisibleRows }
