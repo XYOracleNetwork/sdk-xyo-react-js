161e7a0282244ed4097415dba3974d66b1fd06ed
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-23 14:58:55 -0500

    optionally hide border when no data exists

--- packages/sdk/packages/payload/packages/table/src/components/Table/TableRowNoData.tsx
+++ packages/sdk/packages/payload/packages/table/src/components/Table/TableRowNoData.tsx
@@ -1,17 +1,26 @@
-import { TableCell, TableRow, TableRowProps, Typography, TypographyProps } from '@mui/material'
+import { styled, TableCell, TableRow, TableRowProps, Typography, TypographyProps } from '@mui/material'
 
 export interface TableRowNoDataProps extends TableRowProps {
   typographyProps?: TypographyProps
+  hideBorder?: boolean
 }
 
-export const TableRowNoData: React.FC<TableRowNoDataProps> = ({ typographyProps, ...props }) => {
+export const TableRowNoData: React.FC<TableRowNoDataProps> = ({ hideBorder = false, typographyProps, ...props }) => {
   return (
     <TableRow {...props}>
-      <TableCell sx={{ border: 'none' }}>
+      <StyledTableCell hideBorder={hideBorder}>
         <Typography variant="body2" {...typographyProps}>
           No Data To Display...
         </Typography>
-      </TableCell>
+      </StyledTableCell>
     </TableRow>
   )
 }
+
+interface StyledTableCellProps {
+  hideBorder?: boolean
+}
+
+const StyledTableCell = styled(TableCell, { name: 'StyledTableCell' })<StyledTableCellProps>(({ hideBorder }) => ({
+  ...(hideBorder && { border: 'none' }),
+}))
