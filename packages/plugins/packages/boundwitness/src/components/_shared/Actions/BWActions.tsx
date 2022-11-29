cac0cfa35d9eedc5b5381cf860db78472966b20a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-29 16:40:21 -0500

    add previous hash to bw detail actions

--- packages/plugins/packages/boundwitness/src/components/_shared/Actions/BWActions.tsx
+++ packages/plugins/packages/boundwitness/src/components/_shared/Actions/BWActions.tsx
@@ -1,3 +1,4 @@
+import LinkRoundedIcon from '@mui/icons-material/LinkRounded'
 import { FlexBoxProps, FlexRow } from '@xylabs/react-flexbox'
 import { QuickTipButton } from '@xylabs/react-quick-tip-button'
 import { ellipsize } from '@xylabs/sdk-js'
@@ -10,14 +11,27 @@ import { BWVerification } from './Verification'
 export interface BWActionsProps extends FlexBoxProps {
   hideJSONButton?: boolean
   hideValidation?: boolean
+  hidePreviousHash?: boolean
   boundwitness?: XyoBoundWitness
   additionalActions?: ReactNode
 }
 
-export const BWActions: React.FC<BWActionsProps> = ({ additionalActions, hideJSONButton, hideValidation, boundwitness, ...props }) => {
+export const BWActions: React.FC<BWActionsProps> = ({
+  additionalActions,
+  hideJSONButton,
+  hideValidation,
+  hidePreviousHash,
+  boundwitness,
+  ...props
+}) => {
   const { hash } = boundwitness ? new PayloadWrapper(boundwitness) : { hash: '' }
+  const previousHash = boundwitness?.previous_hashes[0] ?? null
+  const formattedPreviousHash = previousHash === null ? 'No Previous Hash' : `Previous Hash - ${ellipsize(previousHash, 8)}`
   return (
     <FlexRow {...props}>
+      {hidePreviousHash && boundwitness?.previous_hashes.length ? null : (
+        <QuickTipButton Icon={LinkRoundedIcon} disableDialog hoverText={formattedPreviousHash} />
+      )}
       {hideJSONButton ? null : <BWVerification boundwitness={boundwitness} />}
       {hideValidation ? null : (
         <QuickTipButton title={`JSON for ${ellipsize(hash, 8)}`} dialogProps={{ fullWidth: true, maxWidth: 'md' }}>
