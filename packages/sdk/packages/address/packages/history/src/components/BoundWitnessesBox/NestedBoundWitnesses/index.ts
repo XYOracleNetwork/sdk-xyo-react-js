a05d5fea93b120d584cee3f0195c94c5c9fec2f9
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-11-29 14:14:13 -0500

    decouple glowing component from nested boundwitnesses

--- packages/sdk/packages/address/packages/history/src/components/BoundWitnessesBox/NestedBoundWitnesses/index.ts
+++ packages/sdk/packages/address/packages/history/src/components/BoundWitnessesBox/NestedBoundWitnesses/index.ts
@@ -1,2 +1,1 @@
-export * from './NestedBoundWitnessBox'
 export * from './NestedBoundWitnessesBox'
--- packages/sdk/packages/address/packages/history/src/components/BoundWitnessesBox/index.ts
+++ packages/sdk/packages/address/packages/history/src/components/BoundWitnessesBox/index.ts
@@ -1,4 +1,5 @@
 export * from './ActiveBoundWitness'
 export * from './BoundWitnessBoxWithAddressRouter'
 export * from './BoundWitnessesBox'
+export * from './layout'
 export * from './NestedBoundWitnesses'
