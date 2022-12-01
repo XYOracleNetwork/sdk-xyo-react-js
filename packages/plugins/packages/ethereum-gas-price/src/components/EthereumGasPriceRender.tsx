62ea22166ff4ed0d009599023cdbf5196b5ccff1
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-01 16:33:55 -0500

    box layout

--- packages/plugins/packages/ethereum-gas-price/src/components/EthereumGasPriceRender.tsx
+++ packages/plugins/packages/ethereum-gas-price/src/components/EthereumGasPriceRender.tsx
@@ -1,17 +1,45 @@
-import { FlexBoxProps, FlexCol } from '@xylabs/react-flexbox'
+import { Chip, Grid, useTheme } from '@mui/material'
+import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/react-flexbox'
 import { XyoPayloadDetailsRenderProps } from '@xyo-network/react-payload-plugin'
-import { PayloadDataMissing } from '@xyo-network/react-shared'
+import { PayloadDataMissing, TypographyEx, useGetTokenData } from '@xyo-network/react-shared'
 import isEmpty from 'lodash/isEmpty'
 
-import { FeeDataPayload } from './lib'
+import { GasFeeCard } from './Fees'
+import { FeeDataPayload, FeePerGasValues } from './lib'
 
 export const EthereumGasPriceRender: React.FC<XyoPayloadDetailsRenderProps & FlexBoxProps> = ({ ...props }) => {
-  const { payload } = props
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { payload, listMode, ...flexProps } = props
+  const theme = useTheme()
   const gasPricePayload = payload ? (payload as FeeDataPayload) : undefined
 
-  if ([!isEmpty(gasPricePayload?.feePerGas), !isEmpty(gasPricePayload?.priorityFeePerGas)].some(Boolean)) {
+  const [ethData] = useGetTokenData(['eth'])
+
+  if ([isEmpty(gasPricePayload?.feePerGas), isEmpty(gasPricePayload?.priorityFeePerGas)].some(Boolean)) {
     return <PayloadDataMissing alertBody="Payload is missing valid gas fee data." />
   }
 
-  return <FlexCol {...props}></FlexCol>
+  return (
+    <FlexCol alignItems="start" rowGap={4} {...flexProps}>
+      <FlexRow justifyItems="space-between" alignItems="end" columnGap={2} rowGap={2} flexWrap="wrap" width="100%" justifyContent="space-between">
+        <TypographyEx fontSize={theme.spacing(6)} lineHeight={1}>
+          Gas Fee Estimate <img height={theme.spacing(4)} src={ethData.icon} />
+        </TypographyEx>
+        <FlexRow columnGap={1}>
+          {gasPricePayload?.timestamp ? <Chip label={gasPricePayload.timestamp} /> : null}
+          {gasPricePayload?.baseFee ? <Chip label={`Base Fee - ${gasPricePayload.baseFee}`} /> : null}
+        </FlexRow>
+      </FlexRow>
+      <FlexRow columnGap={2} flexWrap="wrap" rowGap={2} width="100%" justifyContent="space-between">
+        <Grid container spacing={3}>
+          {gasPricePayload &&
+            FeePerGasValues.map((value) => (
+              <Grid key={value} item md={6} lg={3}>
+                <GasFeeCard gasPrice={gasPricePayload?.feePerGas[value]} priorityFee={gasPricePayload.priorityFeePerGas[value]} speed={value} />
+              </Grid>
+            ))}
+        </Grid>
+      </FlexRow>
+    </FlexCol>
+  )
 }
--- packages/plugins/packages/ethereum-gas-price/src/components/lib/FeePerGasToSpeed.ts
+++ packages/plugins/packages/ethereum-gas-price/src/components/lib/FeePerGasToSpeed.ts
@@ -3,6 +3,8 @@ import { FeePerGas } from '@xyo-network/gas-price-payload-plugin'
 export type FeePerGasValue = keyof FeePerGas
 export type Speed = 'slowest' | 'slow' | 'fast' | 'fastest'
 
+export const FeePerGasValues: FeePerGasValue[] = ['low', 'medium', 'high', 'veryHigh']
+
 export const FeePerGasToSpeed: Record<keyof FeePerGas, Speed> = {
   high: 'fast',
   low: 'slowest',
