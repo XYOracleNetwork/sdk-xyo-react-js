36507911fdc6fcd8eeab463bed29a431ae241b48
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-01 17:58:13 -0500

    add quicktip for more info

--- packages/plugins/packages/ethereum-gas-price/src/components/EthereumGasPriceRender.tsx
+++ packages/plugins/packages/ethereum-gas-price/src/components/EthereumGasPriceRender.tsx
@@ -1,11 +1,24 @@
-import { Chip, Grid, useTheme } from '@mui/material'
+import { Chip, Grid, Link, useTheme } from '@mui/material'
 import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/react-flexbox'
+import { QuickTipButton } from '@xylabs/react-quick-tip-button'
 import { XyoPayloadDetailsRenderProps } from '@xyo-network/react-payload-plugin'
 import { PayloadDataMissing, TypographyEx, useGetTokenData } from '@xyo-network/react-shared'
 import isEmpty from 'lodash/isEmpty'
 
 import { GasFeeCard } from './Fees'
 import { FeeDataPayload, FeePerGasValues } from './lib'
+
+const DivinationDescription: React.FC = () => (
+  <QuickTipButton hoverText="More Info">
+    <TypographyEx>
+      Ethereum gas price estimates derived by combining pricing from Blocknative, Etherchain, Ethers, and Etherscan. Learn more about recent changes
+      to gas fees on Ethereum{' '}
+      <Link href="https://www.blocknative.com/blog/eip-1559-fees" target="_blank" rel="nofollow">
+        here.
+      </Link>
+    </TypographyEx>
+  </QuickTipButton>
+)
 
 export const EthereumGasPriceRender: React.FC<XyoPayloadDetailsRenderProps & FlexBoxProps> = ({ ...props }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -23,7 +36,10 @@ export const EthereumGasPriceRender: React.FC<XyoPayloadDetailsRenderProps & Fle
     <FlexCol alignItems="start" rowGap={4} {...flexProps}>
       <FlexRow justifyItems="space-between" alignItems="end" columnGap={2} rowGap={2} flexWrap="wrap" width="100%" justifyContent="space-between">
         <TypographyEx fontSize={theme.spacing(6)} lineHeight={1}>
-          Gas Fee Estimate <img height={theme.spacing(4)} src={ethData.icon} />
+          Gas Fee Estimate <img height={theme.spacing(4)} src={ethData.icon} />{' '}
+          <span style={{ lineHeight: 0, verticalAlign: 'super' }}>
+            <DivinationDescription />
+          </span>
         </TypographyEx>
         <FlexRow columnGap={1}>
           {gasPricePayload?.timestamp ? <Chip label={new Date(gasPricePayload.timestamp).toLocaleString()} /> : null}
