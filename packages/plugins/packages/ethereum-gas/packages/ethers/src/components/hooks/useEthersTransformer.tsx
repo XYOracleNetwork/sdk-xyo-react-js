91659765bcea1d5359e47b50dfb4dda42d44cfe6
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-08 15:46:58 -0500

    transform hook

---
+++ packages/plugins/packages/ethereum-gas/packages/ethers/src/components/hooks/useEthersTransformer.tsx
@@ -0,0 +1,35 @@
+import { XyoEthereumGasEthersPayload } from '@xyo-network/ethers-ethereum-gas-payload-plugin'
+import { GasPriceWitnessUIBasePayload } from '@xyo-network/react-gas-price'
+
+const calculateGasPrice = (maxFeePerGas?: number | null, maxPriorityFeePerGas?: number | null) => {
+  if (!maxFeePerGas || !maxPriorityFeePerGas) {
+    return
+  }
+
+  return (maxFeePerGas - maxPriorityFeePerGas) / 1_000_000_000
+}
+
+export const useEthersTransformer = (payload?: XyoEthereumGasEthersPayload): GasPriceWitnessUIBasePayload | undefined => {
+  if (payload) {
+    return {
+      baseFee: {
+        label: 'lastBaseFeePerGas',
+        value: payload.lastBaseFeePerGas ? payload.lastBaseFeePerGas / 1_000_000_000 : undefined,
+      },
+      gasPrice: [
+        {
+          price: {
+            label: 'maxFeePerGas',
+            value: calculateGasPrice(payload?.maxFeePerGas, payload?.maxFeePerGas),
+          },
+          priorityFee: {
+            label: 'maxPriorityFeePerGas',
+            value: payload.maxPriorityFeePerGas ? payload.maxPriorityFeePerGas / 1_000_000_000 : undefined,
+          },
+        },
+      ],
+      timestamp: payload.timestamp,
+      website: 'https://docs.ethers.io/v5/api/providers/provider/#Provider-getFeeData',
+    }
+  }
+}
