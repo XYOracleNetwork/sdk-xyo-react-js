b95b7057a756e9a3d5d6bbe0513d34ab6f36da59
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-08 08:39:23 -0500

    cleanup

--- packages/plugins/packages/ethereum-gas-price/src/components/lib/FeeDataPayload.ts
+++
@@ -1,4 +0,0 @@
-import { FeeData } from '@xyo-network/gas-price-payload-plugin'
-import { XyoPayload } from '@xyo-network/payload'
-
-export type FeeDataPayload = XyoPayload<FeeData>
--- packages/plugins/packages/ethereum-gas-price/src/components/lib/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './FeeDataPayload'
-export * from './FeePerGasToSpeed'
