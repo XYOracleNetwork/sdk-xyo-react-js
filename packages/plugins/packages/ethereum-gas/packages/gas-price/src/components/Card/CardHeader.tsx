47e64781f05c3defceeda787a070f0362dc46fd7
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-08 16:46:50 -0500

    fix type error

--- packages/plugins/packages/ethereum-gas/packages/gas-price/src/components/Card/CardHeader.tsx
+++ packages/plugins/packages/ethereum-gas/packages/gas-price/src/components/Card/CardHeader.tsx
@@ -1,5 +1,5 @@
-import { CardHeader, CardHeaderProps } from '@mui/material'
-import { GasPriceHeaderActionsBox, GasPriceHeaderTypography } from '@xyo-network/react-gas-price'
+import { CardHeaderProps } from '@mui/material'
+import { GasPriceHeaderActionsBox, GasPriceHeaderTypography, StyledCardHeader } from '@xyo-network/react-gas-price'
 import { XyoPayloadRenderProps } from '@xyo-network/react-payload-plugin'
 import { forwardRef } from 'react'
 
@@ -9,13 +9,13 @@ export const EthereumGasPriceCardHeader = forwardRef<HTMLDivElement, XyoPayloadR
   const gasPricePayload = payload ? (payload as FeeDataPayload) : undefined
 
   return (
-    <CardHeader
+    <StyledCardHeader
       title={<GasPriceHeaderTypography heading="Gas Fee Estimate" />}
       action={
         <GasPriceHeaderActionsBox
           timestamp={gasPricePayload?.timestamp}
           baseFee={gasPricePayload?.baseFee}
-          blockNumber={gasPricePayload?.blockNumber ? parseInt(gasPricePayload?.blockNumber) : null}
+          blockNumber={gasPricePayload?.blockNumber ? parseInt(gasPricePayload?.blockNumber) : undefined}
         />
       }
     />
