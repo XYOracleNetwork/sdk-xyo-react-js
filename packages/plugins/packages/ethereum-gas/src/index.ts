4cbdf6361a268e20abe1460c263a6a9b7daf8ca5
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-08 15:15:22 -0500

    link parent packages

--- packages/plugins/packages/ethereum-gas/src/index.ts
+++ packages/plugins/packages/ethereum-gas/src/index.ts
@@ -1,8 +1,13 @@
+import { EthereumGasPriceEthersPlugin } from '@xyo-network/react-ethereum-gas-price-ethers-plugin'
 import { EthereumGasPriceEtherscanPlugin } from '@xyo-network/react-ethereum-gas-price-etherscan-plugin'
 import { EthereumGasPricePlugin } from '@xyo-network/react-ethereum-gas-price-plugin'
 import { XyoPayloadRenderPlugin } from '@xyo-network/react-payload-plugin'
 
-export const EthereumGasPricePayloadPlugins: XyoPayloadRenderPlugin[] = [EthereumGasPricePlugin, EthereumGasPriceEtherscanPlugin]
+export const EthereumGasPricePayloadPlugins: XyoPayloadRenderPlugin[] = [
+  EthereumGasPricePlugin,
+  EthereumGasPriceEtherscanPlugin,
+  EthereumGasPriceEthersPlugin,
+]
 
 // eslint-disable-next-line import/no-default-export
 export default EthereumGasPricePayloadPlugins
