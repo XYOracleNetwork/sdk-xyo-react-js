78c565657194c45635b13036abcdb7069ee8ee7d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-09 14:50:37 -0500

    strongly type export

--- packages/plugins/packages/ethereum-gas/packages/etherchain/src/index.ts
+++ packages/plugins/packages/ethereum-gas/packages/etherchain/src/index.ts
@@ -1,10 +1,11 @@
 import { EthereumGasPriceEtherchainV1Plugin } from '@xyo-network/react-ethereum-gas-price-etherchain-v1-plugin'
+import { XyoPayloadRenderPlugin } from '@xyo-network/react-payload-plugin'
 // import { XyoEthereumGasEtherchainV2PayloadPlugin } from '@xyo-network/etherchain-ethereum-gas-v2-payload-plugin'
 
 // export * from '@xyo-network/etherchain-ethereum-gas-v1-payload-plugin'
 // export * from '@xyo-network/etherchain-ethereum-gas-v2-payload-plugin'
 
-export const EthereumGasPriceEtherchainPlugins = [EthereumGasPriceEtherchainV1Plugin]
+export const EthereumGasPriceEtherchainPlugins: XyoPayloadRenderPlugin[] = [EthereumGasPriceEtherchainV1Plugin]
 
 // eslint-disable-next-line import/no-default-export
 export default EthereumGasPriceEtherchainPlugins
