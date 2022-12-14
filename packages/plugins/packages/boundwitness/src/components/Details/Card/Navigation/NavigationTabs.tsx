027b12865672d9e246365b5152162c489327bc70
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-14 10:35:29 -0500

    style active tabs

--- packages/plugins/packages/boundwitness/src/components/Details/Card/Navigation/NavigationTabs.tsx
+++ packages/plugins/packages/boundwitness/src/components/Details/Card/Navigation/NavigationTabs.tsx
@@ -1,4 +1,4 @@
-import { styled, Tab, Tabs, TabsProps } from '@mui/material'
+import { Paper, Tab, TabProps, Tabs, TabsProps, useTheme } from '@mui/material'
 import { XyoBoundWitness, XyoBoundWitnessSchema } from '@xyo-network/boundwitness'
 import { Dispatch, SetStateAction, SyntheticEvent } from 'react'
 import { FaSignature } from 'react-icons/fa'
@@ -14,23 +14,28 @@ export const BWNavigationTabs: React.FC<BWNavigationTabs> = ({ setValue, value, 
   const payloadCount = boundWitness?.payload_schemas.filter((schema) => schema !== XyoBoundWitnessSchema).length ?? 0
   const boundWitnessCount = boundWitness?.payload_schemas.filter((schema) => schema === XyoBoundWitnessSchema).length ?? 0
   const signers = boundWitness?.addresses.length ?? 0
+  const theme = useTheme()
 
   const handleChange = (_event: SyntheticEvent, newValue: number) => {
     setValue?.(newValue)
   }
 
+  const tabProps: TabProps = {
+    iconPosition: 'start',
+    sx: { boxShadow: 'none', fontSize: theme.spacing(1.75), justifyContent: 'start', lineHeight: 3, minHeight: 0, overflow: 'hidden' },
+  }
+
   return (
     <Tabs orientation="vertical" value={value} onChange={handleChange} aria-label="BW Vertical tabs" {...props}>
-      <StyledTab label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} iconPosition={'start'} />
-      <StyledTab label={`Bound Witnesses - ${boundWitnessCount}`} icon={<VscSymbolMethod />} iconPosition={'start'} />
-      <StyledTab label={`Signatures - ${signers}`} icon={<FaSignature />} iconPosition={'start'} />
+      <Tab component={Paper} elevation={value === 0 ? 3 : 0} {...tabProps} label={`Payloads - ${payloadCount}`} icon={<VscSymbolNamespace />} />
+      <Tab
+        component={Paper}
+        elevation={value === 1 ? 3 : 0}
+        {...tabProps}
+        label={`Bound Witnesses - ${boundWitnessCount}`}
+        icon={<VscSymbolMethod />}
+      />
+      <Tab component={Paper} elevation={value === 2 ? 3 : 0} {...tabProps} label={`Signatures - ${signers}`} icon={<FaSignature />} />
     </Tabs>
   )
 }
-
-const StyledTab = styled(Tab, { name: 'StyledTab' })(({ theme }) => ({
-  fontSize: theme.spacing(1.75),
-  justifyContent: 'start',
-  lineHeight: 3,
-  minHeight: 0,
-}))
