0a154f720413dfe658a5c0c2c0d8b4bddee51874
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-14 15:36:43 -0500

    bw renderer design changes (#587)
    * add tabbed navigation option
    
    * spacing for better readability
    
    * extract styles and add collapsed state
    
    * tab list styling
    
    * smoother collapse
    
    * style active tabs
    
    * stretch dividing border
    
    * transition in bottom nav when collapsed
    
    * ditch card header bg color
    
    * card header padding
    
    * working visible rows
    
    * extract card content

--- packages/plugins/packages/boundwitness/src/components/Details/Card/DetailsCard.tsx
+++ packages/plugins/packages/boundwitness/src/components/Details/Card/DetailsCard.tsx
@@ -1,15 +1,13 @@
 import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded'
 import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
-import { Card, CardProps, Collapse, Divider, IconButton } from '@mui/material'
-import { FlexCol } from '@xylabs/react-flexbox'
+import { Card, CardProps, Divider, IconButton } from '@mui/material'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { XyoPayloadDetailsRenderProps } from '@xyo-network/react-payload-plugin'
-import { TableHeightProvider, useTableHeight } from '@xyo-network/react-table'
+import { TableHeightProvider } from '@xyo-network/react-table'
 import { forwardRef, useEffect, useState } from 'react'
 
-import { BoundWitnessPayloadsTable, BoundWitnessPayloadsTableForBWs, BoundWitnessSignatureTable } from '../../_shared'
 import { BoundWitnessCardHeader } from '../../Card'
-import { BoundWitnessBottomNavigation } from './Navigation'
+import { DetailsCardContent } from './DetailsCardContent'
 
 const BoundWitnessDetailsCard = forwardRef<HTMLDivElement, XyoPayloadDetailsRenderProps & CardProps>(({ visibleRows, ...props }, ref) => {
   return (
@@ -27,8 +25,6 @@ const BoundWitnessDetailsCardInner = forwardRef<HTMLDivElement, XyoPayloadDetail
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ({ payload, active: activeProp, listMode, visibleRows, ...props }, ref) => {
     const boundwitness = payload as XyoBoundWitness
-    const { height } = useTableHeight()
-    const [activeTab, setActiveTab] = useState(0)
     const [collapsed, setCollapsed] = useState(!activeProp)
 
     useEffect(() => {
@@ -52,21 +48,7 @@ const BoundWitnessDetailsCardInner = forwardRef<HTMLDivElement, XyoPayloadDetail
           }
           sx={{ columnGap: 2 }}
         />
-        <FlexCol alignItems="stretch" ref={ref} {...props}>
-          <Collapse in={collapsed}>
-            <FlexCol alignItems="stretch" justifyContent="start" height={height !== undefined ? height : 'auto'} overflow="scroll">
-              {activeTab === 0 ? <BoundWitnessPayloadsTable boundwitness={boundwitness} variant="scrollable" /> : null}
-              {activeTab === 1 ? <BoundWitnessPayloadsTableForBWs boundwitness={boundwitness} variant="scrollable" /> : null}
-              {activeTab === 2 ? <BoundWitnessSignatureTable block={boundwitness} variant="scrollable" /> : null}
-            </FlexCol>
-          </Collapse>
-          <BoundWitnessBottomNavigation
-            onClick={() => setCollapsed(true)}
-            activeTab={activeTab}
-            setActiveTab={setActiveTab}
-            boundWitness={boundwitness}
-          />
-        </FlexCol>
+        <DetailsCardContent boundwitness={boundwitness} collapsed={collapsed} setCollapsed={setCollapsed} ref={ref} />
       </Card>
     )
   },
