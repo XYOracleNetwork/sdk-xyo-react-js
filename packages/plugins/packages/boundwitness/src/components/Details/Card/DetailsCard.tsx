a10bf8f3a8d3af959e211d030c8e0d6edc8ea24f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-14 15:28:09 -0500

    extract card content

--- packages/plugins/packages/boundwitness/src/components/Details/Card/DetailsCard.tsx
+++ packages/plugins/packages/boundwitness/src/components/Details/Card/DetailsCard.tsx
@@ -1,15 +1,13 @@
 import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded'
 import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
-import { Card, CardProps, Collapse, Divider, IconButton } from '@mui/material'
-import { FlexGrowCol, FlexRow } from '@xylabs/react-flexbox'
+import { Card, CardProps, Divider, IconButton } from '@mui/material'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
 import { XyoPayloadDetailsRenderProps } from '@xyo-network/react-payload-plugin'
-import { TableHeightProvider, useTableHeight } from '@xyo-network/react-table'
+import { TableHeightProvider } from '@xyo-network/react-table'
 import { forwardRef, useEffect, useState } from 'react'
 
-import { BoundWitnessPayloadsTable, BoundWitnessPayloadsTableForBWs, BoundWitnessSignatureTable } from '../../_shared'
 import { BoundWitnessCardHeader } from '../../Card'
-import { BoundWitnessBottomNavigation, BWNavigationTabs } from './Navigation'
+import { DetailsCardContent } from './DetailsCardContent'
 
 const BoundWitnessDetailsCard = forwardRef<HTMLDivElement, XyoPayloadDetailsRenderProps & CardProps>(({ visibleRows, ...props }, ref) => {
   return (
@@ -27,8 +25,6 @@ const BoundWitnessDetailsCardInner = forwardRef<HTMLDivElement, XyoPayloadDetail
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ({ payload, active: activeProp, listMode, visibleRows, ...props }, ref) => {
     const boundwitness = payload as XyoBoundWitness
-    const { height } = useTableHeight()
-    const [activeTab, setActiveTab] = useState(0)
     const [collapsed, setCollapsed] = useState(!activeProp)
 
     useEffect(() => {
@@ -52,26 +48,7 @@ const BoundWitnessDetailsCardInner = forwardRef<HTMLDivElement, XyoPayloadDetail
           }
           sx={{ columnGap: 2 }}
         />
-        <FlexGrowCol alignItems="stretch" height={height !== undefined && collapsed ? height : 'auto'}>
-          <Collapse in={collapsed}>
-            <FlexRow alignItems="stretch" height={height !== undefined && collapsed ? height : 'auto'} ref={ref} {...props}>
-              <BWNavigationTabs value={activeTab} setValue={setActiveTab} boundWitness={boundwitness} />
-              <FlexGrowCol justifyContent="start" overflow="scroll" sx={{ borderLeft: '1px solid', borderLeftColor: 'divider' }}>
-                {activeTab === 0 ? <BoundWitnessPayloadsTable boundwitness={boundwitness} variant="scrollable" /> : null}
-                {activeTab === 1 ? <BoundWitnessPayloadsTableForBWs boundwitness={boundwitness} variant="scrollable" /> : null}
-                {activeTab === 2 ? <BoundWitnessSignatureTable block={boundwitness} variant="scrollable" /> : null}
-              </FlexGrowCol>
-            </FlexRow>
-          </Collapse>
-        </FlexGrowCol>
-        <Collapse in={!collapsed} unmountOnExit>
-          <BoundWitnessBottomNavigation
-            onClick={() => setCollapsed(true)}
-            activeTab={activeTab}
-            setActiveTab={setActiveTab}
-            boundWitness={boundwitness}
-          />
-        </Collapse>
+        <DetailsCardContent boundwitness={boundwitness} collapsed={collapsed} setCollapsed={setCollapsed} ref={ref} />
       </Card>
     )
   },
