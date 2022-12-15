c46cbd37384b377342cb69187bbdc8f582ad1204
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-15 18:05:39 -0500

    design-review-work (#594)
    * fix calculations
    
    * slimmer font
    
    * remove background and padding from layout components
    
    * simplify layout grid
    
    * complete column spacing
    
    * add table cell ex with transparent bg by default
    
    * remove bg from sticky headers

--- packages/sdk/packages/address/packages/history/src/components/layout/Container.tsx
+++ packages/sdk/packages/address/packages/history/src/components/layout/Container.tsx
@@ -3,31 +3,22 @@ import { FlexBoxProps, FlexGrowCol } from '@xylabs/react-flexbox'
 import { WithChildren } from '@xylabs/react-shared'
 import { forwardRef } from 'react'
 
-const GridContainerWrap = styled(FlexGrowCol, { name: 'GridContainerWrap' })(({ theme }) => ({
-  // Handles the negative margins used by the MUI grid system
-  paddingBottom: theme.spacing(1),
-  paddingLeft: theme.spacing(4),
-  paddingRight: theme.spacing(1),
-  paddingTop: theme.spacing(4),
-}))
-
-const GridContainer = styled(Grid, { name: 'GridContainer' })(({ theme }) => ({
-  borderRadius: theme.spacing(1),
+const GridContainer = styled(Grid, { name: 'GridContainer' })(() => ({
   flexGrow: 1,
   height: '100%',
   overflow: 'hidden',
 }))
 
-interface WrappedContainerProps extends FlexBoxProps, WithChildren {
+export interface WrappedContainerProps extends FlexBoxProps, WithChildren {
   spacing?: number
 }
 
 const WrappedContainer = forwardRef<HTMLDivElement, WrappedContainerProps>(({ children, spacing, ...props }, ref) => (
-  <GridContainerWrap ref={ref} {...props}>
+  <FlexGrowCol ref={ref} {...props}>
     <GridContainer container spacing={spacing}>
       {children}
     </GridContainer>
-  </GridContainerWrap>
+  </FlexGrowCol>
 ))
 
 WrappedContainer.displayName = 'WrappedContainerWithRef'
