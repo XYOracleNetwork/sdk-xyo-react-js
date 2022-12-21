96caac75d288066fed446642ff4f9ba9da517f58
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-21 17:31:33 -0500

    useAddNamedModules hook

---
+++ packages/sdk/packages/node/src/contexts/useAddNamedModules.tsx
@@ -0,0 +1,68 @@
+import { useAsyncEffect } from '@xylabs/react-shared'
+import { ApiConfig } from '@xylabs/sdk-js'
+import { ArchivistWrapper } from '@xyo-network/archivist'
+import { DivinerWrapper } from '@xyo-network/diviner'
+import { HttpProxyModule } from '@xyo-network/http-proxy-module'
+import { AbstractModuleConfigSchema, Module, ModuleWrapper } from '@xyo-network/module'
+import { MemoryNode, NodeWrapper } from '@xyo-network/node'
+import { useState } from 'react'
+
+import { useNode } from './useNode'
+
+type ModuleList = Record<string, symbol>
+
+const addWrapper = (remoteModule: Module) => {
+  let wrapper: ModuleWrapper | undefined
+  if (remoteModule.config.schema.includes('archivist')) {
+    wrapper = new ArchivistWrapper(remoteModule)
+  } else if (remoteModule.config.schema.includes('divine')) {
+    wrapper = new DivinerWrapper(remoteModule)
+  }
+  return wrapper
+}
+
+const addToNode = (remoteModule: Module, name: string, memoryNode: MemoryNode) => {
+  const wrapper = addWrapper(remoteModule)
+  memoryNode.register(wrapper ?? remoteModule)
+  memoryNode.attach(remoteModule.address, name)
+  console.log('attached:', name)
+}
+
+export const useAddNamedModules = (moduleList?: ModuleList, apiConfig?: ApiConfig, reset = false) => {
+  const [node, setNode] = useNode<MemoryNode>()
+  const [complete, setComplete] = useState(false)
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async () => {
+      if (moduleList && apiConfig && node) {
+        let localNode = node
+        if (reset) {
+          localNode = await MemoryNode.create()
+        }
+        await Promise.allSettled(
+          Object.entries(moduleList).map(async ([_key, { description }]) => {
+            try {
+              const remoteModule = await HttpProxyModule.create({
+                apiConfig,
+                config: { schema: AbstractModuleConfigSchema },
+                name: description,
+              })
+
+              addToNode(remoteModule, description ?? 'unknown', localNode)
+              // using .assign instead of spread because we need to keep the prototype chain intact for root node / module methods
+              // const abstractNode = Object.assign(Object.create(Object.getPrototypeOf(node)), node)
+              const wrappedNode = new NodeWrapper(localNode)
+              setNode?.(wrappedNode)
+              setComplete(true)
+            } catch (error) {
+              console.log(error)
+            }
+          }),
+        )
+      }
+    },
+    [apiConfig, moduleList, node, reset, setNode],
+  )
+  return complete
+}
