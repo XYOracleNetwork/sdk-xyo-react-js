2f04e41050d4b41d9e8365dd46476b1bd31266bb
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-21 17:30:50 -0500

    fix dispatch type

--- packages/sdk/packages/node/src/contexts/useNode.ts
+++ packages/sdk/packages/node/src/contexts/useNode.ts
@@ -4,7 +4,7 @@ import { Dispatch } from 'react'
 
 import { NodeContext } from './Context'
 
-export const useNode = <T extends NodeModule = NodeModule>(): [T | undefined, Dispatch<AbstractNode> | undefined] => {
+export const useNode = <T extends NodeModule = NodeModule>(): [T | undefined, Dispatch<NodeModule> | undefined] => {
   const { node, setNode } = useContextEx(NodeContext, 'XyoNode')
 
   return [node as T, setNode]
