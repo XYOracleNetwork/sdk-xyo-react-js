99142fb662f9df33b83b2c6bda24aeb0af84e9fd
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-22 15:58:50 -0500

    query node for address history

--- packages/sdk/packages/address/packages/history/src/hooks/useAddressHistory.tsx
+++ packages/sdk/packages/address/packages/history/src/hooks/useAddressHistory.tsx
@@ -1,39 +1,18 @@
-import { useAsyncEffect } from '@xylabs/react-shared'
 import { XyoBoundWitness } from '@xyo-network/boundwitness'
-import { AddressHistoryQuerySchema, DivinerWrapper } from '@xyo-network/diviner'
-import { XyoError } from '@xyo-network/module'
-import { MemoryNode } from '@xyo-network/node'
-import { XyoPayloadBuilder } from '@xyo-network/payload'
-import { useNode } from '@xyo-network/react-node'
-import { useState } from 'react'
+import { AddressHistoryQuerySchema } from '@xyo-network/diviner'
+import { XyoPayload, XyoPayloadBuilder } from '@xyo-network/payload'
+import { useNodeQueryDiviner } from '@xyo-network/react-node'
+import { useEffect, useState } from 'react'
 
-export const useAddressHistory = (address?: string): [XyoBoundWitness[] | undefined, XyoError | undefined, () => void] => {
-  const [blocks, setBlocks] = useState<XyoBoundWitness[]>()
-  const [error, setError] = useState<XyoError>()
+export const useAddressHistory = (address?: string): [XyoBoundWitness[] | undefined, Error | undefined, () => void] => {
+  const [query, setQuery] = useState<XyoPayload>()
   const [refresh, setRefresh] = useState(1)
-
+  const [blocks, error] = useNodeQueryDiviner(AddressHistoryQuerySchema, query)
   const refreshHistory = () => setRefresh((previous) => previous + 1)
 
-  const [node] = useNode<MemoryNode>()
+  useEffect(() => {
+    setQuery(new XyoPayloadBuilder({ schema: AddressHistoryQuerySchema }).fields({ address }).build())
+  }, [address, refresh])
 
-  useAsyncEffect(
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    async (mounted) => {
-      if (node && address) {
-        try {
-          const diviner = (await node.tryResolve({ name: [AddressHistoryQuerySchema] }))[0] as DivinerWrapper
-          const query = new XyoPayloadBuilder({ schema: AddressHistoryQuerySchema }).fields({ address }).build()
-          const result = (await new DivinerWrapper(diviner).divine([query])) as XyoBoundWitness[]
-          if (mounted() && result) {
-            setBlocks(result)
-          }
-        } catch (e) {
-          setError(e as XyoError)
-        }
-      }
-    },
-    [address, node, refresh],
-  )
-
-  return [blocks, error, refreshHistory]
+  return [blocks as XyoBoundWitness[], error, refreshHistory]
 }
