af529ca172fd868a6a62a49d5eec0d6659ca8e71
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-22 17:41:55 -0500

    use new schema list hook

--- packages/sdk/packages/schema-api/src/hooks/useSchemaDefinitions.tsx
+++ packages/sdk/packages/schema-api/src/hooks/useSchemaDefinitions.tsx
@@ -1,12 +1,13 @@
 import { useAsyncEffect } from '@xylabs/react-shared'
+import { useArchive } from '@xyo-network/react-api'
+import { useSchemaList } from '@xyo-network/react-schema'
 import { XyoSchemaPayload } from '@xyo-network/schema-payload-plugin'
 import { XyoSchemaCache } from '@xyo-network/utils'
 import { useState } from 'react'
 
-import { useDivineSchemaList } from '../diviners'
-
 export const useSchemaDefinitions = (): XyoSchemaPayload[] | undefined => {
-  const [schemaList] = useDivineSchemaList()
+  const { archive } = useArchive()
+  const [schemaList] = useSchemaList(archive)
   const [schemaPayloads, setSchemaPayloads] = useState<XyoSchemaPayload[]>()
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
