d7f7400d2260791a77f2882e357be40587b81dec
Matt Jones <885417+jonesmac@users.noreply.github.com>
2022-12-23 10:29:29 -0500

    use Payload return type

--- packages/sdk/packages/schema/src/hooks/useSchemaStats.tsx
+++ packages/sdk/packages/schema/src/hooks/useSchemaStats.tsx
@@ -1,13 +1,12 @@
-import { SchemaStats } from '@xyo-network/api'
-import { SchemaStatsQuerySchema } from '@xyo-network/node-core-model'
+import { SchemaStatsPayload, SchemaStatsQuerySchema } from '@xyo-network/node-core-model'
 import { TYPES } from '@xyo-network/node-core-types'
 import { XyoPayload, XyoPayloadBuilder } from '@xyo-network/payload'
 import { useNodeQueryDiviner } from '@xyo-network/react-node'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
-export const useSchemaStats = (archive?: string): [SchemaStats | undefined, Error | undefined, Dispatch<SetStateAction<number>>] => {
+export const useSchemaStats = (archive?: string): [SchemaStatsPayload | undefined, Error | undefined, Dispatch<SetStateAction<number>>] => {
   const [refresh, setRefresh] = useState(1)
-  const refreshHistory = () => setRefresh((previous) => previous + 1)
+  const refreshStats = () => setRefresh((previous) => previous + 1)
 
   const [query, setQuery] = useState<XyoPayload>()
   const [schemaStats, error] = useNodeQueryDiviner(TYPES.SchemaStatsDiviner.description, query)
@@ -18,5 +17,5 @@ export const useSchemaStats = (archive?: string): [SchemaStats | undefined, Erro
     }
   }, [archive, refresh])
 
-  return [schemaStats?.[0] as SchemaStats, error, refreshHistory]
+  return [schemaStats?.[0] as SchemaStatsPayload, error, refreshStats]
 }
