60b7f9dba72c9b8736c722bd779c1636bab26add
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-01-08 16:00:16 -0500

    archivists for current archive

--- packages/sdk/packages/api/src/archive/contexts/Provider.tsx
+++ packages/sdk/packages/api/src/archive/contexts/Provider.tsx
@@ -1,4 +1,7 @@
-import { WithChildren } from '@xylabs/react-shared'
+import { useAsyncEffect, WithChildren } from '@xylabs/react-shared'
+import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
+import { MemoryNode } from '@xyo-network/node'
+import { useNode } from '@xyo-network/react-node'
 import { useState } from 'react'
 
 import { ArchiveContext } from './Context'
@@ -8,7 +11,26 @@ export interface ArchiveProviderProps {
 }
 
 export const ArchiveProvider: React.FC<WithChildren<ArchiveProviderProps>> = ({ defaultArchive, ...props }) => {
-  const [archive, setArchive] = useState<string>()
+  const [archive, setArchive] = useState<string | undefined>(defaultArchive)
+  const [archivePayloadArchivist, setArchivePayloadArchivist] = useState<ArchivistWrapper>()
+  const [archiveBoundWitnessArchivist, setArchiveBoundWitnessArchivist] = useState<ArchivistWrapper>()
+  const [node] = useNode<MemoryNode>(false)
 
-  return <ArchiveContext.Provider value={{ archive: archive ?? defaultArchive, provided: true, setArchive }} {...props} />
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async () => {
+      const wrappedPayloadArchivist = await node?.tryResolveWrapped(ArchivistWrapper, { name: [`${archive}[payload]`] })
+      const wrappedBoundWitnessArchivist = await node?.tryResolveWrapped(ArchivistWrapper, { name: [`${archive}[boundwitness]`] })
+      setArchivePayloadArchivist(wrappedPayloadArchivist?.shift())
+      setArchiveBoundWitnessArchivist(wrappedBoundWitnessArchivist?.shift())
+    },
+    [archive, node],
+  )
+
+  return (
+    <ArchiveContext.Provider
+      value={{ archive: archive ?? defaultArchive, archiveBoundWitnessArchivist, archivePayloadArchivist, provided: true, setArchive }}
+      {...props}
+    />
+  )
 }
