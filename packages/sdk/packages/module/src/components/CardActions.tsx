b347922552ea9e7688de592f96625e3c40eb6dc5
Arie Trouw <arie.trouw@xylabs.com>
2023-02-13 17:14:41 -0800

    ModuleCard Updates

--- packages/sdk/packages/module/src/components/CardActions.tsx
+++ packages/sdk/packages/module/src/components/CardActions.tsx
@@ -1,13 +1,24 @@
 import { CardActions, CardActionsProps } from '@mui/material'
 import { ButtonEx } from '@xylabs/react-button'
 import { ModuleWrapper } from '@xyo-network/module'
+import { findNetworkComponent } from '@xyo-network/react-shared'
 
 import { ModuleRenderProps } from '../ModuleRenderProps'
+
+const getModuleIcons = (moduleType: string, wrapper: ModuleWrapper) => {
+  return wrapper?.queries().find((query) => query.startsWith(`network.xyo.query.${moduleType}`)) ? findNetworkComponent(moduleType)?.icon() : null
+}
 
 export const ModuleCardActions: React.FC<CardActionsProps & ModuleRenderProps> = ({ children, module, ...props }) => {
   const wrapper = module ? new ModuleWrapper(module) : undefined
   return (
-    <CardActions {...props}>
+    <CardActions style={{ justifyContent: 'space-between' }} {...props}>
+      {wrapper
+        ? ['sentinel', 'bridge', 'archivist', 'diviner', 'node'].map((moduleType) => {
+            const icon = getModuleIcons(moduleType, wrapper)
+            return icon ? <ButtonEx key={moduleType}>{icon}</ButtonEx> : null
+          })
+        : null}
       <ButtonEx onClick={() => wrapper?.discover()}>Discover</ButtonEx>
       {children}
     </CardActions>
--- packages/sdk/packages/module/src/components/DetailsBox.tsx
+++ packages/sdk/packages/module/src/components/DetailsBox.tsx
@@ -1,25 +1,41 @@
 import { EthAddress } from '@xylabs/eth-address'
+import { ButtonEx } from '@xylabs/react-button'
 import { EthAccountBox } from '@xylabs/react-crypto'
 import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/react-flexbox'
 import { ModuleWrapper } from '@xyo-network/module'
 import { findNetworkComponent } from '@xyo-network/react-shared'
+import { useState } from 'react'
 
 import { ModuleRenderProps } from '../ModuleRenderProps'
 
+const getModuleIcon = (moduleType: string, wrapper: ModuleWrapper) => {
+  return wrapper?.queries().find((query) => query.startsWith(`network.xyo.query.${moduleType}`)) ? findNetworkComponent(moduleType)?.icon() : null
+}
+
 export const ModuleDetailsBox: React.FC<ModuleRenderProps & FlexBoxProps> = ({ module, ...props }) => {
   const wrapper = module ? new ModuleWrapper(module) : undefined
+  const [showQueries, setShowQueries] = useState(false)
   return (
     <FlexCol {...props}>
-      <EthAccountBox address={EthAddress.fromString(wrapper?.address)} />
-      {wrapper?.queries().map((query) => {
-        return <FlexRow key={query}>{query}</FlexRow>
-      })}
       <FlexRow>
-        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.archivist')) ? findNetworkComponent('archivist')?.icon() : null}
-        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.diviner')) ? findNetworkComponent('diviner')?.icon() : null}
-        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.witness')) ? findNetworkComponent('sentinel')?.icon() : null}
-        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.node')) ? findNetworkComponent('node')?.icon() : null}
+        {wrapper
+          ? ['sentinel', 'bridge', 'archivist', 'diviner', 'node'].map((moduleType) => {
+              const icon = getModuleIcon(moduleType, wrapper)
+              return icon ? (
+                <ButtonEx onClick={() => setShowQueries(!showQueries)} key={moduleType}>
+                  {icon}
+                </ButtonEx>
+              ) : null
+            })
+          : null}
+        <EthAccountBox address={EthAddress.fromString(wrapper?.address)} />
       </FlexRow>
+
+      {showQueries
+        ? wrapper?.queries().map((query) => {
+            return <FlexRow key={query}>{query}</FlexRow>
+          })
+        : null}
     </FlexCol>
   )
 }
