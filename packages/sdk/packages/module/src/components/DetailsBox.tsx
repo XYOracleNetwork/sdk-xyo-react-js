33dc7bd7be8c1488f7ca048f6990e30c7106587a
Arie Trouw <arie.trouw@xylabs.com>
2023-02-13 16:07:02 -0800

    Module Details Box

--- packages/sdk/packages/module/src/components/DetailsBox.tsx
+++ packages/sdk/packages/module/src/components/DetailsBox.tsx
@@ -1,7 +1,8 @@
 import { EthAddress } from '@xylabs/eth-address'
 import { EthAccountBox } from '@xylabs/react-crypto'
-import { FlexBoxProps, FlexCol } from '@xylabs/react-flexbox'
+import { FlexBoxProps, FlexCol, FlexRow } from '@xylabs/react-flexbox'
 import { ModuleWrapper } from '@xyo-network/module'
+import { findNetworkComponent } from '@xyo-network/react-shared'
 
 import { ModuleRenderProps } from '../ModuleRenderProps'
 
@@ -10,6 +11,15 @@ export const ModuleDetailsBox: React.FC<ModuleRenderProps & FlexBoxProps> = ({ m
   return (
     <FlexCol {...props}>
       <EthAccountBox address={EthAddress.fromString(wrapper?.address)} />
+      {wrapper?.queries().map((query) => {
+        return <FlexRow key={query}>{query}</FlexRow>
+      })}
+      <FlexRow>
+        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.archivist')) ? findNetworkComponent('archivist')?.icon() : null}
+        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.diviner')) ? findNetworkComponent('diviner')?.icon() : null}
+        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.witness')) ? findNetworkComponent('sentinel')?.icon() : null}
+        {wrapper?.queries().find((query) => query.startsWith('network.xyo.query.node')) ? findNetworkComponent('node')?.icon() : null}
+      </FlexRow>
     </FlexCol>
   )
 }
