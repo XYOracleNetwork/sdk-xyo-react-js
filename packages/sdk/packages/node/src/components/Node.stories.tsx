391f20ce0dab37316d8b248183726affd02eeacc
Arie Trouw <arie.trouw@xylabs.com>
2023-02-17 12:59:47 -0800

    deploy

--- packages/sdk/packages/node/src/components/Node.stories.tsx
+++ packages/sdk/packages/node/src/components/Node.stories.tsx
@@ -27,13 +27,15 @@ const Template: ComponentStory<React.FC> = (props) => {
 
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    async () => {
+    async (mounted) => {
       if (node) {
         try {
           const mod = await TestModule.create({ config: { schema: 'network.xyo.test.module' } })
           node?.register(mod)
-          node?.attach(mod.address)
-          setDescription(JSON.stringify(await node?.description(), null, 2))
+          await node?.attach(mod.address)
+          if (mounted()) {
+            setDescription(JSON.stringify(await node?.description(), null, 2))
+          }
         } catch (e) {
           console.error(e)
         }
