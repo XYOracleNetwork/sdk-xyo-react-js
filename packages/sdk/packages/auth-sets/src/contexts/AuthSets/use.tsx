9e26044719c4a5eea7d60aa3de157b4f95596d14
Arie Trouw <arie.trouw@xylabs.com>
2023-02-22 16:16:28 -0800

    useAuthSets required param

--- packages/sdk/packages/auth-sets/src/contexts/AuthSets/use.tsx
+++ packages/sdk/packages/auth-sets/src/contexts/AuthSets/use.tsx
@@ -2,4 +2,4 @@ import { useContextEx } from '@xyo-network/react-shared'
 
 import { AuthSetsContext } from './Context'
 
-export const useAuthSets = () => useContextEx(AuthSetsContext, 'AuthSets', true)
+export const useAuthSets = (required = false) => useContextEx(AuthSetsContext, 'AuthSets', required)
