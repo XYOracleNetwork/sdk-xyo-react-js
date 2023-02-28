417bd36c054d121e6ce9a3ddb859a29c0a9ebf08
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-02-28 17:04:52 -0500

    memory node provider fixes

--- packages/sdk/packages/node/src/contexts/MemoryProvider.tsx
+++ packages/sdk/packages/node/src/contexts/MemoryProvider.tsx
@@ -1,30 +1,28 @@
 import { useAsyncEffect, WithChildren } from '@xylabs/react-shared'
 import { ModuleParams } from '@xyo-network/module'
 import { MemoryNode, NodeConfig } from '@xyo-network/node'
-import { useDataState } from '@xyo-network/react-shared'
 import { useState } from 'react'
 
 import { NodeProvider } from './Provider'
 
 export type MemoryNodeProviderProps = WithChildren<{
+  moduleParams?: ModuleParams<NodeConfig>
   required?: boolean
 }> &
   ModuleParams<NodeConfig>
 
-export const MemoryNodeProvider: React.FC<MemoryNodeProviderProps> = ({ children, required = false, ...params }) => {
+export const MemoryNodeProvider: React.FC<MemoryNodeProviderProps> = ({ children, moduleParams, required = false }) => {
   const [node, setNode] = useState<MemoryNode>()
-  const [config, setConfig] = useDataState<ModuleParams<NodeConfig>>(params)
-  setConfig(params)
 
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
     async (mounted) => {
-      const node = await MemoryNode.create(config)
+      const node = await MemoryNode.create(moduleParams)
       if (mounted()) {
         setNode(node)
       }
     },
-    [config],
+    [moduleParams],
   )
 
   return (
