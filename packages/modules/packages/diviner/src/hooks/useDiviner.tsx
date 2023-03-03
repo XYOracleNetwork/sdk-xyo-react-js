e92fe3c25c2836abac3d8f39ed2b499107a9c149
Arie Trouw <arie.trouw@xylabs.com>
2023-03-03 13:08:12 -0800

    Diviner Hook

---
+++ packages/modules/packages/diviner/src/hooks/useDiviner.tsx
@@ -0,0 +1,4 @@
+import { DivinerModule, DivinerWrapper } from '@xyo-network/diviner'
+import { createUseModuleHook, useNode } from '@xyo-network/react-node'
+
+export const useDiviner = createUseModuleHook<DivinerModule, DivinerWrapper>(DivinerWrapper.wrap, useNode)
--- packages/modules/src/index.ts
+++ packages/modules/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@xyo-network/react-archivist'
+export * from '@xyo-network/react-diviner'
 export * from '@xyo-network/react-module'
 export * from '@xyo-network/react-node'
 export * from '@xyo-network/react-sentinel'
