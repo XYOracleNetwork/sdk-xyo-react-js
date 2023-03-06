0048ba38da79780eeaf2818ccf39e4412525e194
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-06 16:55:50 -0500

    ensure wallet has proper base path

--- packages/sdk/packages/wallet/src/contexts/Wallet/State.ts
+++ packages/sdk/packages/wallet/src/contexts/Wallet/State.ts
@@ -1,12 +1,9 @@
 import { HDWallet } from '@xyo-network/account'
-import { AccountInstance } from '@xyo-network/account-model'
 import { ContextExState } from '@xyo-network/react-shared'
 import { Dispatch, SetStateAction } from 'react'
 
 export interface WalletContextState extends ContextExState {
   activeAccountIndex?: number
-  activeRootAccountPath?: string
-  deriveRootAccount?: (path: string) => AccountInstance
   setActiveAccountIndex?: Dispatch<number>
   setWallet?: Dispatch<SetStateAction<HDWallet | undefined>>
   wallet?: HDWallet
