668c0c10da9b551e94ae3af5083720493b713aae
Arie Trouw <arie.trouw@xylabs.com>
2023-03-06 12:21:02 -0800

    attach events

--- packages/modules/packages/node/src/hooks/useModule.tsx
+++ packages/modules/packages/node/src/hooks/useModule.tsx
@@ -1,6 +1,6 @@
 import { useAsyncEffect } from '@xylabs/react-shared'
 import { Module, ModuleFilter } from '@xyo-network/module-model'
-import { ModuleDetachedEventArgs, ModuleDetachedEventEmitter } from '@xyo-network/node'
+import { ModuleAttachedEventArgs, ModuleAttachedEventEmitter, ModuleDetachedEventArgs, ModuleDetachedEventEmitter } from '@xyo-network/node'
 import { useMemo, useState } from 'react'
 
 import { useProvidedWrappedNode } from './useProvidedNode'
@@ -13,35 +13,50 @@ export const useModule = <TModule extends Module = Module>(
   const [node, nodeError] = useProvidedWrappedNode()
   const [module, setModule] = useState<TModule>()
   const [error, setError] = useState<Error>()
+
   const address = module?.address
+
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
     async (mounted) => {
-      const detachHandler = (args: ModuleDetachedEventArgs) => {
-        const eventModule = args.module
-        if (eventModule.address === address && mounted()) {
-          setModule(undefined)
-          setError(undefined)
-        }
-      }
       try {
         if (nodeError) {
           setError(nodeError)
           setModule(undefined)
         } else {
           if (node) {
-            const emitter = node.module as ModuleDetachedEventEmitter
+            const attachEmitter = node.module as ModuleAttachedEventEmitter
+            const detachEmitter = node.module as ModuleDetachedEventEmitter
+            const attachHandler = (args: ModuleAttachedEventArgs) => {
+              const eventModule = args.module
+              if (eventModule.address === nameOrAddress || (module?.config.name === nameOrAddress && mounted())) {
+                setModule(eventModule as TModule)
+                setError(undefined)
+              }
+            }
+            const detachHandler = (args: ModuleDetachedEventArgs) => {
+              const eventModule = args.module
+              if (eventModule.address === address && mounted()) {
+                setModule(undefined)
+                setError(undefined)
+              }
+            }
             const module: TModule | undefined = nameOrAddress
               ? await node.resolve<TModule>(nameOrAddress)
               : (await node.resolve<TModule>(filter)).pop()
             if (mounted()) {
-              emitter.on('moduleDetached', detachHandler)
+              if (module) {
+                detachEmitter.on('moduleDetached', detachHandler)
+              } else {
+                attachEmitter.on('moduleAttached', attachHandler)
+              }
               setModule(module)
               setError(undefined)
             }
             return () => {
               //remove the event handler on unmount
-              emitter.on('moduleDetached', detachHandler, true)
+              attachEmitter.on('moduleAttached', attachHandler, true)
+              detachEmitter.on('moduleDetached', detachHandler, true)
             }
           } else {
             console.log('Setting All to undefined')
