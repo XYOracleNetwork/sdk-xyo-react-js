074e16dfab99bb4f476a90077d19d7aeac9cf628
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-06 14:04:08 -0500

    HDWallet instead of XyoWalletBase

--- packages/sdk/packages/wallet/src/contexts/Account/State.ts
+++ packages/sdk/packages/wallet/src/contexts/Account/State.ts
@@ -1,8 +1,8 @@
-import { Account } from '@xyo-network/account'
+import { AccountInstance } from '@xyo-network/account-model'
 import { ContextExState } from '@xyo-network/react-shared'
 import { Dispatch } from 'react'
 
 export interface AccountContextState extends ContextExState {
-  account?: Account
-  setAccount?: Dispatch<Account>
+  account?: AccountInstance
+  setAccount?: Dispatch<AccountInstance>
 }
--- packages/sdk/packages/wallet/src/contexts/Wallet/Provider.tsx
+++ packages/sdk/packages/wallet/src/contexts/Wallet/Provider.tsx
@@ -1,25 +1,33 @@
 import { WithChildren } from '@xylabs/react-shared'
-import { XyoWalletBase } from '@xyo-network/wallet'
+import { HDWallet } from '@xyo-network/account'
 import { useEffect, useState } from 'react'
 
 import { AccountContext } from '../Account'
 import { WalletContext } from './Context'
 import { useWallet } from './use'
 
+const WalletBase = 'm/'
+const WalletRootPath = `${WalletBase}44'/60'/0'/0/`
+
 export interface WalletProviderProps {
   defaultActiveAccountIndex?: number
-  defaultWallet?: XyoWalletBase
+  defaultWallet?: HDWallet
 }
 
 const AccountWalletProvider: React.FC<WithChildren> = (props) => {
   const { wallet, activeAccountIndex = 0 } = useWallet()
 
-  return <AccountContext.Provider value={{ account: wallet?.getAccount(activeAccountIndex), provided: true }} {...props} />
+  return <AccountContext.Provider value={{ account: wallet?.deriveAccount(`${WalletRootPath}${activeAccountIndex}`), provided: true }} {...props} />
 }
 
 export const WalletProvider: React.FC<WithChildren<WalletProviderProps>> = ({ children, defaultActiveAccountIndex = 0, defaultWallet, ...props }) => {
-  const [wallet, setWallet] = useState<XyoWalletBase | undefined>(defaultWallet)
+  const [wallet, setWallet] = useState<HDWallet | undefined>(defaultWallet)
   const [activeAccountIndex, setActiveAccountIndex] = useState(defaultActiveAccountIndex)
+  const [activeRootAccountPath, setActiveRootAccountPath] = useState<string>()
+
+  useEffect(() => {
+    setActiveRootAccountPath(`${WalletRootPath}${activeAccountIndex.toString()}`)
+  }, [activeAccountIndex])
 
   useEffect(() => {
     if (defaultActiveAccountIndex !== undefined) {
@@ -33,10 +41,20 @@ export const WalletProvider: React.FC<WithChildren<WalletProviderProps>> = ({ ch
     }
   }, [defaultWallet])
 
+  const deriveRootAccount = (path: string) => {
+    if (wallet) {
+      return wallet.deriveAccount(`${WalletRootPath}${path}`)
+    } else {
+      throw new Error('Wallet was not created')
+    }
+  }
+
   return (
     <WalletContext.Provider
       value={{
         activeAccountIndex,
+        activeRootAccountPath,
+        deriveRootAccount,
         provided: true,
         setActiveAccountIndex,
         setWallet,
