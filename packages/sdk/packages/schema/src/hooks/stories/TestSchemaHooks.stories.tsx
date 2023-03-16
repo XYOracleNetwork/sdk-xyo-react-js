7c3cdf4cdb89739e0198f341ab4c7357020b2d06
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-16 09:57:45 -0400

    story improved but still not working

--- packages/sdk/packages/schema/src/hooks/stories/TestSchemaHooks.stories.tsx
+++ packages/sdk/packages/schema/src/hooks/stories/TestSchemaHooks.stories.tsx
@@ -1,28 +1,56 @@
 import { Typography } from '@mui/material'
 import { ComponentStory, DecoratorFn, Meta } from '@storybook/react'
-import { NodeConfigSchema } from '@xyo-network/node'
-import { MemoryNodeProvider } from '@xyo-network/react-node'
+import { useAsyncEffect } from '@xylabs/react-shared'
+import { Account, HDWallet } from '@xyo-network/account'
+import { HttpBridge, HttpBridgeConfigSchema } from '@xyo-network/bridge'
+import { MemoryNode, NodeConfigSchema } from '@xyo-network/node'
+import { NodeProvider } from '@xyo-network/react-node'
+import { DefaultSeedPhrase } from '@xyo-network/react-storybook'
+import { WalletProvider } from '@xyo-network/react-wallet'
 import { XyoSchemaCache } from '@xyo-network/utils'
+import { useState } from 'react'
 
 import { useSchemaDefinitions } from '../useSchemaDefinitions'
 import { useSchemaStats } from '../useSchemaStats'
 
 const apiConfig = { apiDomain: 'https://beta.api.archivist.xyo.network' }
+const nodeUri = 'http://localhost:8080/node'
+const randomWallet = HDWallet.fromMnemonic(DefaultSeedPhrase)
 
-const MemoryNodeDecorator: DecoratorFn = (Story, args) => (
-  <MemoryNodeProvider config={{ schema: NodeConfigSchema }}>
-    <Story {...args} />
-  </MemoryNodeProvider>
-)
+const MemoryNodeDecorator: DecoratorFn = (Story, args) => {
+  const [node, setNode] = useState<MemoryNode>()
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async () => {
+      const node = await MemoryNode.create({ config: { schema: NodeConfigSchema } })
+      const bridge = await HttpBridge.create({ config: { nodeUri, schema: HttpBridgeConfigSchema, security: { allowAnonymous: true } } })
+      await node.register(bridge).attach(bridge.address)
+      setNode(node)
+    },
+    [],
+  )
+
+  return (
+    <WalletProvider defaultWallet={randomWallet}>
+      <NodeProvider node={node}>
+        <Story {...args} />
+      </NodeProvider>
+    </WalletProvider>
+  )
+}
 
 // eslint-disable-next-line import/no-default-export
 export default {
+  decorators: [MemoryNodeDecorator],
   title: 'schema/Hooks',
 } as Meta
 
 const Template: ComponentStory<React.FC> = () => {
   XyoSchemaCache.instance.proxy = `${apiConfig.apiDomain}/domain`
-  const [schemaStats] = useSchemaStats('temp')
+  const phrase = 'temp'
+  const address = new Account({ phrase }).addressValue.hex
+  const [schemaStats] = useSchemaStats(address)
   const schemaList = schemaStats?.filter(({ name }) => !!name) as { name: string }[]
   const schemaDefinitions = useSchemaDefinitions(schemaList)
 
@@ -39,7 +67,5 @@ const Template: ComponentStory<React.FC> = () => {
 }
 
 const Default = Template.bind({})
-Default.decorators = [MemoryNodeDecorator]
-Default.args = {}
 
 export { Default }
