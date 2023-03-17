0c65342483fd574ccc0d73b642e1a3baba1cbe35
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-17 16:48:41 -0400

    convert schema property links to events

--- packages/sdk/packages/schema/src/components/Property/SchemaProperty.tsx
+++ packages/sdk/packages/schema/src/components/Property/SchemaProperty.tsx
@@ -4,12 +4,14 @@ import VerifiedIcon from '@mui/icons-material/Verified'
 import { IconButton } from '@mui/material'
 import { LinkEx } from '@xylabs/react-link'
 import { useAsyncEffect } from '@xylabs/react-shared'
+import { useXyoEvent, XyoEventDispatch, XyoEventNoun } from '@xyo-network/react-event'
 import { Property, PropertyProps, PropertyValue } from '@xyo-network/react-property'
 import { XyoSchemaCache, XyoSchemaCacheEntry } from '@xyo-network/utils'
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 
 export type SchemaPropertyProps = PropertyProps & {
   value?: string
+  /** @deprecated - use events instead */
   viewSchemaUrl?: string
 }
 
@@ -30,17 +32,31 @@ const useResolveSchema = (schema?: string) => {
   return entry
 }
 
-export const SchemaProperty: React.FC<SchemaPropertyProps> = ({ titleProps, value, viewSchemaUrl = 'https://explore.xyo.network/', ...props }) => {
+export const SchemaProperty = forwardRef<HTMLDivElement, SchemaPropertyProps>(({ titleProps, value, ...props }, forwardedRef) => {
   const resolvedSchema = useResolveSchema(value)
+  const [buttonRef, buttonDispatch] = useXyoEvent<HTMLButtonElement>(undefined)
+  const [divRef, divDispatch] = useXyoEvent<HTMLDivElement>(undefined)
+
+  const onClick = (dispatch?: XyoEventDispatch<XyoEventNoun, 'click', string>, openNewWindow = false) => {
+    dispatch?.(
+      'schema',
+      'click',
+      JSON.stringify({
+        openNewWindow,
+        schema: value,
+      }),
+    )
+  }
+
   return (
-    <Property title="Schema" value={value} tip="Schema sent with the payload" titleProps={titleProps} {...props}>
+    <Property ref={forwardedRef} title="Schema" value={value} tip="Schema sent with the payload" titleProps={titleProps} {...props}>
       {value ? (
         resolvedSchema === null ? (
-          <IconButton rel="noopener noreferrer" size="small" target="_blank" href={`${viewSchemaUrl}${value}`}>
+          <IconButton ref={buttonRef} size="small" onClick={() => onClick(buttonDispatch)}>
             <NewReleasesIcon color="warning" fontSize="inherit" />
           </IconButton>
         ) : resolvedSchema === undefined ? (
-          <IconButton rel="noopener noreferrer" size="small" target="_blank" href={`${viewSchemaUrl}${value}`}>
+          <IconButton ref={buttonRef} size="small" onClick={() => onClick(buttonDispatch)}>
             <NewReleasesIcon color="disabled" fontSize="inherit" />
           </IconButton>
         ) : (
@@ -50,15 +66,15 @@ export const SchemaProperty: React.FC<SchemaPropertyProps> = ({ titleProps, valu
         )
       ) : null}
       {value ? (
-        <LinkEx flexGrow={1} alignItems="center" href={`${viewSchemaUrl}${value}`} rel="noopener noreferrer" display="flex">
-          <PropertyValue value={value} title="view schema in new window" />
-        </LinkEx>
-      ) : null}
-      {value ? (
-        <LinkEx marginX={1} alignItems="center" href={`${viewSchemaUrl}${value}`} target="_blank" rel="noopener noreferrer" display="flex">
-          <OpenInNewIcon fontSize="inherit" />
-        </LinkEx>
+        <>
+          <PropertyValue ref={divRef} value={value} title="view schema" onClick={() => onClick(divDispatch)} sx={{ cursor: 'pointer' }} />
+          <IconButton ref={buttonRef} size="small" onClick={() => onClick(buttonDispatch, true)}>
+            <OpenInNewIcon fontSize="inherit" />
+          </IconButton>
+        </>
       ) : null}
     </Property>
   )
-}
+})
+
+SchemaProperty.displayName = 'SchemaProperty'
