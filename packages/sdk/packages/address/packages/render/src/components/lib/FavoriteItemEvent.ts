9899172e902eaa3cd58fcc59efb69a70e709ccf4
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-24 15:37:32 -0400

    extract type and add generater

--- packages/sdk/packages/address/packages/render/src/components/lib/FavoriteItemEvent.ts
+++ packages/sdk/packages/address/packages/render/src/components/lib/FavoriteItemEvent.ts
@@ -1,6 +1,20 @@
 export interface FavoriteItemEvent {
   alias?: string
   favorite?: boolean
-  favoriteType?: 'address' | 'schema' | 'hash'
+  favoriteType?: FavoriteType
   favoriteValue?: string
 }
+
+export type FavoriteType = 'address' | 'schema' | 'hash'
+
+export const generateFavoriteEvent = (
+  alias?: string,
+  favorite?: boolean,
+  favoriteType?: FavoriteType,
+  favoriteValue?: string,
+): FavoriteItemEvent => ({
+  alias,
+  favorite: !!favorite,
+  favoriteType,
+  favoriteValue,
+})
