606f9bd08345279b88b4adff4194e19ac5382f51
Arie Trouw <arie.trouw@xylabs.com>
2023-03-28 11:58:29 -0700

    Logging for useModules

--- packages/modules/packages/node/src/hooks/useModules.ts
+++ packages/modules/packages/node/src/hooks/useModules.ts
@@ -1,42 +1,51 @@
 import { useAsyncEffect } from '@xylabs/react-shared'
+import { Logger } from '@xyo-network/core'
 import { Module, ModuleFilter } from '@xyo-network/module-model'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useProvidedWrappedNode } from './useProvidedNode'
 
-export const useModules = <TModule extends Module = Module>(filter?: ModuleFilter): [TModule[] | undefined, Error | undefined] => {
+export const useModules = <TModule extends Module = Module>(filter?: ModuleFilter, logger?: Logger): [TModule[] | undefined, Error | undefined] => {
   const [node, nodeError] = useProvidedWrappedNode()
   const [modules, setModules] = useState<TModule[]>()
   const [error, setError] = useState<Error>()
 
+  useEffect(() => {
+    logger?.debug('useModules: useAsyncEffect')
+    if (nodeError) {
+      setError(nodeError)
+      setModules(undefined)
+    }
+  }, [nodeError, logger])
+
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
     async (mounted) => {
+      logger?.debug('useModules: useAsyncEffect')
       try {
-        if (nodeError) {
-          setError(nodeError)
-          setModules(undefined)
-        } else {
-          if (node) {
-            const modules: TModule[] | undefined = await node.resolve<TModule>(filter)
-            if (mounted()) {
-              setModules(modules)
-              setError(undefined)
-            }
-          } else {
+        if (node) {
+          const modules: TModule[] | undefined = await node.resolve<TModule>(filter)
+          if (mounted()) {
+            setModules(modules)
             setError(undefined)
-            setModules(undefined)
           }
+        } else {
+          setError(undefined)
+          setModules(undefined)
         }
       } catch (ex) {
         if (mounted()) {
           const error = ex as Error
+          logger?.error(`useModules: useAsyncEffect [${error.message}]`)
           setError(error)
           setModules(undefined)
         }
       }
+      return () => {
+        logger?.debug('useModules: unmount')
+      }
     },
-    [filter, node, nodeError],
+    [filter, node, logger],
   )
 
   return [modules, error]
