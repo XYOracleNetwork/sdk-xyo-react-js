551245e8f71d1f2510cbc8da29c0a47a5fbde1bf
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-29 15:53:45 -0400

    dead code

--- packages/sdk/packages/node-renderer/src/components/story/TestData.tsx
+++ packages/sdk/packages/node-renderer/src/components/story/TestData.tsx
@@ -1,10 +1,4 @@
-import HubIcon from '@mui/icons-material/Hub'
 import { CytoscapeOptions } from 'cytoscape'
-
-import { encodeSvg } from '../../lib'
-
-const icon = <HubIcon fontSize="small" />
-const url = encodeSvg(icon)
 
 const elements: CytoscapeOptions['elements'] = [
   {
@@ -29,37 +23,6 @@ const elements: CytoscapeOptions['elements'] = [
   },
 ]
 
-const layout: CytoscapeOptions['layout'] = {
-  name: 'cose',
-  nodeDimensionsIncludeLabels: true,
-}
-
-const style: CytoscapeOptions['style'] = [
-  {
-    selector: 'node',
-    style: {
-      'background-height': '75%',
-      'background-image': url,
-      'background-image-smoothing': 'yes',
-      'background-width': '75%',
-      label: 'data(id)',
-    },
-  },
-
-  {
-    selector: 'edge',
-    style: {
-      'curve-style': 'bezier',
-      'line-color': '#ccc',
-      'target-arrow-color': '#ccc',
-      'target-arrow-shape': 'triangle',
-      width: 3,
-    },
-  },
-]
-
 export const options: CytoscapeOptions = {
   elements,
-  layout,
-  style,
 }
