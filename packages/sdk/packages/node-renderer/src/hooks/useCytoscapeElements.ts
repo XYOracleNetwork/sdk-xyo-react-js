3da224d36723854e1315621ca303388fd022c51d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-03-29 22:10:28 -0400

    refactor to make graph reactive and decompose functions

--- packages/sdk/packages/node-renderer/src/hooks/useCytoscapeElements.ts
+++ packages/sdk/packages/node-renderer/src/hooks/useCytoscapeElements.ts
@@ -1,11 +1,48 @@
 import { useAsyncEffect } from '@xylabs/react-shared'
-import { ModuleWrapper } from '@xyo-network/module'
+import { EventUnsubscribeFunction, ModuleDescription, ModuleWrapper } from '@xyo-network/module'
 import { NodeWrapper } from '@xyo-network/node'
 import { useProvidedWrappedNode } from '@xyo-network/react-node'
-import { CytoscapeOptions } from 'cytoscape'
-import { useState } from 'react'
+import { ElementDefinition } from 'cytoscape'
+import { useEffect, useState } from 'react'
 
 import { parseModuleType } from '../lib'
+
+const MaxNameLength = 20
+
+const normalizeName = (name?: string) => {
+  if (!name) return
+  if (name.length > MaxNameLength) return `${name.substring(0, 20)}...`
+  return name
+}
+
+const buildNode = (description: ModuleDescription): ElementDefinition => {
+  const newNodeId = normalizeName(description.name) ?? description.address.substring(0, 8)
+  return {
+    data: {
+      id: newNodeId,
+      type: parseModuleType(description.queries),
+    },
+  }
+}
+
+const buildEdge = (rootNode: ElementDefinition, newNode: ElementDefinition) => ({
+  data: {
+    id: `${rootNode.data.id}/${newNode.data.id}`,
+    source: rootNode.data.id,
+    target: newNode.data.id,
+  },
+})
+
+const buildRootNode = async (wrapper: NodeWrapper): Promise<[ModuleDescription, ElementDefinition]> => {
+  const description = await wrapper?.describe()
+  return [description, buildNode(description)]
+}
+
+const buildChild = async (wrapper: NodeWrapper, address: string) => {
+  const [result] = await wrapper.resolveWrapped(ModuleWrapper, { address: [address] })
+  const description = await result.describe()
+  return buildNode(description)
+}
 
 /**
  * Note: Relies on describe but could eventually be converted to a discover call
@@ -13,55 +50,48 @@ import { parseModuleType } from '../lib'
  */
 export const useCytoscapeElements = () => {
   const [node] = useProvidedWrappedNode()
-  const [elements, setElements] = useState<CytoscapeOptions['elements']>()
+  const [elements, setElements] = useState<ElementDefinition[]>([])
+  const [refresh, setRefresh] = useState(1)
 
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    async () => {
-      if (node) {
+    async (mounted) => {
+      if (node && refresh) {
         try {
-          const newElements = []
-          const wrapper = NodeWrapper.wrap(node)
-          const description = await wrapper?.describe()
-          const rootNodeId = description.name ?? description.address.substring(0, 6)
-          newElements.push({
-            data: { id: rootNodeId, type: parseModuleType(description.queries) },
-          })
+          const [description, newRootNode] = await buildRootNode(node)
+          if (mounted()) setElements(() => [newRootNode])
+
           const children = description.children
-          await Promise.all(
+          await Promise.allSettled(
             (children ?? [])?.map(async (address) => {
-              const [result] = await wrapper.resolveWrapped(ModuleWrapper, { address: [address] })
               try {
-                const description = await result.describe()
-                const newNodeId = description.name ?? description.address.substring(0, 6)
-                const newNode = {
-                  data: {
-                    id: newNodeId,
-                    type: parseModuleType(description.queries),
-                  },
-                }
-                newElements.push(newNode)
-                const newEdge = {
-                  data: {
-                    id: `${rootNodeId}/${newNodeId}`,
-                    source: rootNodeId,
-                    target: newNodeId,
-                  },
-                }
-                newElements.push(newEdge)
+                const newNode = await buildChild(node, address)
+                if (mounted()) setElements((previous) => [...previous, newNode])
+
+                const newEdge = buildEdge(newRootNode, newNode)
+                if (mounted()) setElements((previous) => [...previous, newEdge])
               } catch (e) {
-                console.error(e, result)
+                console.error('Error parsing children', e)
               }
             }),
           )
-          setElements(newElements)
         } catch (e) {
-          console.error(e)
+          console.error('Error Getting initial description', e)
         }
       }
     },
-    [node],
+    [node, refresh],
   )
+
+  useEffect(() => {
+    let listener: EventUnsubscribeFunction
+    if (node) {
+      listener = node.onAny((eventName) => {
+        if (eventName === 'moduleAttached') setRefresh((previous) => previous + 1)
+      })
+    }
+    return () => listener?.()
+  }, [node])
 
   return elements
 }
