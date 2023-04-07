454964ef87c81672fc8bafc4c89e0904e35a2899
Arie Trouw <arie.trouw@xylabs.com>
2023-04-07 12:55:51 -0700

    allowing initial registration for memory node

--- packages/modules/packages/node/packages/provider/src/MemoryProvider.tsx
+++ packages/modules/packages/node/packages/provider/src/MemoryProvider.tsx
@@ -1,5 +1,5 @@
 import { useAsyncEffect, WithChildren } from '@xylabs/react-shared'
-import { ModuleParams } from '@xyo-network/module'
+import { Module, ModuleParams } from '@xyo-network/module'
 import { MemoryNode, NodeConfig } from '@xyo-network/node'
 import { useState } from 'react'
 
@@ -7,12 +7,32 @@ import { NodeProvider } from './Node'
 
 export type MemoryNodeProviderProps = WithChildren<{
   moduleParams?: ModuleParams<NodeConfig>
+  modules?: Module[]
   required?: boolean
 }> &
   ModuleParams<NodeConfig>
 
-export const MemoryNodeProvider: React.FC<MemoryNodeProviderProps> = ({ children, moduleParams }) => {
+export const MemoryNodeProvider: React.FC<MemoryNodeProviderProps> = ({ children, modules, moduleParams }) => {
   const [node, setNode] = useState<MemoryNode>()
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async () => {
+      if (node && modules) {
+        await Promise.all(
+          modules.map(async (module) => {
+            await node.register(module)
+          }),
+        )
+        return () => {
+          modules.map(async (module) => {
+            await node.unregister(module)
+          })
+        }
+      }
+    },
+    [modules, node],
+  )
 
   useAsyncEffect(
     // eslint-disable-next-line react-hooks/exhaustive-deps
