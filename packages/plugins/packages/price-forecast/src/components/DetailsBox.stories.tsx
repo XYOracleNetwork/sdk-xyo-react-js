28ebb391b5c1658832224ab9ed6bafc56cb9767d
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-04-26 14:15:14 -0400

    stub payload in story

--- packages/plugins/packages/price-forecast/src/components/DetailsBox.stories.tsx
+++ packages/plugins/packages/price-forecast/src/components/DetailsBox.stories.tsx
@@ -1,6 +1,35 @@
 import { Meta, StoryFn } from '@storybook/react'
 
+import { ForecastPayloadSchema } from '../lib'
 import { PriceForecastDetailsBox } from './DetailsBox'
+
+const tenMin = 600000
+
+const ForecastingDivinerPayload = {
+  schema: ForecastPayloadSchema,
+  values: [
+    {
+      error: 0,
+      timestamp: Date.now(),
+      value: 1000,
+    },
+    {
+      error: 0,
+      timestamp: Date.now() - tenMin,
+      value: 1000,
+    },
+    {
+      error: 0,
+      timestamp: Date.now() - tenMin * 2,
+      value: 1000,
+    },
+    {
+      error: 0,
+      timestamp: Date.now() - tenMin * 3,
+      value: 1000,
+    },
+  ],
+}
 
 const StorybookEntry = {
   argTypes: {},
@@ -20,7 +49,12 @@ const Template: StoryFn<typeof PriceForecastDetailsBox> = (args) => {
 const Default = Template.bind({})
 Default.args = {}
 
-export { Default }
+const WithData = Template.bind({})
+WithData.args = {
+  payload: ForecastingDivinerPayload,
+}
+
+export { Default, WithData }
 
 // eslint-disable-next-line import/no-default-export
 export default StorybookEntry
