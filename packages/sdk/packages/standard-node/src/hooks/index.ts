3ff498129d703e1c743777946a52eddce5445829
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-04 14:35:08 -0400

    add hook for building a sample node

--- packages/sdk/packages/standard-node/src/hooks/index.ts
+++ packages/sdk/packages/standard-node/src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useActiveNodeAddress'
+export * from './useBuildSampleNode'
---
+++ packages/sdk/packages/standard-node/src/hooks/useBuildSampleNode.tsx
@@ -0,0 +1,25 @@
+import { useAsyncEffect } from '@xylabs/react-async-effect'
+import { NodeWrapper } from '@xyo-network/node'
+import { useState } from 'react'
+
+import { MemoryNodeBuilder } from '../lib'
+
+type SampleNodeModules = 'MemoryArchivist' | 'Bridge' | 'SystemInfoWitness'
+
+export const useBuildSampleNode = (sampleModules: SampleNodeModules[] = [], nodeModuleName = 'Node', apiDomain?: string) => {
+  const [node, setNode] = useState<NodeWrapper>()
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async () => {
+      const nodeBuilder = await MemoryNodeBuilder.create({ name: nodeModuleName })
+      if (sampleModules.includes('MemoryArchivist')) await nodeBuilder.addArchivistMemory('Archivist')
+      if (sampleModules.includes('Bridge') && apiDomain) await nodeBuilder.addBridge(apiDomain)
+
+      setNode(nodeBuilder.wrappedNode)
+    },
+    [sampleModules, apiDomain, nodeModuleName],
+  )
+
+  return node
+}
