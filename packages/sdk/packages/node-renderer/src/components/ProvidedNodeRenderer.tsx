0a18a5ff09243cb987db8e8d5c88237be1d1eb11
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-04 14:57:18 -0400

    take node as prop to renderer

--- packages/sdk/packages/node-renderer/src/components/ProvidedNodeRenderer.tsx
+++ packages/sdk/packages/node-renderer/src/components/ProvidedNodeRenderer.tsx
@@ -1,11 +1,17 @@
+import { NodeWrapper } from '@xyo-network/node'
 import { useProvidedWrappedNode } from '@xyo-network/react-node'
 
 import { useCytoscapeElements, useCytoscapeOptions } from '../hooks'
+import { NodeRelationalGraphProps } from './lib'
 import { NodeRelationalGraph } from './RelationalGraph'
 
-export const ProvidedNodeRenderer: React.FC = (props) => {
-  const [node] = useProvidedWrappedNode()
-  const elements = useCytoscapeElements(node)
+export interface ProvidedNodeRendererProps extends NodeRelationalGraphProps {
+  node?: NodeWrapper
+}
+
+export const ProvidedNodeRenderer: React.FC<ProvidedNodeRendererProps> = ({ node, ...props }) => {
+  const [providedNode] = useProvidedWrappedNode()
+  const elements = useCytoscapeElements(node ?? providedNode)
   const options = useCytoscapeOptions(elements)
 
   return <NodeRelationalGraph alignItems="stretch" flexGrow={1} height="100%" options={options} {...props} />
