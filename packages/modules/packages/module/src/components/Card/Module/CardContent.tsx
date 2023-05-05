26801e7ac42ed562df5b78595cecef10dfd1b3ed
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-05 16:14:28 -0400

    dialog for discover query results

--- packages/modules/packages/module/src/components/Card/Module/CardContent.tsx
+++ packages/modules/packages/module/src/components/Card/Module/CardContent.tsx
@@ -1,29 +1,7 @@
-import { CardContent, CardContentProps, Typography } from '@mui/material'
-import { EthAddress } from '@xylabs/eth-address'
-import { useAsyncEffect } from '@xylabs/react-async-effect'
-import { EthAccountBox } from '@xylabs/react-crypto'
-import { AnyConfigSchema, ModuleConfig, ModuleWrapper } from '@xyo-network/module'
-import { useState } from 'react'
+import { CardContent, CardContentProps } from '@mui/material'
 
 import { ModuleRenderProps } from '../../../ModuleRenderProps'
 
 export const ModuleCardContent: React.FC<ModuleRenderProps & CardContentProps> = ({ children, module, ...props }) => {
-  const [config, setConfig] = useState<AnyConfigSchema<ModuleConfig>>()
-
-  const account = module?.address ? EthAddress.fromString(module?.address) : undefined
-
-  useAsyncEffect(
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    async (mounted) => {
-      const wrapper = module ? ModuleWrapper.wrap(module) : undefined
-      const payloads = await wrapper?.discover()
-      console.log(`Payloads: ${JSON.stringify(payloads, null, 2)}`)
-      if (mounted()) {
-        setConfig(payloads?.[0])
-      }
-    },
-    [module],
-  )
-
   return <CardContent {...props}>{children}</CardContent>
 }
---
+++ packages/modules/packages/module/src/components/Card/Module/components/DiscoverDialog.tsx
@@ -0,0 +1,36 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogProps } from '@mui/material'
+import { useAsyncEffect } from '@xylabs/react-async-effect'
+import { ModuleWrapper } from '@xyo-network/module'
+import { Payload } from '@xyo-network/payload-model'
+import { Dispatch, SetStateAction, useState } from 'react'
+
+export interface DiscoverDialogProps extends DialogProps {
+  setOpen?: Dispatch<SetStateAction<boolean>>
+  wrapper?: ModuleWrapper
+}
+
+export const DiscoverDialog: React.FC<DiscoverDialogProps> = ({ setOpen, wrapper, ...props }) => {
+  const [discoverPayloads, setDiscoverPayloads] = useState<Payload[]>([])
+
+  useAsyncEffect(
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async (mounted) => {
+      if (wrapper) {
+        const payloads = await wrapper.discover()
+        if (mounted()) setDiscoverPayloads(payloads)
+      }
+    },
+    [wrapper],
+  )
+
+  return (
+    <Dialog {...props}>
+      <DialogContent>
+        <pre>{JSON.stringify(discoverPayloads, null, 2)}</pre>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setOpen?.(false)}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
