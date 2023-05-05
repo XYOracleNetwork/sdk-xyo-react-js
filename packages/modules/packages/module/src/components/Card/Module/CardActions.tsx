668df8ffc02759800b90fba1c8ac67709170380c
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-05 16:14:56 -0400

    styled module card actions

--- packages/modules/packages/module/src/components/Card/Module/CardActions.tsx
+++ packages/modules/packages/module/src/components/Card/Module/CardActions.tsx
@@ -1,5 +1,4 @@
 import { CardActions, CardActionsProps, Divider, Icon, Typography } from '@mui/material'
-import { useAsyncEffect } from '@xylabs/react-async-effect'
 import { ButtonEx } from '@xylabs/react-button'
 import { FlexGrowRow } from '@xylabs/react-flexbox'
 import { AnyConfigSchema, ModuleConfig, ModuleWrapper } from '@xyo-network/module'
@@ -7,46 +6,38 @@ import { findNetworkComponent } from '@xyo-network/react-shared'
 import { useState } from 'react'
 
 import { ModuleRenderProps } from '../../../ModuleRenderProps'
+import { DiscoverDialog } from './components'
 
 const getModuleIcons = (moduleType: string, wrapper: ModuleWrapper) => {
   return wrapper?.queries.find((query) => query.startsWith(`network.xyo.query.${moduleType}`)) ? findNetworkComponent(moduleType)?.icon() : null
 }
 
 export const ModuleCardActions: React.FC<CardActionsProps & ModuleRenderProps> = ({ children, module, sx, ...props }) => {
-  const [config, setConfig] = useState<AnyConfigSchema<ModuleConfig>>()
+  const [discoverDialogOpen, setDiscoverDialogOpen] = useState(false)
   const wrapper = module ? ModuleWrapper.wrap(module) : undefined
-
-  useAsyncEffect(
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    async (mounted) => {
-      const wrapper = module ? ModuleWrapper.wrap(module) : undefined
-      const payloads = await wrapper?.discover()
-      console.log(`Payloads: ${JSON.stringify(payloads, null, 2)}`)
-      if (mounted()) {
-        setConfig(payloads?.[0])
-      }
-    },
-    [module],
-  )
+  const config = wrapper?.config
 
   return (
     <CardActions sx={{ alignItems: 'stretch', flexDirection: 'column', ml: 1, ...sx }} {...props}>
-      <Typography variant="caption" fontFamily="inherit" fontWeight="500" sx={{ mb: 1 }}>
+      <Typography variant="caption" fontFamily="inherit" fontWeight="500">
         {config?.schema}
       </Typography>
-      <Divider flexItem />
+      <Divider flexItem sx={{ my: 1 }} />
       <FlexGrowRow justifyContent="space-between" alignItems="center">
         {wrapper
           ? ['sentinel', 'bridge', 'archivist', 'diviner', 'node'].map((moduleType) => {
               const icon = getModuleIcons(moduleType, wrapper)
               return icon ? (
-                <Icon color={'primary'} key={moduleType}>
+                <Icon color={'primary'} key={moduleType} title={moduleType.toLocaleUpperCase()}>
                   {icon}
                 </Icon>
               ) : null
             })
           : null}
-        <ButtonEx onClick={() => wrapper?.discover()}>Discover</ButtonEx>
+        <ButtonEx onClick={() => setDiscoverDialogOpen(true)} size={'small'} variant={'outlined'}>
+          Discover
+        </ButtonEx>
+        <DiscoverDialog fullWidth maxWidth={'md'} open={discoverDialogOpen} setOpen={setDiscoverDialogOpen} wrapper={wrapper} />
       </FlexGrowRow>
       {children}
     </CardActions>
