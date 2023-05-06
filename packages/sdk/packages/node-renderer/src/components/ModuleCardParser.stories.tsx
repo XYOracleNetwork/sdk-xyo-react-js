3d744d22bbe66fbe7a8c8a5af83b8ff5c1b6d881
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-05 21:26:00 -0400

    lint

--- packages/sdk/packages/node-renderer/src/components/ModuleCardParser.stories.tsx
+++ packages/sdk/packages/node-renderer/src/components/ModuleCardParser.stories.tsx
@@ -1,32 +1,31 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { MemoryArchivist, MemoryArchivistConfigSchema } from '@xyo-network/archivist'
+import { IdWitness, IdWitnessConfigSchema } from '@xyo-network/id-plugin'
 
 import { ModuleCardParser } from './ModuleCardParser'
-import { MemoryArchivist, MemoryArchivistConfigSchema } from '@xyo-network/archivist';
-import { IdWitness, IdWitnessConfigSchema } from '@xyo-network/id-plugin'
 
 const meta: Meta<typeof ModuleCardParser> = {
   component: ModuleCardParser,
+  render: (props, { loaded }) => <ModuleCardParser {...props} {...loaded} />,
   title: 'node/renderer/ModuleCardParser',
-  render: (props, { loaded } ) => <ModuleCardParser {...props} {...loaded} />
 }
 
 // eslint-disable-next-line import/no-default-export
 export default meta
-type Story = StoryObj<typeof ModuleCardParser>;
-
+type Story = StoryObj<typeof ModuleCardParser>
 
 export const WithArchivist: Story = {
   loaders: [
     async () => ({
-      module: await MemoryArchivist.create({ config: { schema: MemoryArchivistConfigSchema, name: 'MemoryArchivist' } }),
+      module: await MemoryArchivist.create({ config: { name: 'MemoryArchivist', schema: MemoryArchivistConfigSchema } }),
     }),
   ],
-};
+}
 
 export const WithWitness: Story = {
   loaders: [
     async () => ({
-      module: await IdWitness.create({ config: { schema: IdWitnessConfigSchema, name: 'IdWitness' } }),
+      module: await IdWitness.create({ config: { name: 'IdWitness', schema: IdWitnessConfigSchema } }),
     }),
   ],
-};
+}
--- packages/sdk/packages/node-renderer/src/components/ModuleCardParser.tsx
+++ packages/sdk/packages/node-renderer/src/components/ModuleCardParser.tsx
@@ -1,15 +1,15 @@
-import { ArchivistModule } from '@xyo-network/archivist-model';
-import { DivinerModule } from '@xyo-network/diviner-model';
-import { Module } from '@xyo-network/module';
-import { ArchivistCard } from '@xyo-network/react-archivist';
-import { DivinerCard, ModuleCard } from '@xyo-network/react-module';
+import { ArchivistModule } from '@xyo-network/archivist-model'
+import { DivinerModule } from '@xyo-network/diviner-model'
+import { Module } from '@xyo-network/module'
+import { ArchivistCard } from '@xyo-network/react-archivist'
+import { DivinerCard, ModuleCard } from '@xyo-network/react-module'
 
 export interface ModuleCardParserProps {
   module?: Module
 }
 
 export const ModuleCardParser: React.FC<ModuleCardParserProps> = ({ module }) => {
-  switch(true) {
+  switch (true) {
     case module?.config.schema.includes('archivist'):
       return <ArchivistCard module={module as ArchivistModule} />
     case module?.config.schema.includes('diviner'):
