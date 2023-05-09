4200c34cb2277366ee32a613fdd29d05a7e1f781
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-09 17:27:22 -0400

    move icon from card actions to card avatar

--- packages/modules/packages/module/src/components/Card/Module/CardActions.tsx
+++ packages/modules/packages/module/src/components/Card/Module/CardActions.tsx
@@ -2,15 +2,10 @@ import { CardActions, CardActionsProps, Chip } from '@mui/material'
 import { ButtonEx } from '@xylabs/react-button'
 import { FlexGrowRow } from '@xylabs/react-flexbox'
 import { ModuleWrapper } from '@xyo-network/module'
-import { findNetworkComponent } from '@xyo-network/react-shared'
 import { useState } from 'react'
 
 import { ModuleRenderProps } from '../../../ModuleRenderProps'
 import { DiscoverDialog } from './components'
-
-const getModuleIcons = (moduleType: string, wrapper: ModuleWrapper) => {
-  return wrapper?.queries.find((query) => query.startsWith(`network.xyo.query.${moduleType}`)) ? findNetworkComponent(moduleType)?.icon : null
-}
 
 export const ModuleCardActions: React.FC<CardActionsProps & ModuleRenderProps> = ({ children, module, sx, ...props }) => {
   const [discoverDialogOpen, setDiscoverDialogOpen] = useState(false)
@@ -20,21 +15,7 @@ export const ModuleCardActions: React.FC<CardActionsProps & ModuleRenderProps> =
   return (
     <CardActions sx={{ alignItems: 'stretch', flexDirection: 'column', ml: 1, ...sx }} {...props}>
       <FlexGrowRow justifyContent="space-between" alignItems="center">
-        {wrapper
-          ? ['sentinel', 'bridge', 'archivist', 'diviner', 'node', 'witness'].map((moduleType) => {
-              const Icon = getModuleIcons(moduleType, wrapper)
-              return Icon ? (
-                <Chip
-                  avatar={<Icon sx={{ p: 0.25 }} />}
-                  key={moduleType}
-                  label={config?.schema}
-                  size={'small'}
-                  sx={{ bgcolor: 'transparent' }}
-                  title={moduleType.toLocaleUpperCase()}
-                />
-              ) : null
-            })
-          : null}
+        {wrapper ? <Chip label={config?.schema} size={'small'} /> : null}
         <ButtonEx onClick={() => setDiscoverDialogOpen(true)} size={'small'} variant={'outlined'}>
           Discover
         </ButtonEx>
