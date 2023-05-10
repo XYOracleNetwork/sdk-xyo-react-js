01d56afa28effa9ad9ad83ce652475cab2721fd8
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-10 15:41:31 -0400

    story for Archivist Stats

---
+++ packages/modules/packages/archivist/src/components/Card/components/Stats/ArchivistStats.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryFn } from '@storybook/react'
+import { Account } from '@xyo-network/account'
+import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
+import { Payload } from '@xyo-network/payload-model'
+
+import { ArchivistStats } from './ArchivistStats'
+
+const account = new Account()
+const payload = () => ({ schema: 'network.xyo.payload', timestamp: Date.now() })
+const boundWitness = () => new BoundWitnessBuilder().payload(payload()).witness(account).build()
+
+const StorybookEntry = {
+  component: ArchivistStats,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+  title: 'modules/archivist/ArchivistStats',
+} as Meta<typeof ArchivistStats>
+
+const Template: StoryFn<typeof ArchivistStats> = (props) => <ArchivistStats {...props} />
+
+const Default = Template.bind({})
+const WithStats = Template.bind({})
+WithStats.args = {
+  addresses: { [account.addressValue.hex]: 1 },
+  boundWitnesses: [boundWitness() as unknown as Payload],
+  payloads: [payload(), boundWitness() as unknown as Payload],
+  schemas: { 'network.xyo.boundwitness': 1, 'network.xyo.payload': 1 },
+}
+
+export { Default, WithStats }
+
+// eslint-disable-next-line import/no-default-export
+export default StorybookEntry
