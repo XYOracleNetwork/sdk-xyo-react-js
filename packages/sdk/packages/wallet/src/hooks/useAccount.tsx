2f5a9e75380371638d52f6d21565c5f4cdc58bb0
Arie Trouw <arie.trouw@xylabs.com>
2023-06-23 18:37:51 -0700

    Fixed render loop in useAccount

--- packages/sdk/packages/wallet/src/hooks/useAccount.tsx
+++ packages/sdk/packages/wallet/src/hooks/useAccount.tsx
@@ -3,7 +3,7 @@ import { usePromise } from '@xyo-network/react-shared'
 import { WalletInstance } from '@xyo-network/wallet-model'
 import { useState } from 'react'
 
-import { useContextAccount } from '../contexts'
+import { useContextAccount } from '../../../..'
 
 export interface AccountHookParams {
   account?: AccountInstance
@@ -13,17 +13,18 @@ export interface AccountHookParams {
 }
 
 export const useAccount = ({ wallet, account, path, required = false }: AccountHookParams = {}): [AccountInstance | undefined, Error | undefined] => {
-  let validationError: Error | undefined = undefined
-  if (wallet && account) {
-    validationError = Error('useAccount can not have both a wallet and an account in the parameters')
+  const [validationError, setValidationError] = useState<Error>()
+  if (wallet && account && !validationError) {
+    setValidationError(Error('useAccount can not have both a wallet and an account in the parameters'))
   }
 
-  if (path && account) {
-    validationError = Error('useAccount can not have both a path and an account in the parameters')
+  if (path && account && !validationError) {
+    setValidationError(Error('useAccount can not have both a path and an account in the parameters'))
   }
   const [error, setError] = useState<Error>()
   const [contextAccount] = useContextAccount(!(account || wallet) && required)
   const [activeAccount] = usePromise(async () => {
+    console.log('useAccount:activeAccount:usePromise')
     try {
       if (!validationError) {
         if (wallet) {
@@ -37,11 +38,15 @@ export const useAccount = ({ wallet, account, path, required = false }: AccountH
         }
       }
     } catch (ex) {
-      setError(ex as Error)
+      const error = ex as Error
+      console.error(error.message)
+      setError(error)
     }
   }, [path, wallet, contextAccount, validationError])
   if (validationError && !error) {
+    console.error(validationError.message)
     setError(validationError)
   }
+
   return [error ? undefined : activeAccount, error]
 }
