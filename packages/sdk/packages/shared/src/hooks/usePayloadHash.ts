0924a7de565152e7580ca7c5680a754e4c3ad97c
Arie Trouw <arie.trouw@xylabs.com>
2023-06-26 13:34:07 -0700

    hook dependency memos

--- packages/sdk/packages/shared/src/hooks/usePayloadHash.ts
+++ packages/sdk/packages/shared/src/hooks/usePayloadHash.ts
@@ -1,19 +1,21 @@
 import { PayloadHasher } from '@xyo-network/core'
 import { Payload } from '@xyo-network/payload-model'
-import { DependencyList } from 'react'
+import { DependencyList, useMemo } from 'react'
 
 import { usePromise } from './usePromise'
 
-export const usePayloadHash = <TPayload extends Payload>(payload: TPayload | undefined | null, dependencies: DependencyList) => {
-  return usePromise(() => (payload ? PayloadHasher.hashAsync(payload) : undefined), dependencies)[0]
+export const usePayloadHash = <TPayload extends Payload>(payload: TPayload | undefined | null, dependencies: DependencyList = []) => {
+  const deps = useMemo(() => [payload, ...dependencies], [payload, dependencies])
+  return usePromise(() => (payload ? PayloadHasher.hashAsync(payload) : undefined), deps)[0]
 }
 
-export const usePayloadHashes = <TPayload extends Payload>(payloads: TPayload[] | undefined | null, dependencies: DependencyList) => {
+export const usePayloadHashes = <TPayload extends Payload>(payloads: TPayload[] | undefined | null, dependencies: DependencyList = []) => {
+  const deps = useMemo(() => [payloads, ...dependencies], [payloads, dependencies])
   return usePromise(
     () =>
       payloads
         ? Promise.all(payloads.map<Promise<[TPayload, string]>>(async (payload) => [payload, await PayloadHasher.hashAsync(payload)]))
         : undefined,
-    dependencies,
+    deps,
   )[0]
 }
