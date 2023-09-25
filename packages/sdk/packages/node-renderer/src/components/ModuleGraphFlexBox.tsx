bad3dd319e81047bbe886d7bccb9cdc7107705b5
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-09-25 14:56:45 -0400

    working story to go from node to bridge

--- packages/sdk/packages/node-renderer/src/components/ModuleGraphFlexBox.tsx
+++ packages/sdk/packages/node-renderer/src/components/ModuleGraphFlexBox.tsx
@@ -1,24 +1,42 @@
-import { FlexBoxProps } from "@xylabs/react-flexbox";
-import { ModuleInstance } from "@xyo-network/module";
-import { EventObject } from "cytoscape";
-import { useEffect } from "react";
-import { useCytoscapeInstance } from "../contexts";
-import { useRelationalGraphOptions } from "../hooks";
-import { NodeRelationalGraphFlexBox } from "./RelationalGraph";
+import { FlexBoxProps } from '@xylabs/react-flexbox'
+import { ModuleInstance } from '@xyo-network/module'
+import { useModuleFromNode } from '@xyo-network/react-node'
+import { EventObject, NodeDataDefinition } from 'cytoscape'
+import { useEffect, useMemo, useState } from 'react'
+
+import { useCytoscapeInstance } from '../contexts'
+import { useCytoscapeElements, useRelationalGraphOptions } from '../hooks'
+import { NodeRelationalGraphFlexBox } from './RelationalGraph'
 
 export interface ModuleGraphFlexBoxProps extends FlexBoxProps {
-  rootModule?: ModuleInstance
+  rootModule?: ModuleInstance | null
 }
 
-export const ModuleGraphFlexBox: React.FC<ModuleGraphFlexBoxProps> = ({rootModule, ...props}) => {
-  const options = useRelationalGraphOptions(rootModule)
-
+export const ModuleGraphFlexBox: React.FC<ModuleGraphFlexBoxProps> = ({ rootModule, ...props }) => {
   const { cy } = useCytoscapeInstance(true)
+  const options = useRelationalGraphOptions(rootModule ?? undefined)
+
+  const [selectedElement, setSelectedElement] = useState<NodeDataDefinition>()
+  const { address: selectedAddress, rootNodeId: selectedRootNodeId } = selectedElement?.data() ?? {}
+  const [module] = useModuleFromNode(selectedAddress)
+  const newElements = useCytoscapeElements(module)
+
+  const updatedOptions = useMemo(() => {
+    if (newElements.length) {
+      const { elements: existingElements, ...rest } = options
+      return {
+        elements: [...newElements],
+        ...rest,
+      }
+    }
+  }, [newElements, options])
+
+  const resolvedOptions = updatedOptions ?? options
 
   useEffect(() => {
     const listener = (event: EventObject) => {
       const element = event.target[0]
-      console.log(element.data().address)
+      setSelectedElement(element)
     }
     if (cy) {
       cy.on('select', listener)
@@ -29,5 +47,5 @@ export const ModuleGraphFlexBox: React.FC<ModuleGraphFlexBoxProps> = ({rootModul
     }
   }, [cy])
 
-  return <NodeRelationalGraphFlexBox options={options} {...props}/>
+  return <NodeRelationalGraphFlexBox options={resolvedOptions} {...props} />
 }
