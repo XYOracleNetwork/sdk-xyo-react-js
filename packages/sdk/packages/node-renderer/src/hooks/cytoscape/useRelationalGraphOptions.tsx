2ec3d119e91adb7c51ccd7fb2350aceebf1374fb
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-09-25 14:56:17 -0400

    lint

--- packages/sdk/packages/node-renderer/src/hooks/cytoscape/useRelationalGraphOptions.tsx
+++ packages/sdk/packages/node-renderer/src/hooks/cytoscape/useRelationalGraphOptions.tsx
@@ -1,7 +1,8 @@
-import { ModuleInstance } from "@xyo-network/module";
-import { CytoscapeOptions } from "cytoscape";
-import { useCytoscapeElements } from "./useCytoscapeElements";
-import { useCytoscapeOptions } from "./useCytoscapeOptions";
+import { ModuleInstance } from '@xyo-network/module'
+import { CytoscapeOptions } from 'cytoscape'
+
+import { useCytoscapeElements } from './useCytoscapeElements'
+import { useCytoscapeOptions } from './useCytoscapeOptions'
 
 export const useRelationalGraphOptions = (module?: ModuleInstance): CytoscapeOptions => {
   const elements = useCytoscapeElements(module)
