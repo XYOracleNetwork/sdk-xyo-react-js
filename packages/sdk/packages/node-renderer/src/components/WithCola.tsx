8cd7871eb9ece5c7bed2c210f7e7650808582205
Arie Trouw <arie.trouw@xylabs.com>
2023-09-26 11:22:25 -0700

    @xylabs/buffer issue resolved

--- packages/sdk/packages/node-renderer/src/components/WithCola.tsx
+++ packages/sdk/packages/node-renderer/src/components/WithCola.tsx
@@ -1,12 +1,15 @@
-import { WithChildren } from '@xylabs/react-shared'
 import { use } from 'cytoscape'
 import cola from 'cytoscape-cola'
-import { useEffect } from 'react'
+import { ReactElement, useEffect } from 'react'
+
+type WithChildren<T = unknown> = Omit<T, 'children'> & {
+  children?: ReactElement | undefined
+}
 
 export const WithCola: React.FC<WithChildren> = ({ children }) => {
   useEffect(() => {
     use(cola)
   }, [])
 
-  return children
+  return children ?? null
 }
