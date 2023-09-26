9c20fb373835f7c9b31f2ab815053d7bd706a201
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-09-26 11:07:59 -0400

    ensure cola is linked before loading the rest of the graph

--- packages/sdk/packages/node-renderer/src/components/WithCola.tsx
+++ packages/sdk/packages/node-renderer/src/components/WithCola.tsx
@@ -1,15 +1,17 @@
 import { use } from 'cytoscape'
 import cola from 'cytoscape-cola'
-import { ReactElement, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 type WithChildren<T = unknown> = Omit<T, 'children'> & {
   children?: ReactElement | undefined
 }
 
 export const WithCola: React.FC<WithChildren> = ({ children }) => {
+  const [initialized, setInitialized] = useState(false)
   useEffect(() => {
     use(cola)
+    setInitialized(true)
   }, [])
 
-  return children ?? null
+  return <>{initialized ? children : undefined}</>
 }
