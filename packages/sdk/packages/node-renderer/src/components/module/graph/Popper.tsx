65d515eed9bf82ff223349eb6394a6550c101c67
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-09-28 15:42:50 -0400

    slight fade on popper bg color

--- packages/sdk/packages/node-renderer/src/components/module/graph/Popper.tsx
+++ packages/sdk/packages/node-renderer/src/components/module/graph/Popper.tsx
@@ -1,4 +1,4 @@
-import { Chip, Popper, PopperProps, Typography } from '@mui/material'
+import { alpha, Chip, Popper, PopperProps, Theme, Typography } from '@mui/material'
 import { FlexCol, FlexRow } from '@xylabs/react-flexbox'
 import { Identicon } from '@xylabs/react-identicon'
 
@@ -11,7 +11,7 @@ export interface ModuleHoverPopperProps extends PopperProps {
 export const ModuleHoverPopper: React.FC<ModuleHoverPopperProps> = ({ address, element, name, ...props }) => {
   return (
     <Popper anchorEl={element} {...props}>
-      <FlexCol gap={2} paper p={2}>
+      <FlexCol gap={2} p={2} paper sx={{ backgroundColor: (theme: Theme) => alpha(theme.palette.background.paper, 0.95) }}>
         <FlexRow gap={2}>
           <Identicon value={address} size={24} />
           <Typography>{name}</Typography>
