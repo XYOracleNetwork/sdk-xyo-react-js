7e4854ec84817c0bcbfdc0c186642f676a31da50
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-09-29 10:09:49 -0400

    only show popper when anchor element exists to fix console error

--- packages/sdk/packages/node-renderer/src/components/module/graph/Popper.tsx
+++ packages/sdk/packages/node-renderer/src/components/module/graph/Popper.tsx
@@ -4,20 +4,24 @@ import { Identicon } from '@xylabs/react-identicon'
 
 export interface ModuleHoverPopperProps extends PopperProps {
   address?: string
-  element?: HTMLElement | null
+  element?: PopperProps['anchorEl']
   name?: string
 }
 
 export const ModuleHoverPopper: React.FC<ModuleHoverPopperProps> = ({ address, element, name, ...props }) => {
   return (
-    <Popper anchorEl={element} {...props}>
-      <FlexCol gap={2} p={2} paper sx={{ backgroundColor: (theme: Theme) => alpha(theme.palette.background.paper, 0.95) }}>
-        <FlexRow gap={2}>
-          <Identicon value={address} size={24} />
-          <Typography>{name}</Typography>
-        </FlexRow>
-        <Chip label={address} color={'primary'} />
-      </FlexCol>
-    </Popper>
+    <>
+      {element ? (
+        <Popper anchorEl={element} {...props}>
+          <FlexCol gap={2} p={2} paper sx={{ backgroundColor: (theme: Theme) => alpha(theme.palette.background.paper, 0.95) }}>
+            <FlexRow gap={2}>
+              <Identicon value={address} size={24} />
+              <Typography>{name}</Typography>
+            </FlexRow>
+            <Chip label={address} color={'primary'} />
+          </FlexCol>
+        </Popper>
+      ) : null}
+    </>
   )
 }
