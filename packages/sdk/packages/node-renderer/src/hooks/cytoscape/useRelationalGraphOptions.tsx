19bfdf95f16a6325889a62e569dfdae20cf9fda0
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-10-02 22:10:44 -0400

    lint

--- packages/sdk/packages/node-renderer/src/hooks/cytoscape/useRelationalGraphOptions.tsx
+++ packages/sdk/packages/node-renderer/src/hooks/cytoscape/useRelationalGraphOptions.tsx
@@ -2,7 +2,7 @@ import { ModuleInstance } from '@xyo-network/module'
 import { useState } from 'react'
 
 import { ConcentricLayout } from '../../Cytoscape'
-import { useCytoscapeElements } from './elements/useCytoscapeElements'
+import { useCytoscapeElements } from './elements'
 import { useCytoscapeOptions } from './useCytoscapeOptions'
 import { useCytoscapeStyle } from './useCytoscapeStyle'
 
