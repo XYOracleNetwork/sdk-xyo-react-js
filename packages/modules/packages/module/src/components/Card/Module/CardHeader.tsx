4c6c064110fc35a5fadda31d089edc9696bfd561
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-10-30 17:09:56 -0400

    fix console warnings

--- packages/modules/packages/module/src/components/Card/Module/CardHeader.tsx
+++ packages/modules/packages/module/src/components/Card/Module/CardHeader.tsx
@@ -1,9 +1,10 @@
 import { CardHeader, CardHeaderProps } from '@mui/material'
 import { Module } from '@xyo-network/module'
 import { findNetworkComponent } from '@xyo-network/react-shared'
-import { Fragment } from 'react'
 
 import { ModuleRenderProps } from '../../../ModuleRenderProps'
+
+const moduleTypes = ['sentinel', 'bridge', 'archivist', 'diviner', 'node', 'witness']
 
 const getModuleIcons = (moduleType: string, module: Module) => {
   return module?.queries.find((query) => query.startsWith(`network.xyo.query.${moduleType}`)) ? findNetworkComponent(moduleType)?.icon : null
@@ -18,9 +19,9 @@ export const ModuleCardHeader: React.FC<ModuleRenderProps & CardHeaderProps> = (
         avatar ?? (
           <>
             {module
-              ? ['sentinel', 'bridge', 'archivist', 'diviner', 'node', 'witness'].map((moduleType) => {
+              ? moduleTypes.map((moduleType) => {
                   const Icon = getModuleIcons(moduleType, module)
-                  return <Fragment key={moduleType}>{Icon ? <Icon fontSize={'large'} color="primary" /> : null}</Fragment>
+                  return Icon ? <Icon key={moduleType} fontSize={'large'} color="primary" /> : null
                 })
               : null}
           </>
