9c392a57ac8daad22df7ffce4617c0ec141cacba
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-11-30 17:03:34 -0500

    build table cells programmatically

--- packages/sdk/packages/connected-accounts/src/components/wallet/table/ConnectedWalletsTableRow.tsx
+++ packages/sdk/packages/connected-accounts/src/components/wallet/table/ConnectedWalletsTableRow.tsx
@@ -2,6 +2,7 @@ import { Check } from '@mui/icons-material'
 import { Button, TableCell, TableRow, TableRowProps, Typography, useTheme } from '@mui/material'
 import { ConstrainedImage, EthWalletConnectorBase, useEthWallet } from '@xylabs/react-crypto'
 import { FlexRow } from '@xylabs/react-flexbox'
+import { ReactNode, useMemo } from 'react'
 
 export interface WalletConnectionsTableRowInnerProps extends TableRowProps {
   additionalAccounts?: string[]
@@ -24,35 +25,44 @@ export const WalletConnectionsTableRowInner: React.FC<WalletConnectionsTableRowI
   const totalAccounts = (additionalAccounts?.length ?? 0) + (currentAccount?.length ?? 0)
   const connected = currentAccount?.length ?? 0 > 0
 
-  return (
-    <TableRow {...props}>
-      <TableCell>
-        <FlexRow gap={2} justifyContent="start">
-          <ConstrainedImage constrainedValue={theme.spacing(4)} src={icon} />
-          {name}
-        </FlexRow>
-      </TableCell>
-      <TableCell>{chainName}</TableCell>
-      <TableCell>{totalAccounts}</TableCell>
-      <TableCell>
-        <FlexRow gap={2} justifyContent="start">
-          {connected ? (
-            <Typography sx={{ display: 'inline-flex', gap: 0.5 }}>
-              <Check />
-              Connected
-            </Typography>
-          ) : (
-            <Button variant={'contained'}>Connect</Button>
-          )}
-          {connected ? (
-            <Button variant={'outlined'} color={'error'}>
-              Revoke
-            </Button>
-          ) : null}
-        </FlexRow>
-      </TableCell>
-    </TableRow>
-  )
+  const Cells = useMemo(() => {
+    const TableCells: Record<string, ReactNode> = {
+      wallet: (
+        <TableCell>
+          <FlexRow gap={2} justifyContent="start">
+            <ConstrainedImage constrainedValue={theme.spacing(4)} src={icon} />
+            {name}
+          </FlexRow>
+        </TableCell>
+      ),
+      // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+      chain: <TableCell>{chainName}</TableCell>,
+      // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+      accounts: <TableCell>{totalAccounts}</TableCell>,
+      actions: (
+        <TableCell>
+          <FlexRow gap={2} justifyContent="start">
+            {connected ? (
+              <Typography sx={{ display: 'inline-flex', gap: 0.5 }}>
+                <Check />
+                Connected
+              </Typography>
+            ) : (
+              <Button variant={'contained'}>Connect</Button>
+            )}
+            {connected ? (
+              <Button variant={'outlined'} color={'error'}>
+                Revoke
+              </Button>
+            ) : null}
+          </FlexRow>
+        </TableCell>
+      ),
+    }
+    return TableCells
+  }, [chainName, connected, icon, name, theme, totalAccounts])
+
+  return <TableRow {...props}>{Object.values(Cells).map((cell) => cell)}</TableRow>
 }
 
 export interface WalletConnectionsTableRow extends TableRowProps {
