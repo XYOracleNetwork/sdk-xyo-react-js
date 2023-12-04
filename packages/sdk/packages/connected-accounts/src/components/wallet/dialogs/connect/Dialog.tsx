f71a7db041fd87afd82b426427a22d598af32314
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-04 14:49:25 -0500

    xyo and provider logos

--- packages/sdk/packages/connected-accounts/src/components/wallet/dialogs/connect/Dialog.tsx
+++ packages/sdk/packages/connected-accounts/src/components/wallet/dialogs/connect/Dialog.tsx
@@ -1,6 +1,9 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle, List, ListItem, Typography } from '@mui/material'
-import { FlexRow } from '@xylabs/react-flexbox'
+import { SyncAlt } from '@mui/icons-material'
+import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle, Link, Typography } from '@mui/material'
+import { ConstrainedImage } from '@xylabs/react-crypto'
+import { FlexCol, FlexRow } from '@xylabs/react-flexbox'
 
+import { xyoColorLogo } from '../../../../img'
 import { ActiveProvider } from '../../lib'
 
 export interface ConnectWalletDialogProps extends DialogProps {
@@ -8,6 +11,8 @@ export interface ConnectWalletDialogProps extends DialogProps {
 }
 
 export const ConnectWalletDialog: React.FC<ConnectWalletDialogProps> = ({ activeProvider, ...props }) => {
+  const { icon, providerName } = activeProvider ?? {}
+
   const onConnect = async () => {
     try {
       await activeProvider?.connectWallet?.()
@@ -18,23 +23,41 @@ export const ConnectWalletDialog: React.FC<ConnectWalletDialogProps> = ({ active
   }
 
   return (
-    <Dialog {...props}>
+    <Dialog PaperProps={{ sx: { display: 'flex', gap: 4 } }} {...props}>
       <DialogTitle sx={{ textAlign: 'center' }}>XYO Wants To Access The Blockchain on Your Behalf</DialogTitle>
-      <DialogContent sx={{ flexDirection: 'column', gap: 3 }}>
-        <FlexRow gap={2}>
-          <span>XYO Logo</span>
-          <span>handshake icon</span>
-          <span>Wallet Logo</span>
+      <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+        <FlexRow gap={4} justifyContent="space-evenly">
+          <FlexCol>
+            <img alt="XYO Logo" src={xyoColorLogo} style={{ height: '48px' }} />
+            <Typography variant="subtitle1">XYO App</Typography>
+          </FlexCol>
+          <SyncAlt fontSize={'large'} />
+          <FlexCol>
+            <ConstrainedImage constrainedValue={'48px'} src={icon} alt={providerName} style={{ height: '48px', maxWidth: '48px' }} />
+            <Typography variant="subtitle1">{providerName}</Typography>
+          </FlexCol>
         </FlexRow>
-        <Typography>This will allow XYO to:</Typography>
-        <List>
-          <ListItem>View your wallet account(s) and address(es)</ListItem>
-          <ListItem>Read-only access to browse the public blockchain(s) you select</ListItem>
-        </List>
-        <Typography sx={{ textAlign: 'center' }}>
-          You control what accounts to share and what blockchains to view. You can see or revoke access via your wallet&apos;s settings at anytime.
-          View more on XYO&apos;s sovereign data philosophy [here].
-        </Typography>
+        <FlexCol gap={4}>
+          <Typography fontWeight="bold" sx={{ textAlign: 'center' }}>
+            This will allow XYO to:
+          </Typography>
+          <ul>
+            <li>View your wallet account(s) and address(es)</li>
+            <li>Read-only access to browse the public blockchain(s) you select</li>
+          </ul>
+          <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
+            You control what accounts to share and what blockchains to view. You can see or revoke access via your wallet&apos;s settings at anytime.
+            View more on XYO&apos;s sovereign data philosophy{' '}
+            <Link
+              href="https://cointelegraph.com/innovation-circle/decentralization-and-sovereignty-debunking-our-approach-to-digital-sovereignty"
+              sx={{ fontWeight: 'bold' }}
+              target="_blank"
+            >
+              here
+            </Link>
+            .
+          </Typography>
+        </FlexCol>
       </DialogContent>
       <DialogActions>
         <Button variant="outlined" onClick={() => props.onClose?.({}, 'escapeKeyDown')}>
