0dc33c41da235d09f0a1de0c0ea0460a6c6dd3c4
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-04 14:56:54 -0500

    decompose into small components

--- packages/sdk/packages/connected-accounts/src/components/wallet/dialogs/connect/Dialog.tsx
+++ packages/sdk/packages/connected-accounts/src/components/wallet/dialogs/connect/Dialog.tsx
@@ -1,10 +1,8 @@
-import { SyncAlt } from '@mui/icons-material'
-import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle, Link, Typography } from '@mui/material'
-import { ConstrainedImage } from '@xylabs/react-crypto'
-import { FlexCol, FlexRow } from '@xylabs/react-flexbox'
+import { Button, Dialog, DialogActions, DialogContent, DialogProps, DialogTitle } from '@mui/material'
 
-import { xyoColorLogo } from '../../../../img'
 import { ActiveProvider } from '../../lib'
+import { LinkedProvidersFlexbox } from './LinkedProvidersFlexbox'
+import { WalletPermissionsFlexbox } from './Permissions'
 
 export interface ConnectWalletDialogProps extends DialogProps {
   activeProvider?: ActiveProvider
@@ -26,38 +24,8 @@ export const ConnectWalletDialog: React.FC<ConnectWalletDialogProps> = ({ active
     <Dialog PaperProps={{ sx: { display: 'flex', gap: 4 } }} {...props}>
       <DialogTitle sx={{ textAlign: 'center' }}>XYO Wants To Access The Blockchain on Your Behalf</DialogTitle>
       <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-        <FlexRow gap={4} justifyContent="space-evenly">
-          <FlexCol>
-            <img alt="XYO Logo" src={xyoColorLogo} style={{ height: '48px' }} />
-            <Typography variant="subtitle1">XYO App</Typography>
-          </FlexCol>
-          <SyncAlt fontSize={'large'} />
-          <FlexCol>
-            <ConstrainedImage constrainedValue={'48px'} src={icon} alt={providerName} style={{ height: '48px', maxWidth: '48px' }} />
-            <Typography variant="subtitle1">{providerName}</Typography>
-          </FlexCol>
-        </FlexRow>
-        <FlexCol gap={4}>
-          <Typography fontWeight="bold" sx={{ textAlign: 'center' }}>
-            This will allow XYO to:
-          </Typography>
-          <ul>
-            <li>View your wallet account(s) and address(es)</li>
-            <li>Read-only access to browse the public blockchain(s) you select</li>
-          </ul>
-          <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
-            You control what accounts to share and what blockchains to view. You can see or revoke access via your wallet&apos;s settings at anytime.
-            View more on XYO&apos;s sovereign data philosophy{' '}
-            <Link
-              href="https://cointelegraph.com/innovation-circle/decentralization-and-sovereignty-debunking-our-approach-to-digital-sovereignty"
-              sx={{ fontWeight: 'bold' }}
-              target="_blank"
-            >
-              here
-            </Link>
-            .
-          </Typography>
-        </FlexCol>
+        <LinkedProvidersFlexbox icon={icon} providerName={providerName} />
+        <WalletPermissionsFlexbox />
       </DialogContent>
       <DialogActions>
         <Button variant="outlined" onClick={() => props.onClose?.({}, 'escapeKeyDown')}>
---
+++ packages/sdk/packages/connected-accounts/src/components/wallet/dialogs/connect/Permissions.tsx
@@ -0,0 +1,30 @@
+import { Link, Typography } from '@mui/material'
+import { FlexBoxProps, FlexCol } from '@xylabs/react-flexbox'
+
+export interface WalletPermissionsFlexBoxProps extends FlexBoxProps {}
+
+export const WalletPermissionsFlexbox: React.FC<WalletPermissionsFlexBoxProps> = (props) => {
+  return (
+    <FlexCol gap={4} {...props}>
+      <Typography fontWeight="bold" sx={{ textAlign: 'center' }}>
+        This will allow XYO to:
+      </Typography>
+      <ul>
+        <li>View your wallet account(s) and address(es)</li>
+        <li>Read-only access to browse the public blockchain(s) you select</li>
+      </ul>
+      <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
+        You control what accounts to share and what blockchains to view. You can see or revoke access via your wallet&apos;s settings at anytime. View
+        more on XYO&apos;s sovereign data philosophy{' '}
+        <Link
+          href="https://cointelegraph.com/innovation-circle/decentralization-and-sovereignty-debunking-our-approach-to-digital-sovereignty"
+          sx={{ fontWeight: 'bold' }}
+          target="_blank"
+        >
+          here
+        </Link>
+        .
+      </Typography>
+    </FlexCol>
+  )
+}
