b8aec5e2ea85f8bc7e486505718611c2a8701182
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-08 17:22:25 -0500

    more readable payload types

--- packages/sdk/packages/map/src/types/NetworkLocationAnswerBase.ts
+++ packages/sdk/packages/map/src/types/NetworkLocationAnswerBase.ts
@@ -7,24 +7,17 @@ export interface NetworkLocationAnswerBase<T> {
 
 export const NetworkLocationAnswerSchema = 'network.xyo.location.range.answer'
 export type NetworkLocationAnswerSchema = 'network.xyo.location.range.answer'
-export type NetworkLocationAnswerPayload = NetworkLocationAnswerBase<{ features: Feature<Point>[] }> &
-  Payload<{ schema: NetworkLocationAnswerSchema }>
+export type NetworkLocationAnswerPayload = Payload<NetworkLocationAnswerBase<{ features: Feature<Point>[] }>, NetworkLocationAnswerSchema>
 export const isNetworkLocationAnswer = isPayloadOfSchemaType<NetworkLocationAnswerPayload>(NetworkLocationAnswerSchema)
 
 export const NetworkLocationHeatmapAnswerSchema = 'network.xyo.location.heatmap.answer'
 export type NetworkLocationHeatmapAnswerSchema = 'network.xyo.location.heatmap.answer'
-export type NetworkLocationHeatmapAnswerPayload = Payload<{
-  schema: NetworkLocationHeatmapAnswerSchema & NetworkLocationAnswerBase<{ features: Feature<Polygon>[] }>
-}>
+export type NetworkLocationHeatmapAnswerPayload = Payload<NetworkLocationAnswerBase<{ features: Feature<Polygon>[]  }>, NetworkLocationHeatmapAnswerSchema>
 export const isNetworkLocationHeatmapAnswer = isPayloadOfSchemaType<NetworkLocationHeatmapAnswerPayload>(NetworkLocationHeatmapAnswerSchema)
 
 export const NetworkLocationHeatmapQuadkeyAnswerSchema = 'network.xyo.location.heatmap.quadkey.answer'
 export type NetworkLocationHeatmapQuadkeyAnswerSchema = 'network.xyo.location.heatmap.quadkey.answer'
-export type NetworkLocationHeatmapQuadkeyAnswerPayload = Payload<
-  {
-    schema: NetworkLocationHeatmapQuadkeyAnswerSchema
-  } & NetworkLocationAnswerBase<{ density: number; quadkey: string }[]>
->
+export type NetworkLocationHeatmapQuadkeyAnswerPayload = Payload<NetworkLocationAnswerBase<{ density: number; quadkey: string }[]>, NetworkLocationHeatmapQuadkeyAnswerSchema>
 export const isNetworkLocationHeatmapQuadkeyAnswer = isPayloadOfSchemaType<NetworkLocationHeatmapAnswerPayload>(
   NetworkLocationHeatmapQuadkeyAnswerSchema,
 )
