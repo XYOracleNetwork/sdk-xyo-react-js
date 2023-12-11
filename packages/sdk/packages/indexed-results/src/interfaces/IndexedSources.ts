78f071beee2fa4f98b5b986c6316780b534e1096
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-11 15:53:30 -0500

    lint

--- packages/sdk/packages/indexed-results/src/interfaces/IndexedSources.ts
+++ packages/sdk/packages/indexed-results/src/interfaces/IndexedSources.ts
@@ -1,10 +1,10 @@
 import { Payload } from '@xyo-network/payload-model'
 
 export interface ProcessIndexedResults {
-    /** decompose the query result (i.e. resolve the hashes that are in the query result) */
-    parseResults?: (results: Payload[]) => Promise<Payload[]>
-    /** Validates results from the diviner(s) to ensure they are of the expected type */
-    validateDivinerResults: (payloads: Payload[]) => Promise<Payload[] | undefined>
+  /** decompose the query result (i.e. resolve the hashes that are in the query result) */
+  parseResults?: (results: Payload[]) => Promise<Payload[]>
+  /** Validates results from the diviner(s) to ensure they are of the expected type */
+  validateDivinerResults: (payloads: Payload[]) => Promise<Payload[] | undefined>
 }
 
 export interface IndexedSources extends ProcessIndexedResults {
