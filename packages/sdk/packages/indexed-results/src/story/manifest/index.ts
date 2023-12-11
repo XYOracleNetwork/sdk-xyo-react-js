02f1e0ff714d251c0e07940596fc1d5415e2e9de
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-11 14:29:05 -0500

    pair manifests with locators

---
+++ packages/sdk/packages/indexed-results/src/story/manifest/index.ts
@@ -0,0 +1,5 @@
+export * from './ManifestNodeProvider'
+export * from './modules'
+export * from './nodes/archivist'
+export * from './nodes/sentinel'
+
--- packages/sdk/packages/indexed-results/src/story/node/manifests/index.ts
+++
@@ -1,4 +0,0 @@
-import archivistManifest from './archivist-manifest.json'
-import sentinelManifest from './sentinel-manifest.json'
-
-export { archivistManifest, sentinelManifest }
