c168ccd8a21a47b1afaa32fc5cc37b3130315a81
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-11 22:47:04 -0500

    export freshResult as function from hook

--- packages/sdk/packages/indexed-results/src/hooks/useTriggerFreshIndexedResult.tsx
+++ packages/sdk/packages/indexed-results/src/hooks/useTriggerFreshIndexedResult.tsx
@@ -1,14 +1,14 @@
-import { usePromise } from "@xylabs/react-promise";
+import { useCallback } from "react";
 import { IndexedResultsConfig } from "../interfaces";
 
 export const useTriggerFreshIndexedResult = (config: IndexedResultsConfig, trigger = false) => {
   const { freshIndexedResultConfig, processIndexedResults } = config
 
-  const [triggerResult] = usePromise(async () => {
+  const freshResult = useCallback(async () => {
     const { refresh } = freshIndexedResultConfig
     const { parseResults, validateDivinerResults } = processIndexedResults
     return trigger ? await refresh?.({ parseResults, validateDivinerResults }) : undefined
-  }, [freshIndexedResultConfig, trigger])
+  }, [freshIndexedResultConfig, processIndexedResults, trigger,])
 
-  return triggerResult
+  return freshResult
 }
