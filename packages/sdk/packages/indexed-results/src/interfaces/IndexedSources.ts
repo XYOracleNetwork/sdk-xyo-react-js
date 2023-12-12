f25294bb4cd6a4e9b987cd48c4c443aee02215dd
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-11 20:57:30 -0500

    make archivist optional

--- packages/sdk/packages/indexed-results/src/interfaces/IndexedSources.ts
+++ packages/sdk/packages/indexed-results/src/interfaces/IndexedSources.ts
@@ -1,7 +1,7 @@
 
 export interface IndexedSources {
   /** the source of the indexed query payloads */
-  archivist: string
+  archivist?: string
   /** diviner that can check the the archivist for the latest results */
   diviners: string[]
 }
