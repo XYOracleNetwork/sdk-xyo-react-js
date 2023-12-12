adc37f25340b764c883bebd48b87e06b23f41124
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-11 22:47:31 -0500

    integrate trigger and promise queue with task

--- packages/sdk/packages/indexed-results/src/hooks/useFreshIndexedResult.tsx
+++ packages/sdk/packages/indexed-results/src/hooks/useFreshIndexedResult.tsx
@@ -1,18 +1,42 @@
 import { usePromise } from "@xylabs/react-promise";
+import { Payload } from "@xyo-network/payload-model";
+import { PromiseQueue } from "../classes";
 import { IndexedResultsConfig } from "../interfaces";
-import { usePollDiviners } from "./support";
+import { PollDivinerConfig, usePollDiviners } from "./support";
 import { useTriggerFreshIndexedResult } from "./useTriggerFreshIndexedResult";
 
-export const useFreshIndexedResult = (config: IndexedResultsConfig) => {
-  const triggerResult = useTriggerFreshIndexedResult(config)
+export interface FreshIndexedResultConfig {
+  /** Indexed Results Configuration */
+  config: IndexedResultsConfig
+  /** Configuration for polling diviners */
+  pollingConfig?: PollDivinerConfig
+  /** Optional queue to manage polling requests */
+  queue?: PromiseQueue
+  /** Optional identifier for tasks in the queue */
+  taskId?: string
+  /** External trigger to start the hook logic */
+  trigger?: boolean
+}
 
-  const pollDiviners = usePollDiviners(config)
+export const useFreshIndexedResult = <TResult extends Payload = Payload>({ config, pollingConfig, queue, taskId, trigger}: FreshIndexedResultConfig) => {
+  const freshResult = useTriggerFreshIndexedResult(config, trigger)
+
+  const pollDiviners = usePollDiviners<TResult>(config, pollingConfig)
 
   const [result, error, state] = usePromise(async () => {
-    if (triggerResult) {
-      return await pollDiviners()
+    if (trigger) {
+      if (queue) {
+        const task = async () => {
+          await freshResult()
+          return await pollDiviners()
+        }
+        return await queue.addRequest<ReturnType<typeof task>>(task, taskId ?? Date.now().toString())
+      } else {
+        await freshResult()
+        return await pollDiviners()
+      }
     }
-  }, [pollDiviners, triggerResult])
+  }, [pollDiviners, freshResult, trigger, queue, taskId])
 
   return [result, error, state]
 }
