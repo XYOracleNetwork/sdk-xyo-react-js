28f6a49909283791d7252b088642081997757a92
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-14 13:04:40 -0500

    support polling for fetching indexed result without refresh

--- packages/sdk/packages/indexed-results/src/hooks/useIndexedResults.tsx
+++ packages/sdk/packages/indexed-results/src/hooks/useIndexedResults.tsx
@@ -1,12 +1,35 @@
 import { usePromise } from '@xylabs/react-promise'
+import { Payload } from '@xyo-network/payload-model'
 
-import { IndexedResultsConfig } from '../interfaces'
-import { useTryDiviners } from './support'
+import { IndexedResultsConfig, IndexedResultsQueue, PollingConfig } from '../interfaces'
+import { usePollDiviners } from './support'
 
-export const useIndexedResults = (config: IndexedResultsConfig) => {
-  const tryDiviners = useTryDiviners(config)
+export const useIndexedResults = <TResult extends Payload = Payload>(
+  /** Indexed Results Configuration */
+  config: IndexedResultsConfig,
+  /** Configuration for polling diviners */
+  pollingConfig?: PollingConfig,
+  /** Queue for handling Promise Results */
+  queueConfig?: IndexedResultsQueue,
+  /** External trigger to start the hook logic */
+  trigger?: boolean,
+) => {
+  const { queue, taskId } = queueConfig ?? {}
 
-  const [results] = usePromise(async () => await tryDiviners(), [tryDiviners])
+  const pollDiviners = usePollDiviners<TResult>(config, pollingConfig)
+
+  const [results] = usePromise(async () => {
+    if (trigger) {
+      if (queue) {
+        const task = async () => {
+          return await pollDiviners()
+        }
+        return await queue.addRequest<ReturnType<typeof task>>(task, taskId ?? Date.now().toString())
+      } else {
+        return await pollDiviners()
+      }
+    }
+  }, [pollDiviners, queue, taskId, trigger])
 
   return [results]
 }
