b3e279f98715cc03f9a2c5e9552807b92af4f4c9
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-14 12:57:14 -0500

    pass archivist instance to avoid resolving in a node

--- packages/sdk/packages/indexed-results/src/hooks/support/useFetchModules.tsx
+++ packages/sdk/packages/indexed-results/src/hooks/support/useFetchModules.tsx
@@ -1,22 +1,20 @@
 import { usePromise } from '@xylabs/react-promise'
 import { DivinerInstance, isDivinerInstance } from '@xyo-network/diviner-model'
-import { useArchivistFromNode } from '@xyo-network/react-archivist'
 import { useProvidedNode } from '@xyo-network/react-node'
 
 import { IndexedSources } from '../../interfaces'
 
 export const useFetchModules = (config: IndexedSources) => {
-  const { archivist: archivistName, diviners: divinerNames } = config
+  const { diviners: divinerNames, storageArchivist } = config
   const [node] = useProvidedNode()
 
-  const [archivist] = useArchivistFromNode(archivistName)
   const [diviners] = usePromise<DivinerInstance[]>(async () => {
     const resolvedDiviners = node ? await node.resolve({ name: divinerNames }) : []
     return resolvedDiviners.filter((module) => isDivinerInstance(module)) as DivinerInstance[]
   }, [divinerNames, node])
 
   return {
-    archivist,
+    archivist: storageArchivist,
     diviners,
   }
 }
--- packages/sdk/packages/indexed-results/src/interfaces/IndexedSources.ts
+++ packages/sdk/packages/indexed-results/src/interfaces/IndexedSources.ts
@@ -1,6 +1,8 @@
+import { ArchivistInstance } from '@xyo-network/archivist-model'
+
 export interface IndexedSources {
-  /** the source of the indexed query payloads */
-  archivist?: string
   /** diviner that can check the the archivist for the latest results */
   diviners: string[]
+  /** the archivist to store the resolved payloads from index payloads */
+  storageArchivist?: ArchivistInstance
 }
