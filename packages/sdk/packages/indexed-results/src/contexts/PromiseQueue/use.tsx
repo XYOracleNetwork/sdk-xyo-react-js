f4163fc0de5c6a0b0f198c5ef0d5ba1c8bffc456
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-12-19 10:01:11 -0500

    text cleanup

--- packages/sdk/packages/indexed-results/src/contexts/PromiseQueue/use.tsx
+++ packages/sdk/packages/indexed-results/src/contexts/PromiseQueue/use.tsx
@@ -3,4 +3,4 @@ import { useContextEx } from '@xyo-network/react-shared'
 import { PromiseQueueContext } from './Context'
 import { PromiseQueueState } from './State'
 
-export const usePromiseQueue = (required = true) => useContextEx<PromiseQueueState>(PromiseQueueContext, 'PromiseQueueContext', required)
+export const usePromiseQueue = (required = true) => useContextEx<PromiseQueueState>(PromiseQueueContext, 'PromiseQueue', required)
