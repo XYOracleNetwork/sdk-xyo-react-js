36636b0b22ca86b4e38970686ea2d7102e754895
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-01-04 16:38:11 -0500

    polling strategies

--- packages/sdk/packages/indexed-results/src/interfaces/PollingConfig.ts
+++ packages/sdk/packages/indexed-results/src/interfaces/PollingConfig.ts
@@ -1,6 +1,11 @@
+import { Payload } from '@xyo-network/payload-model'
+
+export type PollResult = Payload[] | null | undefined
+
 export interface PollingConfig {
   initialDelay?: number
   maxDelay?: number
   /** If null, polling is continuous. */
   maxRetries: number | null
+  onFoundResult?: <T extends PollResult = PollResult>(result: T) => boolean
 }
---
+++ packages/sdk/packages/indexed-results/src/interfaces/PollingStrategies.ts
@@ -0,0 +1,24 @@
+import { PollingConfig } from './PollingConfig'
+
+export type PollingStrategyNames = 'Continuous' | 'None' | 'TillComplete'
+
+const continuousPolling = {
+  initialDelay: 500,
+  maxRetries: null,
+}
+
+export const PollingStrategies: Record<Partial<PollingStrategyNames>, PollingConfig> = {
+  Continuous: {
+    ...continuousPolling,
+  },
+  None: {
+    maxRetries: 1,
+  },
+  TillComplete: {
+    ...continuousPolling,
+    onFoundResult: () => {
+      console.warn('Polling strategy set to TillComplete but missing onFoundResult callback')
+      return false
+    },
+  },
+}
