71ac55bd469186a5b43cfa7453ce477b454b9833
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-03-07 16:08:01 -0500

    dialog stories

---
+++ packages/sdk/packages/payload/packages/raw-info/src/components/Dialog/RawInfoDialog.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { FlexCol } from "@xylabs/react-flexbox";
+import { Payload } from "@xyo-network/payload-model";
+import { RawInfoDialog } from "./RawInfoDialog";
+
+type TestPayload = Payload<{ id: string, type: string, schema: string }, 'network.xyo.test'>
+const TestPayload: TestPayload = {
+  id: '123',
+  type: 'test',
+  schema: 'network.xyo.test',
+}
+
+export default {
+  title: 'payload/RawInfoDialog',
+  component: RawInfoDialog,
+} as Meta
+
+const Template: StoryFn<typeof RawInfoDialog> = (args) => <FlexCol><RawInfoDialog {...args} /></FlexCol>
+
+const Default = Template.bind({})
+const WithPayload = Template.bind({})
+WithPayload.args = {
+  dialogContent: 'This is a test',
+  open: true,
+  payload: TestPayload
+}
+
+export { Default, WithPayload };
