619f4f455e9ae40be3d6e9907b854a9436face21
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-03-07 16:06:30 -0500

    info button story

---
+++ packages/sdk/packages/payload/packages/raw-info/src/components/Button/RawInfoButton.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { FlexCol } from "@xylabs/react-flexbox";
+import { Payload } from "@xyo-network/payload-model";
+import { RawInfoButton } from "./RawInfoButton";
+
+type TestPayload = Payload<{ id: string, type: string, schema: string }, 'network.xyo.test'>
+const TestPayload: TestPayload = {
+  id: '123',
+  type: 'test',
+  schema: 'network.xyo.test',
+}
+
+export default {
+  title: 'payload/RawInfoButton',
+  component: RawInfoButton,
+} as Meta
+
+const Template: StoryFn<typeof RawInfoButton> = (args) => <FlexCol><RawInfoButton {...args} /></FlexCol>
+
+const Default = Template.bind({})
+const WithPayload = Template.bind({})
+WithPayload.args = {
+  dialogContent: 'This is a test',
+  payload: TestPayload
+}
+
+export { Default, WithPayload };
