0e8348727d3a56270150594e745937d4bf404544
Arie Trouw <arie.trouw@xylabs.com>
2024-08-06 15:12:08 -0700

    esm only version works in storybook

--- packages/sdk/packages/form/packages/credit-card/src/components/index.ts
+++ packages/sdk/packages/form/packages/credit-card/src/components/index.ts
@@ -1,4 +1,3 @@
 export * from './controls/index.ts'
 export * from './form/index.ts'
-export * from './img/index.ts'
 export * from './support/index.ts'
--- packages/sdk/packages/form/packages/credit-card/src/types/images.d.ts
+++ packages/sdk/packages/form/packages/credit-card/src/types/images.d.ts
@@ -1,5 +1,29 @@
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.svg'
-declare module '*.gif'
-declare module '*.webp'
+declare module '*.svg' {
+  const content: string
+  export default content
+}
+
+declare module '*.png' {
+  const content: string
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
+
+declare module '*.jpeg' {
+  const content: string
+  export default content
+}
+
+declare module '*.gif' {
+  const content: string
+  export default content
+}
+
+declare module '*.webp' {
+  const content: string
+  export default content
+}
