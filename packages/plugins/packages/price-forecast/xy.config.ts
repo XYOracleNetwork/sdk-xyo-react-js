6049e0fb5b5c8eeba329a218c862f9de3b779b64
Arie Trouw <arie.trouw@xylabs.com>
2024-08-06 14:16:35 -0700

    WIP

---
+++ packages/plugins/packages/price-forecast/xy.config.ts
@@ -0,0 +1,12 @@
+import { XyTsupConfig } from '@xylabs/ts-scripts-yarn3'
+const config: XyTsupConfig = {
+  compile: {
+    browser: {
+      src: true,
+    },
+    node: {},
+    neutral: {},
+  },
+}
+
+export default config
