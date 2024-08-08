9c524f76b2ea00728a5440829f6f8d44ad46f8d0
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-08-08 10:30:31 -0400

    working e2e in story

--- packages/sdk/packages/share/src/out/dialog/ShareOutDialog.stories.tsx
+++ packages/sdk/packages/share/src/out/dialog/ShareOutDialog.stories.tsx
@@ -1,7 +1,12 @@
 import { Button, Typography } from '@mui/material'
 import { Meta, StoryFn } from '@storybook/react'
+import { FlexCol, FlexGrowCol, FlexRow } from '@xylabs/react-flexbox'
 import React, { useMemo, useState } from 'react'
 
+import { CopyLinkTextField } from '../CopyLinkTextField.tsx'
+import { GenerateShareLinkButton } from '../GenerateShareLinkButton.tsx'
+import { ShareOutHeadingFlexbox } from '../HeadingFlexbox.tsx'
+import { SocialButtonsFlexbox } from '../SocialButtonsFlexbox.tsx'
 import { ShareOutDialog } from './ShareOutDialog.tsx'
 
 export default {
@@ -15,12 +20,38 @@ const Template: StoryFn<typeof ShareOutDialog> = (props) => {
 const TemplateWithContent: StoryFn<typeof ShareOutDialog> = (props) => {
   const [open, setOpen] = useState(false)
   const onClose = () => setOpen(false)
+  const [shareUrl, setShareUrl] = useState('')
+  const [loading, setLoading] = useState(false)
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { open: unused, ...restProps } = props
 
   const cardImg = 'https://picsum.photos/100/100'
+  const shareLinkName = 'My Share Link'
 
-  const shareOutDialogContent = useMemo(() => <Typography>Share Out DialogContent</Typography>, [])
+  const generateLink = async () => {
+    setLoading(true)
+    await new Promise(resolve => setTimeout(() => {
+      setShareUrl('https://example.com')
+      setLoading(false)
+      resolve(null)
+    }, 2000))
+  }
+
+  const shareOutDialogContent = useMemo(() => (
+    <FlexGrowCol gap={2}>
+      <ShareOutHeadingFlexbox shareLinkName={shareLinkName} shareUrl={shareUrl} />
+      {shareUrl
+        ? (
+            <>
+              <CopyLinkTextField shareLinkName={shareLinkName} shareUrl={shareUrl} />
+              <SocialButtonsFlexbox shareUrl={shareUrl} />
+            </>
+          )
+        : <GenerateShareLinkButton loading={loading} onClick={generateLink} />}
+    </FlexGrowCol>
+  ), [loading, generateLink, shareLinkName, shareUrl])
+
   const shareOutDialogActions = ({ onClose }: { onClose?: () => void }) => <Button onClick={onClose} variant="outlined">Cancel</Button>
 
   return (
@@ -32,9 +63,6 @@ const TemplateWithContent: StoryFn<typeof ShareOutDialog> = (props) => {
 }
 
 const Default = Template.bind({})
-Default.args = {
-  open: true,
-}
 
 const WithContent = TemplateWithContent.bind({})
 WithContent.args = {}
