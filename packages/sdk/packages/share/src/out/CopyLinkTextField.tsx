29d7d98bff84af7ad433528179a8f0c9c94367a8
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-08-08 10:03:20 -0400

    Copy Link Textfield component

---
+++ packages/sdk/packages/share/src/out/CopyLinkTextField.tsx
@@ -0,0 +1,48 @@
+import { CopyAllRounded } from '@mui/icons-material'
+import { IconButton, InputAdornment, StandardTextFieldProps, TextField } from '@mui/material'
+import { forget } from '@xylabs/forget'
+import React, { useState } from 'react'
+
+export interface CopyLinkTextFieldProps extends StandardTextFieldProps {
+  shareLinkName?: string
+  shareUrl?: string
+}
+
+export const CopyLinkTextField: React.FC<CopyLinkTextFieldProps> = ({ shareLinkName, shareUrl, ...props }) => {
+  const [error, setError] = useState<Error>()
+
+  const copyToClipboard = async (link?: string) => {
+    if (link) {
+      try {
+        await navigator.clipboard.writeText(link)
+      } catch (e) {
+        const message = 'Error copying shareUrl to clipboard'
+        console.error(message, e, link)
+        setError(new Error(message))
+      }
+    } else {
+      console.warn('tried to copy shareUrl before it was generated')
+    }
+  }
+
+  return (
+    <TextField
+      disabled
+      error={!!error}
+      helperText={error?.message}
+      // to override the color that appears when it's a text field, only on dark mode
+      sx={{ input: { WebkitTextFillColor: 'lightgray !important' } }}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton aria-label={`copy ${shareLinkName} link`} onClick={() => forget(copyToClipboard(shareUrl))} edge="end">
+              <CopyAllRounded />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+      value={shareUrl}
+      {...props}
+    />
+  )
+}
