c9f7c0dc4b23e09dac21c07a78116e7883c9439b
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-08-08 09:53:18 -0400

    social buttons story

---
+++ packages/sdk/packages/share/src/out/SocialButtonsFlexbox.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryFn } from '@storybook/react'
+import React from 'react'
+
+import { SocialButtonsFlexbox } from './SocialButtonsFlexbox.tsx'
+
+export default {
+  title: 'modules/ShareOut/SocialButtonsFlexbox',
+} as Meta<typeof SocialButtonsFlexbox>
+
+const Template: StoryFn<typeof SocialButtonsFlexbox> = (props) => {
+  return <SocialButtonsFlexbox {...props} />
+}
+
+const Default = Template.bind({})
+Default.args = {}
+
+const WithShareUrl = Template.bind({})
+WithShareUrl.args = {
+  shareUrl: 'https://google.com',
+}
+
+export { Default, WithShareUrl }
