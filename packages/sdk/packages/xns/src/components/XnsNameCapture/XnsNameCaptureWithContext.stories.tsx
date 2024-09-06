e95616c97bf0bd41850dbd1b62f11fb2035aaaf1
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-09-06 10:21:34 -0400

    stories to reflect routing updates

--- packages/sdk/packages/xns/src/components/XnsNameCapture/XnsNameCaptureWithContext.stories.tsx
+++ packages/sdk/packages/xns/src/components/XnsNameCapture/XnsNameCaptureWithContext.stories.tsx
@@ -1,4 +1,6 @@
-import type { Meta, StoryFn } from '@storybook/react'
+import type {
+  Decorator, Meta, StoryFn,
+} from '@storybook/react'
 import React from 'react'
 import type { To } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,15 +9,28 @@ import { XnsNameCaptureWithContext } from './XnsNameCaptureWithContext.tsx'
 
 export default { title: 'modules/xns/XnsNameCaptureWithContext' } as Meta
 
-const Template: StoryFn<typeof XnsNameCaptureWithContext> = (args) => {
-  // Get the current URL
+const testableParams = ['signature', 'username'] as const
+type RouteDecorator = (params: [typeof testableParams[number], string][]) => Decorator
+
+// eslint-disable-next-line react/display-name
+const RouteDecorator: RouteDecorator = params => (Story, args) => {
+  // Get the URL object
   const url = new URL(window.location.href)
 
-  // Update or add the URL parameter
-  url.searchParams.set('signature', '0x1234567890abcdef')
-
-  // Update the URL without reloading the page
+  // Remove all stale testable params from the URL
+  for (const param of testableParams) url.searchParams.delete(param)
   window.history.replaceState({}, '', url)
+
+  // Add the new testable params to the URL
+  for (const [param, value] of params) url.searchParams.set(param, value)
+
+  // Replace the URL without reloading
+  window.history.replaceState({}, '', url)
+
+  return <Story {...args} />
+}
+
+const Template: StoryFn<typeof XnsNameCaptureWithContext> = (args) => {
   return (
     <BrowserRouter>
       <XnsNameCaptureWithContext {...args}></XnsNameCaptureWithContext>
@@ -24,9 +39,20 @@ const Template: StoryFn<typeof XnsNameCaptureWithContext> = (args) => {
 }
 
 const Default = Template.bind({})
+Default.decorators = [RouteDecorator([['signature', '0x1234567890abcdef']])]
 Default.args = {}
 
-const WithOnBuyName = Template.bind({})
-WithOnBuyName.args = { navigate: (to: To) => alert(`navigated to: ${to}`), onCaptureName: (name: string) => Promise.resolve(alert(`Buy Name: ${name}`)) }
+const WithOnCaptureName = Template.bind({})
+WithOnCaptureName.decorators = [RouteDecorator([['signature', '0x1234567890abcdef']])]
+WithOnCaptureName.args = { navigate: (to: To) => alert(`navigated to: ${to}`), onCaptureName: (name: string) => Promise.resolve(alert(`Buy Name: ${name}`)) }
 
-export { Default, WithOnBuyName }
+const WithBadXnsNameInRoute = Template.bind({})
+WithBadXnsNameInRoute.decorators = [RouteDecorator([['signature', '0x1234567890abcdef'], ['username', 'badname.xyo.xyo']])]
+WithBadXnsNameInRoute.args = {
+  navigate: (to: To) => alert(`navigated to: ${to}`),
+  onCaptureName: (name: string) => Promise.resolve(alert(`Buy Name: ${name}`)),
+}
+
+export {
+  Default, WithBadXnsNameInRoute, WithOnCaptureName,
+}
