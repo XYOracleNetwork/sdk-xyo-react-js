ba621bc0b03b0f26196f1d9636e8f0337d45a467
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-10-22 12:50:16 -0400

    optionally clear existing utm params

--- packages/sdk/packages/advertising/src/utm/hooks/UseCaptureUtmLocation.stories.tsx
+++ packages/sdk/packages/advertising/src/utm/hooks/UseCaptureUtmLocation.stories.tsx
@@ -1,11 +1,12 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import type { Meta, StoryFn } from '@storybook/react'
 import { usePromise } from '@xylabs/react-promise'
+import { Utm } from '@xyo-network/advertising-payload-plugins'
 import type { PropsWithChildren } from 'react'
 import React, { useEffect } from 'react'
 import { BrowserRouter, useSearchParams } from 'react-router-dom'
 
-import { LatestUtmPayload } from '../lib/index.ts'
+import { LatestUtmPayload, UtmStorageArchivist } from '../lib/index.ts'
 import { useCaptureUtmLocation } from './useCaptureUtmLocation.ts'
 
 export default { title: 'advertising/UseCaptureUtmLocation' } as Meta
@@ -14,6 +15,10 @@ const UtmStub: React.FC<PropsWithChildren> = ({ children }) => {
   const [, setParams] = useSearchParams()
 
   useEffect(() => {
+    UtmStorageArchivist().then(async (archivist) => {
+      await archivist.clear()
+    }).catch(error => console.error(error))
+
     setParams(() => {
       const newParams = new URLSearchParams()
       newParams.set('utm_campaign', 'test-campaign')
@@ -33,17 +38,47 @@ const RouterDecorator = (Story: StoryFn) => (
   </BrowserRouter>
 )
 
+const TemplateInner = ({ message }: { message: string }) => {
+  const [params] = useSearchParams()
+
+  const [payload] = usePromise(async () => await LatestUtmPayload(), [])
+  return (
+    <Box sx={{
+      display: 'flex', flexDirection: 'column', gap: 2,
+    }}
+    >
+      <Typography>
+        Stored Payload:
+        <code>
+          {JSON.stringify(payload, null, 2)}
+        </code>
+      </Typography>
+      <Typography>
+        {message}
+        <code>
+          {JSON.stringify(params.toString(), null, 2)}
+        </code>
+      </Typography>
+    </Box>
+  )
+}
+
 const Template: StoryFn = () => {
   useCaptureUtmLocation()
 
-  const [payload] = usePromise(async () => {
-    return await LatestUtmPayload()
-  }, [])
+  return <TemplateInner message="Query Parameters (should be empty):" />
+}
 
-  return <Box>{JSON.stringify(payload, null, 2)}</Box>
+const TemplateWithoutClear: StoryFn = () => {
+  useCaptureUtmLocation(false)
+
+  return <TemplateInner message="Query Parameters (should NOT be empty):" />
 }
 
 const Default = Template.bind({})
 Default.decorators = [RouterDecorator]
 
-export { Default }
+const WithNoClearAfterCapture = TemplateWithoutClear.bind({})
+WithNoClearAfterCapture.decorators = [RouterDecorator]
+
+export { Default, WithNoClearAfterCapture }
--- packages/sdk/packages/advertising/src/utm/hooks/useCaptureUtmLocation.ts
+++ packages/sdk/packages/advertising/src/utm/hooks/useCaptureUtmLocation.ts
@@ -1,14 +1,16 @@
 import { usePromise } from '@xylabs/react-promise'
 import { type Utm, UtmSchema } from '@xyo-network/advertising-payload-plugins'
 import { useMemo } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 import { UtmStorageArchivist } from '../lib/index.ts'
-import { useDefaultUtmPageState } from './useDefaultUtmPageState.ts'
+import { useDefaultUtmPageState, UtmQueryParamStrings } from './useDefaultUtmPageState.ts'
 
-export const useCaptureUtmLocation = () => {
+export const useCaptureUtmLocation = (clearAfterCapture = true) => {
   const [{
     utm_campaign, utm_content, utm_medium, utm_source, utm_term,
   }] = useDefaultUtmPageState()
+  const [,setSearchParams] = useSearchParams()
 
   const [sessionStorageArchivist, error] = usePromise(async () => {
     return await UtmStorageArchivist()
@@ -18,21 +20,47 @@ export const useCaptureUtmLocation = () => {
     // Construct the base UTM payload
     const utm: Utm = { schema: UtmSchema }
 
-    // Conditionally add all the UTM parameters present
-    if (utm_campaign) utm.utm_campaign = utm_campaign
-    if (utm_content) utm.utm_content = utm_content
-    if (utm_medium) utm.utm_medium = utm_medium
-    if (utm_source) utm.utm_source = utm_source
-    if (utm_term) utm.utm_term = utm_term
+    // determine if there's a utm field that still has a value
+    // if there is none, it means that either the page loaded
+    // with none or they've already been captured
+    const utmParams = {
+      utm_campaign, utm_content, utm_medium, utm_source, utm_term,
+    }
+    const foundUpdatedUtmParams = Object.entries(utmParams).some(([key, value]) => {
+      return key.includes('utm_') && value
+    })
 
-    return utm
+    if (foundUpdatedUtmParams) {
+      // Conditionally add all the UTM parameters present
+      if (utm_campaign) utm.utm_campaign = utm_campaign
+      if (utm_content) utm.utm_content = utm_content
+      if (utm_medium) utm.utm_medium = utm_medium
+      if (utm_source) utm.utm_source = utm_source
+      if (utm_term) utm.utm_term = utm_term
+
+      return utm
+    } else {
+      return null
+    }
   }, [utm_campaign, utm_content, utm_medium, utm_source, utm_term])
 
   const [,insertError] = usePromise(async () => {
     if (utmPayload && sessionStorageArchivist) {
+      // insert params into session storage
       await sessionStorageArchivist.insert([utmPayload])
+
+      if (clearAfterCapture) {
+        // remove the utm params from the URL
+        setSearchParams(() => {
+          const newParams = new URLSearchParams()
+          for (const value of Object.values(UtmQueryParamStrings)) {
+            newParams.delete(value)
+          }
+          return newParams
+        })
+      }
     }
-  }, [sessionStorageArchivist, utmPayload])
+  }, [sessionStorageArchivist, utmPayload, clearAfterCapture])
 
   return { utmPayload, error: error ?? insertError }
 }
