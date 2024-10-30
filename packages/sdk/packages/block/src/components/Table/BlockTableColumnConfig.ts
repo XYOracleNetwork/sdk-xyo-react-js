5550f2eb1609283c4c1204c405613bbaf897392e
Arie Trouw <arie.trouw@xylabs.com>
2024-10-30 16:46:18 -0700

    Added Time to Blocks List
    4.1.16

--- packages/sdk/packages/block/src/components/Table/BlockTableColumnConfig.ts
+++ packages/sdk/packages/block/src/components/Table/BlockTableColumnConfig.ts
@@ -1,18 +1,17 @@
-export type BlockTableColumnSlug = 'hash' | 'payloads' | 'valid'
+/* eslint-disable @typescript-eslint/member-ordering */
+export type BlockTableColumnSlug = 'hash' | 'time' | 'payloads' | 'valid'
 
 export interface BlockTableColumnConfig {
   xs?: BlockTableColumnSlug[]
-  // eslint-disable-next-line @typescript-eslint/member-ordering
   sm?: BlockTableColumnSlug[]
-  // eslint-disable-next-line @typescript-eslint/member-ordering
   md?: BlockTableColumnSlug[]
-  // eslint-disable-next-line @typescript-eslint/member-ordering
   lg?: BlockTableColumnSlug[]
   xl?: BlockTableColumnSlug[]
 }
 
 export const blockColumnNames: Record<BlockTableColumnSlug, string> = {
   hash: 'Hash',
+  time: 'Time',
   payloads: 'Payloads',
   valid: 'Valid',
 }
@@ -20,9 +19,9 @@ export const blockColumnNames: Record<BlockTableColumnSlug, string> = {
 export const blockTableColumnConfigDefaults = (): BlockTableColumnConfig => {
   const xs: BlockTableColumnSlug[] = ['hash', 'payloads', 'valid']
   const sm: BlockTableColumnSlug[] = ['hash', 'payloads', 'valid']
-  const md: BlockTableColumnSlug[] = ['hash', 'payloads', 'valid']
-  const lg: BlockTableColumnSlug[] = ['hash', 'payloads', 'valid']
-  const xl: BlockTableColumnSlug[] = ['hash', 'payloads', 'valid']
+  const md: BlockTableColumnSlug[] = ['hash', 'time', 'payloads', 'valid']
+  const lg: BlockTableColumnSlug[] = ['hash', 'time', 'payloads', 'valid']
+  const xl: BlockTableColumnSlug[] = ['hash', 'time', 'payloads', 'valid']
   return {
     lg, md, sm, xl, xs,
   }
