fb6ad6eb02a98ec2c46f46b015dc024eb0866a60
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-20 14:56:18 -0500

    remove odd export

--- packages/sdk/packages/shared/src/contexts/index.ts
+++ packages/sdk/packages/shared/src/contexts/index.ts
@@ -1,4 +1,3 @@
-export * from '../../../../../../../../xylabs/sdk-react/packages/shared/src/Collapsible/index.ts'
 export * from './contextEx/index.ts'
 export * from './diviner/index.ts'
 export * from './ListMode/index.ts'
