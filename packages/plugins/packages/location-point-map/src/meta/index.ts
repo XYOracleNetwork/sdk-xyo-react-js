8311fd1d3c5691c053d12ea0c56035e9aaf29515
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-22 16:21:36 -0500

    update valid schemas

--- packages/plugins/packages/location-point-map/src/meta/index.ts
+++ packages/plugins/packages/location-point-map/src/meta/index.ts
@@ -1,9 +1,11 @@
-import { LocationSchema } from '@xyo-network/location-payload-plugin'
+import { GeographicCoordinateSystemLocationSchema, LocationSchema } from '@xyo-network/location-payload-plugin'
 import type { Payload } from '@xyo-network/payload-model'
 import type { PayloadRenderPluginBase } from '@xyo-network/react-payload-plugin'
 
+const validSchemas: Set<string> = new Set([LocationSchema, GeographicCoordinateSystemLocationSchema])
+
 const PointMapRenderPluginMeta: PayloadRenderPluginBase = {
-  canRender: (payload?: Payload) => payload?.schema === LocationSchema,
+  canRender: (payload?: Payload) => !(payload && validSchemas.has(payload.schema)),
   name: 'Point Map',
 }
 
