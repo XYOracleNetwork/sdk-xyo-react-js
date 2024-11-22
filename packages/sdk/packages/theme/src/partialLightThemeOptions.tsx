a0514fe0690b5e7fc75121c4251234ac92131f99
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-22 16:17:30 -0500

    remove mode

--- packages/sdk/packages/theme/src/partialLightThemeOptions.tsx
+++ packages/sdk/packages/theme/src/partialLightThemeOptions.tsx
@@ -2,7 +2,6 @@ import type { ThemeOptions } from '@mui/material'
 
 export const partialLightThemeOptions: ThemeOptions = {
   palette: {
-    mode: 'light',
     primary: { main: '#1f1a66' },
     secondary: { main: '#0f68c9' },
   },
