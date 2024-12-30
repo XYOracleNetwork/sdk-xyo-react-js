d797001907d91bec8bf1d29b2b8f408a65ae2014
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-12-30 17:59:06 -0500

    remove code comment

--- packages/sdk/packages/error/src/components/ErrorBoundary/ThrownErrorBoundary.tsx
+++ packages/sdk/packages/error/src/components/ErrorBoundary/ThrownErrorBoundary.tsx
@@ -4,7 +4,6 @@ import type { ModuleError } from '@xyo-network/payload-model'
 import type { FC } from 'react'
 import React from 'react'
 
-// calling the hook outside of the component since only can be called in functional component
 export const ThrownErrorBoundary: FC<ThrownErrorBoundaryProps<ModuleError>> = (props) => {
   return <ThrownErrorBoundaryBase<ModuleError> {...props} />
 }
