3047af3856390ce2cb774283115b2fa30497ab16
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-02-26 11:17:26 -0800

    start icon for factor

--- packages/sdk/packages/shared/src/components/bigint/TextField.tsx
+++ packages/sdk/packages/shared/src/components/bigint/TextField.tsx
@@ -1,32 +1,62 @@
 import type { FormControlProps, StandardTextFieldProps } from '@mui/material'
 import {
-  FormControl, FormHelperText, TextField,
+  Avatar,
+  FormControl, FormHelperText, Icon, InputAdornment, TextField,
 } from '@mui/material'
 import type { ChangeEventHandler } from 'react'
-import React from 'react'
+import React, { useState } from 'react'
 
 export interface BigIntTextFieldProps extends StandardTextFieldProps {
-  helperText?: string
-  hideHelperText?: boolean
   onChangeBigInt?: (value: bigint) => void
 }
 
 export const BigIntTextField: React.FC<BigIntTextFieldProps> = ({
-  helperText, hideHelperText, onChangeBigInt, onChange, ...props
+  helperText, onChangeBigInt, onChange, ...props
 }) => {
+  const [normalizedFactor, setNormalizedFactor] = useState(18)
   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     // callback for string value
     onChange?.(event)
 
     // callback for bigint value
-    const value = event.target.value
-    onChangeBigInt?.(BigInt(value))
+    const value = Number.parseFloat(event.target.value)
+    const normalizedValue = value * 10 ** normalizedFactor
+    onChangeBigInt?.(BigInt(normalizedValue))
+  }
+
+  const handleNormalizedChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { value } = event.target
+    setNormalizedFactor(Number.parseInt(value, 10))
   }
 
   return (
     <>
-      <TextField onChange={handleChange} type="number" {...props} />
-      {hideHelperText ? null : <FormHelperText>{helperText ?? 'Enter a base 10 number'}</FormHelperText>}
+      <TextField
+        onChange={handleChange}
+        type="number"
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <Icon fontSize="small">
+                  <Avatar sx={{
+                    fontSize: '.75rem',
+                    height: '20px',
+                    width: '20px',
+                  }}
+                  >
+                    {normalizedFactor}
+                  </Avatar>
+                </Icon>
+              </InputAdornment>
+            ),
+          },
+        }}
+        {...props}
+      />
+      <FormHelperText>{helperText ?? 'Enter a number'}</FormHelperText>
+      <TextField value={normalizedFactor} onChange={handleNormalizedChange} type="number" {...props} />
+      <FormHelperText>{helperText ?? 'Set the shift factor'}</FormHelperText>
     </>
   )
 }
