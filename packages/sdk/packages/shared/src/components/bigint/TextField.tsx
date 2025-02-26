87f0243924cfe67a58e7c18b6b7e089ea5bed5c8
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-02-26 12:23:34 -0800

    track various input states and handle errors

--- packages/sdk/packages/shared/src/components/bigint/TextField.tsx
+++ packages/sdk/packages/shared/src/components/bigint/TextField.tsx
@@ -1,10 +1,13 @@
 import type { FormControlProps, StandardTextFieldProps } from '@mui/material'
 import {
-  Avatar,
-  FormControl, FormHelperText, Icon, InputAdornment, TextField,
+  Avatar, FormControl, FormHelperText, IconButton, InputAdornment, TextField,
 } from '@mui/material'
-import type { ChangeEventHandler } from 'react'
-import React, { useState } from 'react'
+import type { FocusEventHandler } from 'react'
+import React, {
+  useMemo, useRef, useState,
+} from 'react'
+
+import { FixedPointPopover } from './FixedPointPopover.tsx'
 
 export interface BigIntTextFieldProps extends StandardTextFieldProps {
   onChangeBigInt?: (value: bigint) => void
@@ -13,50 +16,77 @@ export interface BigIntTextFieldProps extends StandardTextFieldProps {
 export const BigIntTextField: React.FC<BigIntTextFieldProps> = ({
   helperText, onChangeBigInt, onChange, ...props
 }) => {
-  const [normalizedFactor, setNormalizedFactor] = useState(18)
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    // callback for string value
+  const [value, setValue] = useState<number>(0)
+  const [rawValue, setRawValue] = useState<string>('')
+  const [fixedPoint, setFixedPoint] = useState(18)
+  const [error, setError] = useState<Error>()
+  const ref = useRef<HTMLButtonElement>(null)
+  const [open, setOpen] = useState(false)
+
+  const handleChange: FocusEventHandler<HTMLTextAreaElement> = (event) => {
+    // run standard callback
     onChange?.(event)
+    // remove all alpha characters but allow decimals
+    const filteredValue = event.target.value.replaceAll(/[^\d.]/g, '')
+    if (filteredValue.split('.').length > 2) return
+    setRawValue(filteredValue)
 
-    // callback for bigint value
-    const value = Number.parseFloat(event.target.value)
-    const normalizedValue = value * 10 ** normalizedFactor
-    onChangeBigInt?.(BigInt(normalizedValue))
+    const value = Number.parseFloat(filteredValue || '0')
+    if (Number.isNaN(value)) setValue(0)
+    setValue(value)
   }
 
-  const handleNormalizedChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const { value } = event.target
-    setNormalizedFactor(Number.parseInt(value, 10))
-  }
+  const onFixedPointChange = (fixedPoint: number) => setFixedPoint(fixedPoint)
+
+  useMemo(() => {
+    const fixedValue = value * (10 ** fixedPoint)
+    try {
+      const bigInitValue = BigInt(fixedValue)
+      onChangeBigInt?.(bigInitValue)
+      setError(undefined)
+    } catch (e) {
+      setError(e as Error)
+    }
+    // run bigInt callback
+  }, [value, fixedPoint])
 
   return (
     <>
       <TextField
         onChange={handleChange}
-        type="number"
+        type="string"
+        error={Boolean(error)}
         slotProps={{
+          htmlInput: { pattern: '[0-9]*[.]?[0-9]*' },
           input: {
             startAdornment: (
               <InputAdornment position="start">
-                <Icon fontSize="small">
+                <FixedPointPopover
+                  anchorEl={ref.current}
+                  anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                  fixedPoint={fixedPoint}
+                  onClose={() => setOpen(false)}
+                  onFixedPointChange={onFixedPointChange}
+                  open={open}
+                />
+                <IconButton size="small" ref={ref} onClick={() => setOpen(!open)}>
                   <Avatar sx={{
                     fontSize: '.75rem',
                     height: '20px',
                     width: '20px',
                   }}
                   >
-                    {normalizedFactor}
+                    {fixedPoint}
                   </Avatar>
-                </Icon>
+                </IconButton>
               </InputAdornment>
             ),
           },
         }}
+        value={rawValue}
         {...props}
       />
       <FormHelperText>{helperText ?? 'Enter a number'}</FormHelperText>
-      <TextField value={normalizedFactor} onChange={handleNormalizedChange} type="number" {...props} />
-      <FormHelperText>{helperText ?? 'Set the shift factor'}</FormHelperText>
     </>
   )
 }
