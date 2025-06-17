a3d505065c947ccdee5ceb2550c7db8f234164a1
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-06-17 16:48:48 -0400

    fix token symbol check and sane default if symbol not found

--- packages/sdk/packages/shared/src/components/TokenData/useGetTokenData.tsx
+++ packages/sdk/packages/shared/src/components/TokenData/useGetTokenData.tsx
@@ -2,8 +2,15 @@ import { TokenData } from './TokenData.ts'
 
 export const getTokenData = (symbols: (string | undefined)[]) => {
   return symbols?.map((symbol) => {
-    const additionalTokenData = TokenData.find(x => x.tokenSymbol === symbol)
-    const checkedTokenData = additionalTokenData ?? TokenData[0]
+    const additionalTokenData = TokenData.find(x => x.tokenSymbol.toLowerCase() === symbol?.toLowerCase())
+    const checkedTokenData = additionalTokenData ?? {
+      coinmarketcapLink: '',
+      etherscanLink: '',
+      icon: '',
+      readableName: 'Unknown Token',
+      tokenSymbol: 'unknown',
+      uniqueTokenId: 'unknown',
+    }
     return checkedTokenData
   })
 }
