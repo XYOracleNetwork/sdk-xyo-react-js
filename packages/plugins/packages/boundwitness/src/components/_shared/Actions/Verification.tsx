75ab87967143f1101ea283721dc7b71ac9feabca
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-06-18 15:37:35 -0400

    use new component

--- packages/plugins/packages/boundwitness/src/components/_shared/Actions/Verification.tsx
+++ packages/plugins/packages/boundwitness/src/components/_shared/Actions/Verification.tsx
@@ -1,46 +1,17 @@
-import { CancelRounded as CancelRoundedIcon, CheckCircleOutlineRounded as CheckCircleOutlineRoundedIcon } from '@mui/icons-material'
-import type { SvgIconProps } from '@mui/material'
-import { Typography } from '@mui/material'
-import { FlexCol } from '@xylabs/react-flexbox'
+import { ErrorQuickTipButton } from '@xylabs/react-error'
 import { usePromise } from '@xylabs/react-promise'
 import type { QuickTipButtonProps } from '@xylabs/react-quick-tip-button'
-import { QuickTipButton } from '@xylabs/react-quick-tip-button'
 import type { BoundWitness } from '@xyo-network/boundwitness-model'
 import { BoundWitnessValidator } from '@xyo-network/boundwitness-validator'
 import React from 'react'
-
-const InvalidIcon = ({ ref, ...props }: SvgIconProps &
-  { ref?: React.Ref<SVGSVGElement | null> }) => <CancelRoundedIcon color="error" ref={ref} {...props} />
-InvalidIcon.displayName = 'InvalidIcon'
-
 export interface BWVerification extends QuickTipButtonProps {
   boundwitness?: BoundWitness
   iconColors?: boolean
 }
 
 export const BWVerification: React.FC<BWVerification> = ({ boundwitness, iconColors }) => {
-  const [errors = []] = usePromise(async () => await (boundwitness ? new BoundWitnessValidator(boundwitness) : undefined)?.validate(), [boundwitness])
+  const [errors = [], validateError] = usePromise(async () =>
+    await (boundwitness ? new BoundWitnessValidator(boundwitness) : undefined)?.validate(), [boundwitness])
 
-  return (
-    <QuickTipButton
-      Icon={errors.length > 0
-        ? ({ ref, ...props }) => {
-            return <InvalidIcon color={iconColors ? 'error' : undefined} ref={ref} {...props} />
-          }
-        : ({ ref, ...props }) => {
-            return <CheckCircleOutlineRoundedIcon color={iconColors ? 'success' : undefined} ref={ref} {...props} />
-          }}
-      hoverText={errors.length > 0 ? 'Invalid Bound Witness' : 'Valid'}
-    >
-      {errors.length > 0
-        ? (
-            <FlexCol flexWrap="wrap" alignItems="start">
-              {errors.map((error) => {
-                return <Typography key={error.name}>{error.toString()}</Typography>
-              })}
-            </FlexCol>
-          )
-        : <Typography>No Errors</Typography>}
-    </QuickTipButton>
-  )
+  return <ErrorQuickTipButton errors={errors} iconColors={iconColors} validateError={validateError} />
 }
