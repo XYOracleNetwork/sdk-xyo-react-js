9dc6ca3228156fb21c4971c3d24cb722eb691d9a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-06-18 13:22:05 -0400

    warning and instructions to open story in new window

--- packages/sdk/packages/shared/src/components/Copy/CopyIconButton.stories.tsx
+++ packages/sdk/packages/shared/src/components/Copy/CopyIconButton.stories.tsx
@@ -1,6 +1,9 @@
-import { Check, Error } from '@mui/icons-material'
 import {
-  Box, Chip, TextField,
+  Check, Error, Launch,
+} from '@mui/icons-material'
+import {
+  Alert, Box, Chip, TextField,
+  Typography,
 } from '@mui/material'
 import type { Meta, StoryFn } from '@storybook/react-vite'
 import { isDefined } from '@xylabs/typeof'
@@ -19,9 +22,26 @@ const Template: StoryFn<typeof CopyIconButton> = (props) => {
   const [copyText, setCopyText] = useState<string>()
   const [copied, setCopied] = useState<boolean>(false)
   const [copiedText, setCopiedText] = useState<string>()
+  const inIframe = globalThis.self !== window.top
 
   return (
     <Box display="flex" flexDirection="column" alignItems="start" gap={1}>
+      {inIframe
+        ? (
+            <Alert severity="warning">
+              <Typography gutterBottom>
+                This test must be run in its own window and not nested in Storybook.
+              </Typography>
+              {' '}
+              <br />
+              Click the
+              {' '}
+              <Launch fontSize="small" />
+              {' '}
+              icon in the top right corner to open this story in a new window.
+            </Alert>
+          )
+        : null}
       <TextField value={copyText} onChange={e => setCopyText(e.target.value)} placeholder="Text to Copy" />
       <CopyIconButton onCopyCallback={() => setCopied(true)} value={copyText} {...props} />
       {copied ? <TextField defaultValue="" onChange={e => setCopiedText(e.target.value)} placeholder="Paste Text Here" /> : null}
