63ec3c1eb646c3d784fdd85f025124bd58750fb1
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-08-21 11:09:21 -0400

    fix deprecation

--- packages/sdk/packages/embed/src/components/embed-card/menu/EmbedMenu.tsx
+++ packages/sdk/packages/embed/src/components/embed-card/menu/EmbedMenu.tsx
@@ -6,7 +6,6 @@ import React, { useState } from 'react'
 import { JsonMenuItem } from './JsonMenuItem.tsx'
 
 export const EmbedMenu: React.FC<IconButtonProps> = (props) => {
-  // TODO - link to explore website
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
 
@@ -22,7 +21,15 @@ export const EmbedMenu: React.FC<IconButtonProps> = (props) => {
       <IconButton onClick={handleClick} {...props}>
         <MoreVertIcon />
       </IconButton>
-      <Menu anchorEl={anchorEl} open={open} onClose={handleClose} PaperProps={{ variant: 'elevation' }} MenuListProps={{ dense: true }}>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          paper: { variant: 'elevation' },
+          list: { dense: true },
+        }}
+      >
         <JsonMenuItem />
       </Menu>
     </>
