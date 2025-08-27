22411129de5ad66f3c3043cbee939c24d50c0a1b
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-08-27 12:17:25 -0400

    code comment

--- packages/sdk/packages/shared/src/components/bigint/helpers/formatBigIntInput.ts
+++ packages/sdk/packages/shared/src/components/bigint/helpers/formatBigIntInput.ts
@@ -1,3 +1,7 @@
+/**
+ * Format a raw input string (i.e. Change event from input type=text)
+ * Designed to filter out non-digits and extra decimals
+ **/
 export const formatBigIntInput = (rawValue: string) => {
   // remove all alpha characters but allow decimals
   const filteredValue = rawValue.replaceAll(/[^\d.]/g, '')
