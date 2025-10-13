024f4000051caa0759689ee523d068338eeae04b
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-10-13 11:03:35 -0400

    add static payloads for known price widget configs

--- packages/plugins/packages/aggregate-price/src/index.ts
+++ packages/plugins/packages/aggregate-price/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/index.ts'
+export * from './lib/index.ts'
 export * from './models/index.ts'
 export * from './Plugin.ts'
 export { CryptoAssetRenderPlugin as default } from './Plugin.ts'
---
+++ packages/plugins/packages/aggregate-price/src/lib/index.ts
@@ -0,0 +1,1 @@
+export * from './PriceWidgetConfigs.ts'
